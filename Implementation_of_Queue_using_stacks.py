8de02811aaf66dc2f23956ace5b58a69898aee13
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-05-03 23:22:30 +0300

    Update Implementation_of_Queue_using_stacks.py

--- Implementation_of_Queue_using_stacks.py
+++ Implementation_of_Queue_using_stacks.py
@@ -8,29 +8,59 @@ class MyQueue(object):
         :type x: int
         :rtype: None
         """
+        # if len(self.stack2) == 0:
+        #     self.stack1.append(x)
+        # else:
+        #     while len(self.stack2) > 0:
+        #         last = self.stack2[-1]
+        #         self.stack2.pop()
+        #         self.stack1.append(last)
+        #     self.stack1.append(x)
         self.stack1.append(x)
-
     def pop(self):
         """
         :rtype: int
         """
-        if (len(self.stack2) == 0):
-            while(len(self.stack1) != 0):
-                self.stack2.append(self.stack1.pop())
+        # if len(self.stack1) == 0 and len(self.stack2) == 0:
+        #     return None
+        # elif len(self.stack1) > 0:
+        #     while len(self.stack1) > 0:
+        #         self.stack2.append(self.stack1.pop())
+        # return self.stack2.pop()
+        if (len(self.stack1) == 0 and len(self.stack2) == 0):
+            return None
+        elif (len(self.stack2) != 0):
             return self.stack2.pop()
         else:
-            return self.stack2.pop()
+            if (len(self.stack1) == 1):
+                return self.stack1.pop()
+            for i in range(len(self.stack1) - 1):
+                self.stack2.append(self.stack1.pop())
+            return self.stack1.pop()
 
     def peek(self):
         """
         :rtype: int
         """
-        if (len(self.stack2) == 0):
-            while(len(self.stack1) != 0):
+        # if len(self.stack1) == 0 and len(self.stack2) == 0:
+        #     return None
+        # elif len(self.stack1) > 0:
+        #     while len(self.stack1) > 0:
+        #         self.stack2.append(self.stack1.pop())
+        # return self.stack2[-1]
+        if (len(self.stack1) == 0 and len(self.stack2) == 0):
+            return None
+        elif (len(self.stack2) != 0):
+            return self.stack2[-1]
+        elif (len(self.stack2) == 0 and len(self.stack1) != 0):
+            if (len(self.stack1) == 1):
+                return self.stack1[0]
+            for i in range(len(self.stack1)):
                 self.stack2.append(self.stack1.pop())
             return self.stack2[-1]
-        else:
-            return self.stack2[-1]
+            # for i in range(len(self.stack2)):
+            #     self.stack1.append(self.stack2.pop())
+            # return value
 
     def empty(self):
         """
@@ -40,8 +70,6 @@ class MyQueue(object):
             return True
         else:
             return False
-        
-
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
