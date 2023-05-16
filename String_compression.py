4ed287ea3e1641572cf44f2410d367d2f1838c6b
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-05-16 15:28:46 +0300

    Update String_compression.py

--- String_compression.py
+++ String_compression.py
@@ -10,25 +10,27 @@ class Solution(object):
             return 1
         else:
             i = 0
+            pointer = 0
             while i < len(chars):
                 if (i == len(chars) -1):
-                    if counter == 1:
+                    if pointer == i:
                         s += str(chars[i])
                     else:
-                        s += str(chars[i]) + str(counter)
-                        chars [i-counter + 1:i+1] = list(s)
+                        s += str(chars[i]) + str(i - pointer + 1)
+                        chars [pointer:i+1] = list(s)
                     break
                 elif chars[i] == chars [i+1]:
-                    counter += 1
+                    pointer -= 1
                 else:
-                    if counter == 1:
+                    if pointer == i:
                         s += str(chars[i])
                     else:
-                        s += str(chars[i]) + str(counter)
-                        chars [i-counter + 1:i+1] = list(s)
-                        i -= (counter - 2)
-                    counter = 1
+                        s += str(chars[i]) + str(i - pointer + 1)
+                        chars [pointer:i+1] = list(s)
+                        pointer += 1
+                        i = pointer
                     s = ""
                 i += 1
+                pointer += 1
             return len(chars)  
             
