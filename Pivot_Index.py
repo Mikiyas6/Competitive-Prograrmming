975224f42b4073914e02971b9286970753d332f8
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-05-17 14:56:39 +0300

    Update Pivot_Index.py

--- Pivot_Index.py
+++ Pivot_Index.py
@@ -4,15 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        sums = 0
+        prefix_sums_forward = []
+        prefix_sums_reversed = []
+        lists = []
         for i in range(len(nums)):
-            if i == 0 and sum(nums[i+1:]) == 0:
-                return 0
-            elif sum(nums[i+1:]) == sum(nums[:i]):
-                return i
-            elif (i == len(nums) -1):
-                if (sum(nums[:i]) == 0):
-                    return i
-                else:
-                    return -1
+            sums += nums[i]
+            prefix_sums_forward.append(sums)
+        sums = 0
+        for j in range(-1,-len(nums)-1,-1):
+            sums += nums[j]
+            prefix_sums_reversed.append(sums)
+        for k in range(len(prefix_sums_reversed)):
+            lists.append(prefix_sums_reversed[-1])
+            prefix_sums_reversed.pop(-1)
+        prefix_sums_reversed = lists
+        # print(prefix_sums_forward)
+        # print(prefix_sums_reversed)
+        for z in range (len(prefix_sums_forward)):
+            if (prefix_sums_forward[z] == prefix_sums_reversed[z]):
+                return z
+        else:
+            return -1
+                
+
+    
+            
+
     
             
