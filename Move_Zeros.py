0f8b74694ded2dd3dc4ea63eccd1dc8d225a9985
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-06-17 08:44:31 +0300

    Move_Zeros.py

--- Move_Zeros.py
+++ Move_Zeros.py
@@ -5,11 +5,21 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         i = 0
-        length = len(nums)
-        while(i < length):
-            if nums[i] == 0:
-               nums.append(nums.pop(i))
-               i -= 1
-               length -= 1
-            i += 1
+        j = 0
+        while i < len(nums):
+            if len(nums) == 1:
+                break
+            elif nums[i] == 0:
+                i += 1
+            elif nums[i] != 0 and nums[j] != 0:
+                i += 1
+                j += 1
+            elif nums[i] != 0 and nums[j] == 0:
+                temp  = nums[i]
+                nums[j] = temp
+                nums[i] =  0
+                j += 1
+                i += 1
+        
+        
         
