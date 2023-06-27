2dc39c958f394c164c3df1102ff68c7578818ded
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-06-27 21:21:21 +0300

    Update Find_the_winner_of_the_circular_game.py

--- Find_the_winner_of_the_circular_game.py
+++ Find_the_winner_of_the_circular_game.py
@@ -1,18 +1,11 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        list_of_friends = []
-        for i in range(1,n+1):
-            list_of_friends.append(i)
-        i = k % n - 1
-        if i < 0:
-            i = i + len(list_of_friends)
+        list_of_friends = list(range(1,n+1))
+        i = k - 1
         while len(list_of_friends) > 1:
-            list_of_friends.pop(i)
-            print(i,list_of_friends)
-            i += k - 1
-            if i >= len(list_of_friends):
-                i = i - len(list_of_friends)
-                while i >= len(list_of_friends):
+            while i >= len(list_of_friends):
                     i = i - len(list_of_friends) 
+            list_of_friends.pop(i)
+            i += k - 1        
         return list_of_friends[0]
         
