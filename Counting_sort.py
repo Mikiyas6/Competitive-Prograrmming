af14b9f93eb25236e3c50133d593b5dc988a8566
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-07-03 01:56:46 +0300

    Update Counting_sort.py

--- Counting_sort.py
+++ Counting_sort.py
@@ -15,24 +15,11 @@ import sys
 
 def countingSort(arr):
     # Write your code here
-    lists = [0] * (100)
+    length = max(arr)
+    frequency = [0] * length
     for i in range(len(arr)):
-        lists[arr[i]] += 1
-    # print(lists)
-    listo = []
-    for i in range(len(lists)):
-        if i == 0:
-            listo.append(lists[i])
-        else:
-            listo.append(lists[i]+listo[-1])
-    # print(listo)
-    listings = [0] * (len(arr))
-    # print(listings)
-    for i in range(len(arr)):
-        listings[listo[arr[i]]-1] = arr[i]
-        listo[arr[i]] = listo[arr[i]] - 1
-    # print(listings)
-    return lists
+        frequency[arr[i]] += 1
+    return frequency
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
