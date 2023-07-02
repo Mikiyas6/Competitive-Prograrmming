1b9c78739a04493bc60bf11212f09f85c666e389
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-07-03 01:55:29 +0300

    Update Insertion_sort_list.py

--- Insertion_sort_list.py
+++ Insertion_sort_list.py
@@ -1,33 +1,39 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def insertionSortList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        current = head
-        lists = []
-        while current != None:
-            lists.append(current.val)
-            current = current.next
-        for i in range(1,len(lists)):
-            pointer = i
-            for j in range(i-1,-1,-1):
-                if lists[pointer] < lists[j]:
-                    self.swap(lists,j,pointer)
-                    pointer -= 1
-                else:
-                    break
-        current = head
-        for i in range(len(lists)):
-            current.val = lists[i]
-            current = current.next
-        return head
-    def swap(self,lists,x,y):
-        temp = lists[x]
-        lists[x] = lists[y]
-        lists[y] = temp
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    # Write your code here
+    value = arr[-1]
+    arr.pop()
+    for i in range(n-2,-1,-1): 
+        if value < arr[i]:
+            arr.insert(i+1,arr[i])
+            print(" ".join(str(x) for x in arr))
+            arr.pop(i)
+            if i == 0:
+                arr.insert(i,value)
+                print(" ".join(str(x) for x in arr))
+                break
+        else:
+            arr.insert(i+1,value)
+            print(" ".join(str(x) for x in arr))
+            break
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
