b45fddde24eb70ffb111a351b1c98311c30c1384
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-07-03 02:22:24 +0300

    Update Find_target_indices_after_sorting_array.py

--- Find_target_indices_after_sorting_array.py
+++ Find_target_indices_after_sorting_array.py
@@ -1,22 +1,11 @@
-class Solution(object):
-    def targetIndices(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[j] < nums[i]:
+                    nums[j], nums[i] = nums[i], nums[j]
         lists = []
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if j == len(nums) - i -1:
-                    break
-                elif (nums[j+1] < nums[j]):
-                    self.swap(nums,j,j+1)
-        for k in range(len(nums)):
-            if nums[k] == target:
-                lists.append(k)
+            if nums[i] == target:
+                lists.append(i)
         return lists
-    def swap(self,a,x,y):
-        temp = a[x]
-        a[x] = a[y]
-        a[y] = temp
