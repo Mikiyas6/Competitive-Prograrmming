edcbc813bae8ca4c0ce5d863b6efde119b7ee90f
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-07-02 23:13:22 +0300

    Update Maximum_number_of_coins_you_can_get.py

--- Maximum_number_of_coins_you_can_get.py
+++ Maximum_number_of_coins_you_can_get.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type piles: List[int]
         :rtype: int
         """
-        piles = self.Counting_sort(piles)
+        piles = sorted(piles)
         pint = []
         for i in range(len(piles)):
             pint.append(piles.pop())
@@ -13,21 +13,7 @@ class Solution(object):
         for i in range(len(piles)/3):
             total += piles[2*i + 1]
         return total
-    def Counting_sort(self,nums):
-        lists1 = [0] * (max(nums)+1)
-        for i in range(len(nums)):
-            lists1[nums[i]] += 1
-        lists2 = []
-        for i in range(len(lists1)):
-            if i == 0:
-                lists2.append(lists1[i])
-            else:
-                lists2.append(lists1[i]+lists2[-1])
-        lists3 = [0] * (len(nums))
-        for i in range(len(nums)):
-            lists3[lists2[nums[i]]-1] = nums[i]
-            lists2[nums[i]] = lists2[nums[i]] - 1
-        return lists3
+    
 
 
         
