68f636e59c8c4ade93d0e9b5746e78082277db02
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-07-03 12:22:59 +0300

    Update valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,12 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        list_of_s = list(s)
-        list_of_t = list(t)
-        if len(list_of_t) != len(list_of_s):
+
+        # list_of_s = list(s)
+        # list_of_t = list(t)
+        # if len(list_of_t) != len(list_of_s):
+        #     return False
+        # for letter in list_of_t:
+        #     if letter not in list_of_s:
+        #         return False
+        #     else:
+        #         list_of_s.remove(letter)
+        # return True
+        if len(s) != len(t):
             return False
-        for letter in list_of_t:
-            if letter not in list_of_s:
+        dictionary_for_s = {}
+        for letter in s:
+            if letter not in dictionary_for_s.keys():
+                dictionary_for_s[letter] = s.count(letter)
+        for letter in t:
+            if letter not in dictionary_for_s.keys() or t.count(letter) != dictionary_for_s[letter]:
                 return False
-            else:
-                list_of_s.remove(letter)
         return True
