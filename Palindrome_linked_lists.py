6a9ce8be73d9e8b6468accd47035df395987d647
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-07-08 12:19:56 +0300

    Update Palindrome_linked_lists.py

--- Palindrome_linked_lists.py
+++ Palindrome_linked_lists.py
@@ -1,26 +1,62 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        lists = []
-        current = head
-        while(current != None):
-            lists.append(current.val)
-            current = current.next
-        while(len(lists) > 0):
-            if (lists[0] == lists[-1] and len(lists) > 1):
-                lists.pop(0)
-                lists.pop(-1)
-                continue
-            elif(len(lists) == 1):
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        elif not head.next:
+            return True
+        elif not head.next.next:
+            if head.val == head.next.val:
                 return True
             else:
                 return False
-        return True
+        else:
+            # Finding the middle node
+            slow = head
+            fast = head
+            while fast and fast.next:
+                previous = slow
+                slow = slow.next
+                fast = fast.next.next
+            # Finding the length of the Linked List
+            current = head
+            counter = 0
+            while current:
+                counter += 1
+                current = current.next
+            # Deciding path based on the length of the Linked List
+            new_head = None
+            if counter %2 == 0:
+                new_head = slow
+                previous.next = None
+            else:
+                new_head = slow.next
+                previous.next = None
+            # Reversing the new Linked List
+            previous_node = None
+            current_node = new_head
+            while current_node:
+                if not previous_node and not current_node.next:
+                    previous_node = current_node
+                    break
+                next_node = current_node.next
+                current_node.next = previous_node
+                previous_node = current_node
+                current_node = next_node
+            new_head = previous_node
+            # Comparing corresponding values inside the two Linked Lists
+            current1 = head
+            current2 = new_head
+            while current1 and current2:
+                if current1.val != current2.val:
+                    return False
+                current1 = current1.next
+                current2 = current2.next
+            return True
+
+
+    
