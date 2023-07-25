3af0575f5567340888fec8d02dfae5659ebd340c
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-07-25 18:08:46 +0300

    Update Product_of_array_except_self.py

--- Product_of_array_except_self.py
+++ Product_of_array_except_self.py
@@ -1,21 +1,20 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        lists1 = []
-        lists2 = []
-        product = 1
-        for i in range(len(nums)):
-            lists1.append(product)
-            product *= nums[i]
-        product = 1
-        lists2 = range(len(nums))
-        for j in range(-1,-len(nums)-1,-1):
-            lists2[j] = product * lists1[j]
-            product *= nums[j]
-        return lists2
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return
+        elif len(nums) == 1:
+            return 1
+        else:
+            lists1, lists2 = [1], [1]*len(nums)
+            lists3 = []
+            for i in range(1,len(nums)):
+                lists1.append(lists1[-1] * nums[i-1])
+            for j in range(len(nums)-2,-1,-1):
+                lists2[j] = (lists2[j+1] * nums[j+1])
+            for k in range(len(nums)):
+                lists3.append(lists1[k] * lists2[k])
+            return lists3
+        
         
 
 
