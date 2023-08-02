483e5d00c3d73d0fecc83dba7d1dbcbfb7af2dc3
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-08-02 21:43:48 +0300

    Update Power_of_Three.py

--- Power_of_Three.py
+++ Power_of_Three.py
@@ -1,17 +1,8 @@
-class Solution(object):
-    def isPowerOfThree(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        if (n == 0):
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 1:
             return False
-        elif (n == 1):
-            return True
-        if (n %3 != 0):
-            return False
-        elif( n == 3):
+        elif n == 1:
             return True
         else:
             return self.isPowerOfThree(n/3)
-        
