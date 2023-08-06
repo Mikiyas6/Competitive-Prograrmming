b51a3bbcb2a487b23ba56ee572829afdc5ac0fe9
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-08-06 21:35:59 +0300

    Update Merge_Two_sorted_lists.py

--- Merge_Two_sorted_lists.py
+++ Merge_Two_sorted_lists.py
@@ -1,76 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        current1 = list1
-        current2 = list2
-        lists1 = []
-        lists2 = []
-        lists3 = []
-        lists4 = []
-        while(current1 != None):
-            lists1.append(current1.val)
-            current1 = current1.next
-        while(current2 != None):
-            lists2.append(current2.val)
-            current2 = current2.next
-        for value in lists1:
-            lists3.append(value)
-        for value in lists2:
-            lists3.append(value)
-        def sorter(lists):
-            for i in range(len(lists)):
-                for j in range(len(lists) - 1):
-                    if lists[j] > lists[j + 1]:
-                        lists[j], lists[j + 1] = lists[j + 1], lists[j]
-            return lists
-        lists4 = sorter(lists3)
-        def insert_at_the_end(lists):
-            if(len(lists) == 0):
-                    return None
-            else:    
-                value = lists[0]
-                lists.pop(0)
-                return ListNode(value,insert_at_the_end(lists))
-        l3 = insert_at_the_end(lists4)
-        return l3
-            
-        
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 and not list2:
+            return None
+        elif not list1 and list2:
+            value = list2.val
+            list2 = list2.next
+            return ListNode(value,self.mergeTwoLists(list1,list2))
+        elif list1 and not list2:
+            value = list1.val
+            list1 = list1.next
+            return ListNode(value,self.mergeTwoLists(list1,list2))
+        else:
+            if list1.val <= list2.val:
+                value = list1.val
+                list1 = list1.next
+                return ListNode(value,self.mergeTwoLists(list1,list2))
+            else:
+                value = list2.val
+                list2 = list2.next
+                return ListNode(value,self.mergeTwoLists(list1,list2))
 
 
 
-
-        
-                
-
-
-
-        
-
-                    
-                    
-
-
-                    
-
-
-            
-
-
-                
-
-
-
-
-        
-            
-
-        
