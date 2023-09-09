205868deb5ae1c7244836b48da169bd7a7709d17
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-09-09 09:22:15 +0300

    Create Binary_Tree_Maximum_Path_Sum.py

---
+++ Binary_Tree_Maximum_Path_Sum.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        max_path_sum = float('-inf')  # Renamed max_sum to max_path_sum
+        
+        def calculateMaxPath(node):
+            nonlocal max_path_sum
+            if not node:
+                return 0
+            
+            left_gain = max(calculateMaxPath(node.left), 0)
+            right_gain = max(calculateMaxPath(node.right), 0)
+            
+            current_path_sum = node.val + left_gain + right_gain
+            max_path_sum = max(max_path_sum, current_path_sum)
+            
+            return node.val + max(left_gain, right_gain)
+    
+        calculateMaxPath(root)
+        return max_path_sum
