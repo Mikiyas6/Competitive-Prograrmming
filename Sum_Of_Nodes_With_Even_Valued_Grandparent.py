a8687ad2034088d0080f075d0b92b45bea402649
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-09-21 23:04:49 +0300

    Update Sum_Of_Nodes_With_Even_Valued_Grandparent.py

--- Sum_Of_Nodes_With_Even_Valued_Grandparent.py
+++ Sum_Of_Nodes_With_Even_Valued_Grandparent.py
@@ -1,27 +1,27 @@
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
-        total_sum = 0  # Initialize a variable to keep track of the total sum of even-grandparent values.
+        total_sum = 0  
 
         def dfs(node):
-            nonlocal total_sum  # Use a more descriptive name for the total sum variable.
+            nonlocal total_sum  
 
             if not node:
                 return 0
 
-            if node.val % 2 == 0:  # Check if the current node's value is even.
+            if node.val % 2 == 0:  
                 if node.left:
                     if node.left.left:
-                        total_sum += node.left.left.val  # Add the value of the left grandchild if it exists.
+                        total_sum += node.left.left.val  
                     if node.left.right:
-                        total_sum += node.left.right.val  # Add the value of the right grandchild if it exists.
+                        total_sum += node.left.right.val 
                 if node.right:
                     if node.right.left:
-                        total_sum += node.right.left.val  # Add the value of the left grandchild if it exists.
+                        total_sum += node.right.left.val  
                     if node.right.right:
-                        total_sum += node.right.right.val  # Add the value of the right grandchild if it exists.
+                        total_sum += node.right.right.val 
 
-            dfs(node.left)  # Recursively explore the left subtree.
-            dfs(node.right)  # Recursively explore the right subtree.
+            dfs(node.left)  
+            dfs(node.right)  
 
-        dfs(root)  # Start the depth-first search from the root of the tree.
-        return total_sum  # Return the computed total sum.
+        dfs(root)  
+        return total_sum  
