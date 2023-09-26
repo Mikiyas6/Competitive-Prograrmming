503b9e9c1c220275daff2dfc126956c70e6dd6b4
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-09-26 22:37:33 +0300

    Update Number_of_Provinces.py

--- Number_of_Provinces.py
+++ Number_of_Provinces.py
@@ -1,22 +1,34 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        adjacency_list = defaultdict(list)
+        from collections import defaultdict
+
+        Graph = defaultdict(list)
+
         for i in range(len(isConnected)):
-            for j in range(len(isConnected[i])):
-                if isConnected[i][j]:
-                    adjacency_list[i].append(j)
-        visited_nodes = set()
-        
-        def depth_first_search(adjacency_list, visited, node):
+            flag = False
+            for j in range(len(isConnected)):
+                if i != j and isConnected[i][j] == 1:
+                    flag = True
+                    Graph[i].append(j)
+            if not flag:
+                Graph[i].append(None)
+
+    
+        def dfs(node):
+
             visited.add(node)
-            for neighbor in adjacency_list[node]:
+
+            for neighbor in Graph[node]:
                 if neighbor not in visited:
-                    depth_first_search(adjacency_list, visited, neighbor)
-        
-        num_provinces = 0
-        for i in range(len(isConnected)):
-            if i not in visited_nodes:
-                num_provinces += 1
-                depth_first_search(adjacency_list, visited_nodes, i)
-                
-        return num_provinces
+                    visited.add(neighbor)
+                    dfs(neighbor)
+
+        visited = set()    
+        counter = 0
+
+        for i in range(len(Graph)):
+            if i not in visited:
+                dfs(i)
+                counter += 1
+        return counter
+
