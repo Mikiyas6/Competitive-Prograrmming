95fa2c3c1bae4d9d52f530ef72ffa611b46317cb
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-09-29 12:43:43 +0300

    Update Average_of_levels_in_binary_tree.py

--- Average_of_levels_in_binary_tree.py
+++ Average_of_levels_in_binary_tree.py
@@ -1,32 +1,37 @@
-from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        if not root:
-            return []
-
+    
         level_queue = deque([root])
         average_values = []
+        all_items_by_level = []
 
         while level_queue:
+            
             level_size = len(level_queue)
             level_sum = 0
+            level_items = []
+            
 
             for _ in range(level_size):
-                current_node = level_queue.popleft()
-                level_sum += current_node.val
 
-                if current_node.left:
-                    level_queue.append(current_node.left)
-                if current_node.right:
-                    level_queue.append(current_node.right)
+                node = level_queue.popleft()
+                level_items.append(node.val)
+                level_sum += node.val
 
+                if node.left:
+                    level_queue.append(node.left)
+                if node.right:
+                    level_queue.append(node.right)
+
+            all_items_by_level.append(level_items)
             level_average = level_sum / level_size
             average_values.append(level_average)
-        
+
         return average_values
