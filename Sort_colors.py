18393b5075432460b61ee6acab8c9fd7fbed0902
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-10-06 18:16:42 +0300

    Update Sort_colors.py

--- Sort_colors.py
+++ Sort_colors.py
@@ -10,5 +10,20 @@ class Solution(object):
                     break
                 elif nums[j+1] < nums[j]:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
+
+        # for i in range(1,n):
+
+        #     key = nums[i]
+        #     j = i - 1
+
+        #     while j >= 0 and key < nums[j]:
+
+        #         nums[j+1] = nums[j]
+        #         j -= 1
+            
+        #     nums[j+1] = key
+        
+
+        
         
         
