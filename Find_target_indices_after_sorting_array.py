87ca82745e62b4aa894a01f0023d34f5e7bc2b18
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-10-07 12:06:53 +0300

    Update Find_target_indices_after_sorting_array.py

--- Find_target_indices_after_sorting_array.py
+++ Find_target_indices_after_sorting_array.py
@@ -1,11 +1,21 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[j] < nums[i]:
-                    nums[j], nums[i] = nums[i], nums[j]
+        
+        n = len(nums)
         lists = []
+
         for i in range(len(nums)):
+
+            min_index = i
+
+            for j in range(i+1,len(nums)):
+
+                if nums[j] < nums[min_index]:
+                    min_index = j
+                
+            nums[i],nums[min_index] = nums[min_index], nums[i]
+            
             if nums[i] == target:
                 lists.append(i)
+
         return lists
