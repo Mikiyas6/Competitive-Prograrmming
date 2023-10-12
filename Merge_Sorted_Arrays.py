ec1027de4998bfeaa91af41bbd246da34e4f91e0
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-10-12 22:11:47 +0300

    Update Merge_Sorted_Arrays.py

--- Merge_Sorted_Arrays.py
+++ Merge_Sorted_Arrays.py
@@ -19,7 +19,27 @@ class Solution:
 
             nums1[j+1] = key
         
-        print(nums1)
+        class Solution:
+    
+        # i = m - 1
+        # j = n - 1
+        # k = m + n - 1
+
+        # while j >= 0:
+
+        #     if i >=0 and nums1[i] > nums2[j]:
+        #         nums1[k] = nums1[i]
+        #         i -= 1
+        #     else:
+        #         nums1[k] = nums2[j]
+        #         j -= 1
+
+        #     k -= 1
+
+        
+        
+
+        
         
 
         
