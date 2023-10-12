2945259bd0a29ebc56331a4fd0a5a40445ce2ce3
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-10-12 11:59:51 +0300

    Create check_if_an_array_is_sorted.py

---
+++ check_if_an_array_is_sorted.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    def arraySortedOrNot(self, arr, n):
+        # code here
+        if n == 1:
+            return 1
+            
+        i = 0
+        j = i + 1
+        
+        while j < n:
+            
+            if arr[i] > arr[j]:
+                return 0
+            
+            i += 1
+            j += 1
+            
+        return 1
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        
+        ob = Solution()
+        ans = ob.arraySortedOrNot(arr, n)
+        if ans:
+            print(1)
+        else:
+            print(0)
+        tc -= 1
+
+# } Driver Code Ends
