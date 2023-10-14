1b7a2bcf44b46205d3c08612be243158568f8ad6
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-10-14 23:13:52 +0300

    Update Rotate_Array.py

--- Rotate_Array.py
+++ Rotate_Array.py
@@ -3,11 +3,35 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            nums.insert(0,nums.pop())
+        k = (k) % len(nums)
 
+        i = 0
+        j = len(nums) - 1
 
-            
+        while i < j:
 
+            nums[i], nums[j] = nums[j], nums[i]
 
-    
+            i += 1
+            j -= 1
+        
+        i = 0
+        j = k - 1
+
+        while i < j:
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+            i += 1
+            j -= 1
+        
+        i = k
+        j = len(nums) - 1
+
+        while i < j:
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+            i += 1
+            j -= 1
+        
