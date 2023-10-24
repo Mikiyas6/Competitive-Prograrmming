f1b6ae2ef6ca10ae66a5485fddde104b2486b5d5
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-10-24 13:13:15 +0300

    Update sub_array_sum_equals_k.py

--- sub_array_sum_equals_k.py
+++ sub_array_sum_equals_k.py
@@ -1,16 +1,18 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        dictionary = {}
-        total = 0
-        counter = 0
+        
+        curr_sum = 0
+        res = 0
+        prefix_sum = {0:1}
+
         for i in range(len(nums)):
-            total += nums[i]
-            if total == k:
-                counter += 1
-            if (total - k) in dictionary.keys():
-                counter += dictionary[total-k]
-            if total in dictionary:
-                dictionary[total] += 1
-            else:
-                dictionary[total] = 1
-        return counter
+
+            curr_sum += nums[i]
+            diff = curr_sum - k
+
+            res += prefix_sum.get(diff,0)
+            prefix_sum[curr_sum] = 1 + prefix_sum.get(curr_sum,0)
+
+        return res
+
+            
