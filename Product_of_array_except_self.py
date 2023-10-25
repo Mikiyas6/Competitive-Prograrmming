ae47cacf53d491436d5d5c26291b7bcc0fb70a6b
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-10-25 10:25:21 +0300

    Update Product_of_array_except_self.py

--- Product_of_array_except_self.py
+++ Product_of_array_except_self.py
@@ -1,31 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        if not nums:
-            return
-        elif len(nums) == 1:
-            return 1
-        else:
-            lists1, lists2 = [1], [1]*len(nums)
-            lists3 = []
-            for i in range(1,len(nums)):
-                lists1.append(lists1[-1] * nums[i-1])
-            for j in range(len(nums)-2,-1,-1):
-                lists2[j] = (lists2[j+1] * nums[j+1])
-            for k in range(len(nums)):
-                lists3.append(lists1[k] * lists2[k])
-            return lists3
         
-        
+        output = [0]*len(nums)
+        prefix = 1
+        postfix = 1
 
+        for i in range(len(nums)):
 
+            output[i] = prefix
+            prefix *= nums[i]
+        
+        for j in range(len(nums)-1,-1,-1):
 
+            output[j] *= postfix
+            postfix *= nums[j]
+        
+        return output
 
-
-
-        
-        
-        
-        
-
-        
-        
