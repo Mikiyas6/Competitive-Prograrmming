daabfe869721489e5c007bdb98e98e49db5d5ad8
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-04 00:25:22 +0300

    Update relative_sort_array.py

--- relative_sort_array.py
+++ relative_sort_array.py
@@ -1,32 +1,60 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        
+
         dictionary = {}
 
-        for i in range(len(arr2)):
-            dictionary[arr2[i]] = i
+        for index,value in enumerate(arr2):
 
-        lists = []
+            dictionary[value] = index
+
+        lists1 = []
+        lists2 = []
         
-        def custom_comparator(items):
-
-            nonlocal lists
-            nonlocal dictionary
-            
-            if items in dictionary.keys():
-                return dictionary[items]
-            
-        
-        new_list1 = []
-        new_list2 = []
-
         for value in arr1:
-            if value in arr2:
-                new_list1.append(value)
+
+            if value not in arr2:
+                lists1.append(value)
             else:
-                new_list2.append(value)
+                lists2.append(value)
 
-        new_list1.sort(key = custom_comparator)
-        new_list2.sort()
+        def sorter(element):
 
-        return new_list1 + new_list2
+            return dictionary[element]
+
+        lists2.sort(key = sorter)
+        lists1.sort()
+
+        return lists2 + lists1
+
+# class Solution:
+#     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        
+#         dictionary = {}
+
+#         for i in range(len(arr2)):
+#             dictionary[arr2[i]] = i
+
+#         lists = []
+        
+#         def custom_comparator(items):
+
+#             nonlocal lists
+#             nonlocal dictionary
+            
+#             if items in dictionary.keys():
+#                 return dictionary[items]
+            
+        
+#         new_list1 = []
+#         new_list2 = []
+
+#         for value in arr1:
+#             if value in arr2:
+#                 new_list1.append(value)
+#             else:
+#                 new_list2.append(value)
+
+#         new_list1.sort(key = custom_comparator)
+#         new_list2.sort()
+
+#         return new_list1 + new_list2
