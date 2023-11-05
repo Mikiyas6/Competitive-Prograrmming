f0f36c09f85101645629d09f4c756b6c341876fa
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-05 13:29:25 +0300

    Update Boats_To_Save_People.py

--- Boats_To_Save_People.py
+++ Boats_To_Save_People.py
@@ -2,20 +2,42 @@ class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         
         people.sort()
+
         i = 0
         j = len(people) - 1
-
-        counter = 0
+        total = 0
 
         while i < j:
 
             if people[i] + people[j] <= limit:
-                counter += 1
                 i += 1
-                j -= 1
             
-            else:
-                j -= 1
+            total += 1
+            j -= 1
 
-        return len(people) - counter
+        if i == j:
+            total += 1
+        
+        return total
+
+# class Solution:
+#     def numRescueBoats(self, people: List[int], limit: int) -> int:
+        
+#         people.sort()
+#         i = 0
+#         j = len(people) - 1
+
+#         counter = 0
+
+#         while i < j:
+
+#             if people[i] + people[j] <= limit:
+#                 counter += 1
+#                 i += 1
+#                 j -= 1
+            
+#             else:
+#                 j -= 1
+
+#         return len(people) - counter
             
