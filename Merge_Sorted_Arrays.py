7d80d9cd0a84989569c89c9addbf5c6bf0f5cb96
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-06 23:58:25 +0300

    Update Merge_Sorted_Arrays.py

--- Merge_Sorted_Arrays.py
+++ Merge_Sorted_Arrays.py
@@ -2,25 +2,34 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+
         """
 
-        for i in range(len(nums2)):
-            nums1[m+i] = nums2[i]
-       
-        for i in range(len(nums1)):
+        i = m-1
+        j = n-1
+        k = 0
+        length = len(nums1)
 
-            key = nums1[i]
-            j = i - 1
+        while i >= 0 and j >= 0:
 
-            while j >= 0 and key < nums1[j]:
+            if nums2[j] >= nums1[i]:
 
-                nums1[j+1] = nums1[j]
+                nums1[length-k-1] = nums2[j]
                 j -= 1
+            
+            elif nums2[j] < nums1[i]:
 
-            nums1[j+1] = key
+                nums1[length-k-1] = nums1[i]
+                i -= 1
+            
+            k += 1
         
-        class Solution:
-    
+        if i < 0:
+
+            nums1[:length-k] = nums2[:j+1]
+
+     #######################################################       
+
         # i = m - 1
         # j = n - 1
         # k = m + n - 1
@@ -36,8 +45,23 @@ class Solution:
 
         #     k -= 1
 
-        
-        
+    ##############################################################
+    
+        # nums1[m:] = nums2
+
+        # n = len(nums1)
+
+        # for i in range(1,n):
+
+        #     j = i - 1
+        #     key = nums1[i]
+
+        #     while j >= 0 and key < nums1[j]:
+
+        #         nums1[j+1] = nums1[j]
+        #         j -= 1
+            
+        #     nums1[j+1] = key
 
         
         
