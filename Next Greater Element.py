9d6859400e86b0f4afe053876fffa271d2654621
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-07 01:19:00 +0300

    Update Next Greater Element.py

--- Next Greater Element.py
+++ Next Greater Element.py
@@ -1,46 +1,27 @@
-class Solution(object):
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        stack = []
-        list_of_poped_values = []
-        dictionary = {}
-        array = []
-        for i in range(len(nums2)):
-            if (len(stack) == 0):
-                stack.append(nums2[i])
-            else:
-                while(len(stack) > 0):
-                    if( nums2[i] <= stack[-1]):
-                        stack.append(nums2[i])
-                        break
-                    elif (nums2[i] > stack[-1]):
-                        list_of_poped_values.append(stack.pop(-1))
-                        dictionary[nums2[i]] = list_of_poped_values 
-                if len(stack) == 0:
-                    stack.append(nums2[i])
-                list_of_poped_values = []
-        counter = 0
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        lists = []
+
         for i in range(len(nums1)):
-            for key,values in dictionary.items():
-                if nums1[i] in values:
-                    array.append(key)
-                    counter = 0
+
+            flag = False
+
+            start = nums2.index(nums1[i]) + 1
+
+            for j in range(start,len(nums2)):
+
+                if nums1[i] < nums2[j]:
+
+                    flag = True
+                    lists.append(nums2[j])
                     break
-                else:
-                    counter += 1
-            if counter == len(dictionary):
-                array.append(-1)
-                counter = 0
-        return array
-                    
-        
-
-        
-               
-
             
-                    
+            if not flag:
+                lists.append(-1)
+
+        
+        return lists
+
+
+
