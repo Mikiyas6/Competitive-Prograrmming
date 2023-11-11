5edd4f7694dd91a018e2031d5d458ccc4639229c
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-11 17:26:28 +0300

    Update Fizz_Buzz.py

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,17 +1,24 @@
-class Solution(object):
-    def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        lists = []
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        
+        output = []
+
         for i in range(1,n+1):
-            if i%3 == 0 and i%5 == 0:
-                lists.append("FizzBuzz")
-            elif (i%3 == 0):
-                lists.append("Fizz")
-            elif (i%5 == 0):
-                lists.append("Buzz")
+
+            if i % 3 == 0 and i % 5 == 0:
+
+                output.append("FizzBuzz")
+            
+            elif i%3 == 0:
+
+                output.append("Fizz")
+            
+            elif i%5 == 0:
+
+                output.append("Buzz")
+            
             else:
-                lists.append(str(i))
-        return lists
+
+                output.append(str(i))
+        
+        return output
