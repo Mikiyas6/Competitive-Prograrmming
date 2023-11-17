668afa99a598ed795fe92b3e9808c87e209f01d1
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-17 13:56:55 +0300

    Update Longest_Turbulent_SubArray.py

--- Longest_Turbulent_SubArray.py
+++ Longest_Turbulent_SubArray.py
@@ -1,55 +1,37 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
 
-        if not arr:
-            return 0
-
-        if len(arr) == 1:
-            return 1
-
+        previous_sign = ""
         l = 0
-
-        prev_sign = ""
-
         max_length = 0
 
-        for r in range(1,len(arr)):
+        if len(arr) == 1:
 
-            # Determine the current sign
+            return 1
+
+        for r in range(1,len(arr)):
 
             if arr[r-1] > arr[r]:
 
                 sign = ">"
-            
+
             elif arr[r-1] < arr[r]:
 
                 sign = "<"
-            
+
             else:
 
                 sign = "="
 
-            # Calculate the current length
+            if sign == previous_sign:
 
-            length = r - l + 1
-
-            # Shrinking the window size. The shrinking of the window depends on the sign you determined
-
-            if sign == "=": # If the sign is "=" then the window we want to start our window from the current value in the array
-
-                length -= 1
-                l = r
-
-            elif prev_sign == sign: # If the sign is simply different than before, then we want to start the window from r-1 value
-                
-                length -= 1
                 l = r - 1
 
-            max_length = max(max_length,length)
+            if sign == "=":
 
-            prev_sign = sign
-            
+                l = r
+
+            max_length = max(max_length,r-l+1)
+            previous_sign = sign
+
         return max_length
-
-        
-        
