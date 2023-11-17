6f77094e08b351f6610294f1ec7df2e9f0b7cb94
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-17 18:50:15 +0300

    Update Longest_Repeating_Character_Replacement.py

--- Longest_Repeating_Character_Replacement.py
+++ Longest_Repeating_Character_Replacement.py
@@ -22,4 +22,41 @@ class Solution:
         
         return res
 
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+        
+#         l = 0
+#         longest = 0
+#         hashmap = defaultdict(int)
+
+#         for r in range(len(s)):
+
+#             hashmap[s[r]] += 1
+
+#             max_value = max(hashmap.values())
+
+#             length = r - l + 1
+
+#             number_of_replaceable_chars = length - max_value
+
+#             while number_of_replaceable_chars > k:
+
+#                 hashmap[s[l]] -= 1
+
+#                 l += 1
+
+#                 max_value = max(hashmap.values())
+
+#                 length = r - l + 1
+
+#                 number_of_replaceable_chars = length - max_value
+  
+#             longest = max(longest,length)
+        
+#         return longest
+       
+
+
+
+
 
