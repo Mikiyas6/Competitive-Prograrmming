752c8a38f43f4ab1125bc81aa48373bf823abdd0
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-18 12:30:56 +0300

    Update Range_Sum_Query_Immutable.py

--- Range_Sum_Query_Immutable.py
+++ Range_Sum_Query_Immutable.py
@@ -1,18 +1,20 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
-        self.lists = [0] * len(self.nums)
-        accumulator = 0
 
-        for i in range(len(self.nums)):
-            accumulator += nums[i]
-            self.lists[i] = accumulator
-            
+        self.cumulative = 0
+        self.prefix_sum = [0] * (len(nums) + 1)
+
+        for index,value in enumerate(nums):
+
+            self.cumulative += value
+
+            self.prefix_sum[index+1] = self.cumulative
+
+
     def sumRange(self, left: int, right: int) -> int:
-        if left > 0:
-            return self.lists[right] - self.lists[left-1]
-        return self.lists[right]
+        
+        return self.prefix_sum[right+1] - self.prefix_sum[left]
 
 
 # Your NumArray object will be instantiated and called as such:
