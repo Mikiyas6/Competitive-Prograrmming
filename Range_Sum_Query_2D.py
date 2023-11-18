a9b602eb94c81842113f3e5e237070927aafbae9
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-18 13:38:13 +0300

    Update Range_Sum_Query_2D.py

--- Range_Sum_Query_2D.py
+++ Range_Sum_Query_2D.py
@@ -1,16 +1,22 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.old_matrix = matrix
-        row_length, column_length = len(self.old_matrix), len(self.old_matrix[0])
-        self.new_matrix = [[0]*(column_length+1) for i in range(row_length+1)]
-        for row in range(1,len(self.new_matrix)):
-            for column in range(1,len(self.new_matrix[0])):
-                self.new_matrix[row][column] = self.new_matrix[row-1][column] + self.new_matrix[row][column-1] - self.new_matrix[row - 1][column - 1] + self.old_matrix[row-1][column-1]
+
+        self.matrix = matrix
+        self.matrix_sum = [[0]*(len(matrix[0])+1) for _ in range(len(matrix)+1)]
+
+        for row in range(1,len(self.matrix_sum)):
+
+            for col in range(1,len(self.matrix_sum[0])):
+
+                self.matrix_sum[row][col] = self.matrix_sum[row-1][col] + self.matrix_sum[row][col-1] - (self.matrix_sum[row-1][col-1]) + self.matrix[row-1][col-1]
+
+
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.new_matrix[row2+1][col2+1] - self.new_matrix[row1][col2+1] - self.new_matrix[row2+1][col1] + self.new_matrix[row1][col1]
-    
         
+        return self.matrix_sum[row2+1][col2+1] - (self.matrix_sum[row1][col2+1] + self.matrix_sum[row2+1][col1]) + self.matrix_sum[row1][col1]
+        
+
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
