8a9b60ba48f40c5853884a27c45ab71ee53a5fe9
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-18 13:04:37 +0300

    Update Pivot_Index.py

--- Pivot_Index.py
+++ Pivot_Index.py
@@ -1,35 +1,18 @@
-class Solution(object):
-    def pivotIndex(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        sums = 0
-        prefix_sums_forward = []
-        prefix_sums_reversed = []
-        lists = []
-        for i in range(len(nums)):
-            sums += nums[i]
-            prefix_sums_forward.append(sums)
-        sums = 0
-        for j in range(-1,-len(nums)-1,-1):
-            sums += nums[j]
-            prefix_sums_reversed.append(sums)
-        for k in range(len(prefix_sums_reversed)):
-            lists.append(prefix_sums_reversed[-1])
-            prefix_sums_reversed.pop(-1)
-        prefix_sums_reversed = lists
-        # print(prefix_sums_forward)
-        # print(prefix_sums_reversed)
-        for z in range (len(prefix_sums_forward)):
-            if (prefix_sums_forward[z] == prefix_sums_reversed[z]):
-                return z
-        else:
-            return -1
-                
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        cumulative = 0
+        prefix_sum = [0] * (len(nums)+2)
 
-    
-            
+        for index,value in enumerate(nums):
 
-    
-            
+            cumulative += value
+            prefix_sum[index+1] = cumulative
+        
+        for r in range(1,len(prefix_sum)-1):
+
+            if prefix_sum[r-1] == prefix_sum[len(prefix_sum)-2] - prefix_sum[r]:
+
+                return r-1
+        
+        return -1
