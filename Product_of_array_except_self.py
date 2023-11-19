e9af5327a503fa3ae9841c22196f6731e4205410
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-19 22:39:27 +0300

    Update Product_of_array_except_self.py

--- Product_of_array_except_self.py
+++ Product_of_array_except_self.py
@@ -1,19 +1,33 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        left = []
+        right = []
+        output = []
+
+        cumulative = 1
+
+        for value in nums:
+
+            left.append(cumulative)
+            cumulative *= value
         
-        output = [0]*len(nums)
-        prefix = 1
-        postfix = 1
+        cumulative = 1
+        
+        for value in nums[::-1]:
+
+            right.append(cumulative)
+            cumulative *= value
+        
+        right = right[::-1]
 
         for i in range(len(nums)):
 
-            output[i] = prefix
-            prefix *= nums[i]
-        
-        for j in range(len(nums)-1,-1,-1):
-
-            output[j] *= postfix
-            postfix *= nums[j]
+            output.append(left[i]*right[i])
         
         return output
+
+
+
+
 
