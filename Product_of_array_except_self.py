e11a976fe8aff2cea03d0b4d6ff5b957b46fe418
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-19 22:40:36 +0300

    Update Product_of_array_except_self.py

--- Product_of_array_except_self.py
+++ Product_of_array_except_self.py
@@ -28,6 +28,28 @@ class Solution:
         return output
 
 
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+#         output = [0]*len(nums)
+#         prefix = 1
+#         postfix = 1
+
+#         for i in range(len(nums)):
+
+#             output[i] = prefix
+#             prefix *= nums[i]
+        
+#         for j in range(len(nums)-1,-1,-1):
+
+#             output[j] *= postfix
+#             postfix *= nums[j]
+        
+#         return output
+
+
+
+
 
 
 
