581ab2032f20f7e9800a62475daa066567b97fa3
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-20 00:17:13 +0300

    Update Subarray_Sums_Divisible_By_K.py

--- Subarray_Sums_Divisible_By_K.py
+++ Subarray_Sums_Divisible_By_K.py
@@ -1,16 +1,40 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
+
+        cumulative = 0
+        hashmap = defaultdict(int)
+        hashmap[0] = 1
+        count = 0
+
+        for value in nums:
+
+            cumulative += value
+            modulus = cumulative % k
+
+            if modulus in hashmap:
+
+                count += hashmap[modulus]
+            
+            hashmap[modulus] += 1
         
-        curr_sum = 0
-        res = 0
-        prefix_sum = {0:1}
+        return count
 
-        for i in range(len(nums)):
 
-            curr_sum += nums[i]
-            remainder = curr_sum % k
 
-            res += prefix_sum.get(remainder,0)
-            prefix_sum[remainder] = 1 + prefix_sum.get(remainder,0)
+
+# class Solution:
+#     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         
-        return res
+#         curr_sum = 0
+#         res = 0
+#         prefix_sum = {0:1}
+
+#         for i in range(len(nums)):
+
+#             curr_sum += nums[i]
+#             remainder = curr_sum % k
+
+#             res += prefix_sum.get(remainder,0)
+#             prefix_sum[remainder] = 1 + prefix_sum.get(remainder,0)
+        
+#         return res
