375dc990b2d8fdc2fc33e551c7f7e3e874d347f0
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-25 19:31:14 +0300

    Update Maximum_subarray.py

--- Maximum_subarray.py
+++ Maximum_subarray.py
@@ -1,19 +1,23 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
 
-        cumulative = 0
+        if not nums:
+
+            return 0
+        
         max_sum = float("-inf")
+        cumulative = 0
 
         for value in nums:
 
-            if cumulative < 0:
+            if cumulative <= 0:
 
                 cumulative = value
             
             else:
 
                 cumulative += value
-            
+        
             max_sum = max(max_sum,cumulative)
         
         return max_sum
