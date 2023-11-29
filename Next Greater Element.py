d4b7fd94a256343185893333140d1a6e16c6d678
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-11-29 13:33:45 +0300

    Update Next Greater Element.py

--- Next Greater Element.py
+++ Next Greater Element.py
@@ -1,33 +1,36 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-
-        hashmap = defaultdict(int)
-        output = [0] * len(nums1)
         
+        hashmap = defaultdict(int)
+
         for index,value in enumerate(nums1):
 
             hashmap[value] = index
         
         stack = []
-        
+        output = [0] * len(nums1)
+
         for value in nums2:
 
             while stack and value > stack[-1]:
 
                 removed = stack.pop()
-                output[hashmap[removed]] = value
-            
-            if value in nums1:
 
-                stack.append(value)
+                if removed in hashmap:
+
+                    output[hashmap[removed]] = value
+            
+            stack.append(value)
         
         for index,value in enumerate(output):
 
             if value == 0:
 
                 output[index] = -1
-        
+            
         return output
+        
+        
 
 
-
+        
