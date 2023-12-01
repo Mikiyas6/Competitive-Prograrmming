92b6fde2fa95f7777ce98b85c4d6ce1ab964506b
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-01 03:01:05 +0300

    Update BackSpace_String_Compare.py

--- BackSpace_String_Compare.py
+++ BackSpace_String_Compare.py
@@ -1,38 +1,69 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
+
+        string1 = ""
+        string2 = ""
         
-        stack = []
+        for letter in s:
 
-        for char in s:
+            if letter != "#":
 
-            print(stack)
-
-            if char == "#": 
-                if stack:
-                    stack.pop()
-            else:
-
-                stack.append(char)
-        
-        string = "".join(stack)
-
-
-        stack = []
-
-
-        for char in t:
-
-            if char == "#":
-                if stack:
-                    stack.pop()
+                string1 += letter
             
             else:
 
-                stack.append(char)
+                string1 = string1[:len(string1)-1]
+        
+        for letter in t:
+
+            if letter != "#":
+
+                string2 += letter
+            
+            else:
+
+                string2 = string2[:len(string2)-1]
+        
+        return True if string1 == string2 else False
+
+
+
+
+# class Solution:
+#     def backspaceCompare(self, s: str, t: str) -> bool:
+        
+#         stack = []
+
+#         for char in s:
+
+#             print(stack)
+
+#             if char == "#": 
+#                 if stack:
+#                     stack.pop()
+#             else:
+
+#                 stack.append(char)
+        
+#         string = "".join(stack)
+
+
+#         stack = []
+
+
+#         for char in t:
+
+#             if char == "#":
+#                 if stack:
+#                     stack.pop()
+            
+#             else:
+
+#                 stack.append(char)
                 
 
-        if string == "".join(stack):
-            return True
+#         if string == "".join(stack):
+#             return True
         
-        return False
+#         return False
 
