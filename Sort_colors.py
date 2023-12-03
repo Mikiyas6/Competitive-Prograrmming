6eae04d3f64dc6344c30936ec531d1d42379d816
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-03 15:54:53 +0300

    Update Sort_colors.py

--- Sort_colors.py
+++ Sort_colors.py
@@ -4,23 +4,59 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        zero_count, one_count, two_count = 0, 0, 0
+        n = len(nums)
 
-        for value in nums:
+        max_value = max(nums)
 
-            if value == 0:
+        length = max_value + 1
 
-                zero_count += 1
-            
-            elif value == 1:
+        frequency = [0] * length
 
-                one_count += 1
-            
-            else:
+        for i in range(n):
 
-                two_count += 1
+            frequency[nums[i]] += 1
         
-        sorted_nums = [0] * zero_count + [1] * one_count + [2] * two_count
+        output = [0] * len(nums)
+        output_length = 0
+        
+        for index,item in enumerate(frequency):
+
+            if item != 0:
+
+                width = item
+                value = index
+
+                output[output_length:output_length+width] = [value] * width
+                output_length += width
+        
+        for i in range(len(nums)):
+
+            nums[i] = output[i]
+        
+
+        # zero_count, one_count, two_count = 0, 0, 0
+
+        # for value in nums:
+
+        #     if value == 0:
+
+        #         zero_count += 1
+            
+        #     elif value == 1:
+
+        #         one_count += 1
+            
+        #     else:
+
+        #         two_count += 1
+        
+        # sorted_nums = [0] * zero_count + [1] * one_count + [2] * two_count
+
+        # for i in range(len(nums)):
+
+        #     nums[i] = sorted_nums[i]
+        
+        
 
         for i in range(len(nums)):
 
