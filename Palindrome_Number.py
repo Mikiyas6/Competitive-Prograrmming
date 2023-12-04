c4b084169163ea2c74df971222efc4188153c063
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-04 23:25:38 +0300

    Update Palindrome_Number.py

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -1,17 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         
-        string = str(x)
-
-        i = 0 
-        j = len(string) - 1
-
-        while i <= j:
-
-            if string[i] != string[j]:
-                return False
-            
-            i += 1
-            j -= 1
+        x = str(x)
         
-        return True
+        return True if x == x[::-1] else False
