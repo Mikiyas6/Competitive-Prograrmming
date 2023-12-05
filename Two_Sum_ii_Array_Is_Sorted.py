2bdb1a924db464ed0b7b89653117c92ed392e5d1
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-05 14:40:14 +0300

    Update Two_Sum_ii_Array_Is_Sorted.py

--- Two_Sum_ii_Array_Is_Sorted.py
+++ Two_Sum_ii_Array_Is_Sorted.py
@@ -1,22 +1,22 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        if len(numbers) < 2:
-            return []
-
         i = 0
         j = len(numbers) - 1
-        lists = []
 
         while i < j:
 
-            if numbers[i] + numbers[j] == target:
-                lists.extend([i+1,j+1])
+            total = numbers[i] + numbers[j]
+
+            if total < target:
+
                 i += 1
+            
+            elif total > target:
+
                 j -= 1
-            elif numbers[i] + numbers[j] > target:
-                j -= 1
+            
             else:
-                i += 1
+
+                return [i+1,j+1]
         
-        return lists
