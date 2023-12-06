1595d81f397210a50eed3f1c84d7dca84b6e0821
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-06 23:38:27 +0300

    Update Rotate_Array.py

--- Rotate_Array.py
+++ Rotate_Array.py
@@ -3,35 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = (k) % len(nums)
+        n = len(nums)
 
-        i = 0
-        j = len(nums) - 1
-
-        while i < j:
-
-            nums[i], nums[j] = nums[j], nums[i]
-
-            i += 1
-            j -= 1
+        k = k % n
         
-        i = 0
-        j = k - 1
-
-        while i < j:
-
-            nums[i], nums[j] = nums[j], nums[i]
-
-            i += 1
-            j -= 1
+        self.reverse(nums, 0, n - 1)
         
-        i = k
-        j = len(nums) - 1
+        self.reverse(nums, 0, k - 1)
+        
+        self.reverse(nums, k, n - 1)
 
-        while i < j:
+    def reverse(self, nums, start, end):
+      
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
 
-            nums[i], nums[j] = nums[j], nums[i]
 
-            i += 1
-            j -= 1
+
+        
         
