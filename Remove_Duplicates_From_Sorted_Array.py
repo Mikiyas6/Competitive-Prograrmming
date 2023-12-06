e999771c21824c854a76b6449b5585c6eba02cec
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-06 22:06:31 +0300

    Update Remove_Duplicates_From_Sorted_Array.py

--- Remove_Duplicates_From_Sorted_Array.py
+++ Remove_Duplicates_From_Sorted_Array.py
@@ -3,23 +3,15 @@ class Solution:
         
         i = 0
         j = i + 1
-        total = 1
 
         while j < len(nums):
 
-            if nums[i] != nums[j]:
+            if nums[j] != nums[i]:
 
                 nums[i+1] = nums[j]
+
                 i += 1
-                total += 1
-
-            j += 1
-
-        return total
-
-           
-
-
             
-
-
+            j += 1
+        
+        return i+1
