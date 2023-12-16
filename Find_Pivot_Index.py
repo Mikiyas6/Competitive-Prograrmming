5910538420f3ca1de81974f642c88d08eaa4f728
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-16 22:17:37 +0300

    Update Find_Pivot_Index.py

--- Find_Pivot_Index.py
+++ Find_Pivot_Index.py
@@ -1,20 +1,48 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         
+        n = len(nums)
+
+        prefix_sum = [0] * (n+2)
+
         cumulative = 0
-        prefix_sum = [0] * (len(nums)+1)
 
         for index,value in enumerate(nums):
 
             cumulative += value
+
             prefix_sum[index+1] = cumulative
         
-        last_element_index = len(prefix_sum) - 1
-        
-        for r in range(1,last_element_index+1):
+        for index,value in enumerate(prefix_sum[1:n+1]):
 
-            if prefix_sum[last_element_index] - prefix_sum[r] == prefix_sum[r-1]:
+            left = prefix_sum[index]
 
-                return r-1
+            right = prefix_sum[n] - value
+
+            if left == right:
+
+                return index
         
         return -1
+            
+
+# class Solution:
+#     def pivotIndex(self, nums: List[int]) -> int:
+        
+#         cumulative = 0
+#         prefix_sum = [0] * (len(nums)+1)
+
+#         for index,value in enumerate(nums):
+
+#             cumulative += value
+#             prefix_sum[index+1] = cumulative
+        
+#         last_element_index = len(prefix_sum) - 1
+        
+#         for r in range(1,last_element_index+1):
+
+#             if prefix_sum[last_element_index] - prefix_sum[r] == prefix_sum[r-1]:
+
+#                 return r-1
+        
+#         return -1
