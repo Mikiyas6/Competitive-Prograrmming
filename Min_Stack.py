1a239ef665d1c7d27c70a6dc8228502ab3597ae5
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-30 09:44:12 +0300

    Update Min_Stack.py

--- Min_Stack.py
+++ Min_Stack.py
@@ -1,33 +1,29 @@
-class MinStack(object):
+class MinStack:
 
     def __init__(self):
-        self.lists = []
-    def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-         """ 
-        self.lists.append(val)  
-    def pop(self):
-        """
-        :rtype: None
-        """
-        self.lists.pop()
-    def top(self):
-        """
-        :rtype: int
-        """
-        return self.lists[-1]
-    def getMin(self):
-        """
-        :rtype: int
-        """
-        return min(self.lists)
-
-
         
+        self.stack = []
+        self.min_stack = []
 
+    def push(self, val: int) -> None:
 
+        self.stack.append(val)
+        val = min(val,self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(val)
+
+    def pop(self) -> None:
+
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+
+        return self.min_stack[-1]
+    
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
