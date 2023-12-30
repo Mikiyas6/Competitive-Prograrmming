78623c82522b1dd03d484c00a403ee7e1bcf6f64
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2023-12-30 12:30:12 +0300

    Update Next Greater Element.py

--- Next Greater Element.py
+++ Next Greater Element.py
@@ -1,36 +1,27 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
+        monotonic_stack = []
+
+        result = [-1] * len(nums1)
+
         hashmap = defaultdict(int)
 
-        for index,value in enumerate(nums1):
+        for index, value in enumerate(nums1):
 
             hashmap[value] = index
         
-        stack = []
-        output = [0] * len(nums1)
-
         for value in nums2:
 
-            while stack and value > stack[-1]:
+            while monotonic_stack and value > monotonic_stack[-1]:
 
-                removed = stack.pop()
+                removed = monotonic_stack.pop()
 
-                if removed in hashmap:
-
-                    output[hashmap[removed]] = value
+                result[hashmap[removed]] = value
             
-            stack.append(value)
-        
-        for index,value in enumerate(output):
+            if value in hashmap:
 
-            if value == 0:
-
-                output[index] = -1
-            
-        return output
+                monotonic_stack.append(value)
         
-        
+        return result
 
-
-        
