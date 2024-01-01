3cd48e3823c1b1f53edd8120572d56293dc03e31
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-01-01 11:57:51 +0300

    Update Partition_Labels.py

--- Partition_Labels.py
+++ Partition_Labels.py
@@ -1,94 +1,28 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-
+        
         hashmap = defaultdict(int)
 
-        for index,letter in enumerate(s):
+        for index, value in enumerate(s):
 
-            hashmap[letter] = index
+            hashmap[value] = index
         
-        counter = 0
-
-        end = 0
-
         output = []
-        
-        for index,letter in enumerate(s):
 
-            end = max(end,hashmap[letter])
+        farthest = 0
+
+        counter = 0
+        
+        for index, value in enumerate(s):
+
+            farthest = max(farthest,hashmap[value])
 
             counter += 1
 
-            if index == end:
+            if farthest == index:
 
                 output.append(counter)
 
                 counter = 0
         
         return output
-
-
-        # hashmap = defaultdict(list)
-
-        # for index,value in enumerate(s):
-
-        #     hashmap[value].append(index)
-    
-        # for letter,indexes in hashmap.items():
-
-        #     if len(indexes) < 2:
-
-        #         start = indexes[0]
-            
-        #     else:
-
-        #         start = indexes[0]
-        #         end = indexes[-1]
-
-        #         hashmap[letter] = [start,end]
-        
-        # starting = hashmap[s[0]][0]
-
-        # if len(hashmap[s[0]]) > 1:
-        
-        #     ending = end =  hashmap[s[0]][1]
-        
-        # else:
-
-        #     ending = starting
-
-        # output = []
-
-        # for indexes in hashmap.values():
-
-        #     start = indexes[0]
-
-        #     if start > ending:
-
-        #         output.append(ending - starting + 1)
-                
-        #         starting = start
-
-        #         if len(indexes) > 1:
-        #             ending = indexes[1]
-            
-        #     if len(indexes) > 1:
-
-        #         end = indexes[1]
-
-        #         ending = max(ending,end)
-            
-        #     else:
-
-        #         ending = max(ending,start)
-        
-        # output.append(ending - starting + 1)
-        
-        # return output
-
-            
-
-
-                
-        
-
