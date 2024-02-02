ccb95447b4eb1182a6b5711a53ee0a842cf23472
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-02 21:32:05 +0300

    Update Longest_Common_Prefix.py

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -1,22 +1,25 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-
         if not strs:
             return ""
 
         strs.sort()
-
+        
         first_str = strs[0]
         last_str = strs[-1]
 
+        common_prefix = self.findCommonPrefix(first_str, last_str)
+
+        return common_prefix
+
+    def findCommonPrefix(self, first_str: str, last_str: str) -> str:
         common_prefix = ""
-        
-        for i in range(len(first_str)):
-            if i < len(last_str) and first_str[i] == last_str[i]:
+        for i in range(min(len(first_str), len(last_str))):
+            if first_str[i] == last_str[i]:
                 common_prefix += first_str[i]
             else:
                 break
-
         return common_prefix
+
             
 
