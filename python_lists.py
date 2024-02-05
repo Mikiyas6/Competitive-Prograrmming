59ce1d51b308c55fda561170dca10fda8cfa2ff8
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-05 23:26:24 +0300

    Update python_lists.py

--- python_lists.py
+++ python_lists.py
@@ -1,31 +1,41 @@
 if __name__ == '__main__':
     N = int(input())
+    
     lists = []
+    
     for i in range(N):
-        string = input()
-        if "append" in string:
-            listing = string.split()
-            lists.append(int(listing[1]))
-            listing = []
-        elif "print" in string:
+        
+        things_to_do = list(input().split())
+        
+        command = things_to_do[0]
+        
+        if command == "insert":
+            
+            lists.insert(int(things_to_do[1]),int(things_to_do[2]))
+            
+        elif command == "print":
+            
             print(lists)
-        elif "remove" in string:
-            listing = string.split()
-            lists.remove(int(listing[1]))
-            listing = []
-        elif "insert" in string:
-            listing = string.split()
-            lists.insert(int(listing[1]),int(listing[2]))
-            listing = []
-        elif "sort" in string:
-            lists = sorted(lists)
-        elif "pop" in string:
+        
+        elif command == "remove":
+            
+            lists.remove(int(things_to_do[1]))
+        
+        elif command == "append":
+            
+            lists.append(int(things_to_do[1]))
+        
+        elif command == "sort":
+            
+            lists.sort()
+        
+        elif command == "pop":
+            
             lists.pop()
-        elif "reverse" in string:
-            i = 0
-            j = len(lists) - 1
-            while i < j:
-                lists[i], lists[j] = lists[j], lists[i]
-                i += 1
-                j -= 1
+            
+        elif command == "reverse":
+            
+            lists.reverse()
+        
+        
     
