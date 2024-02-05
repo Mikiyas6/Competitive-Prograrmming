02d6a66c45337b2c4e4765847834fd5a4216920d
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-05 22:15:52 +0300

    Create Decrypt_String_From_Alphabet_To_Integer_Mapping.py

---
+++ Decrypt_String_From_Alphabet_To_Integer_Mapping.py
@@ -0,0 +1,49 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        
+        string = "abcdefghijklmnopqrstuvwxyz"
+
+        hashmap = defaultdict(str)
+
+        for index in range(1,len(string)+1):
+
+            if index < 10:
+
+                hashmap[str(index)] = string[index-1]
+
+            else:
+
+                hashmap[str(index) + "#"] = string[index-1]
+        
+        new_string = ""
+
+        i = len(s) - 1
+        
+        while i >= 0:
+
+            if s[i-2] != '#':
+                
+                if s[i] == "#":
+
+                    new_string += hashmap[s[i-2:i+1]]
+
+                    i -= 3
+                
+                else:
+                    
+                    new_string += hashmap[s[i]]
+
+                    i -= 1
+
+            else:
+
+                new_string += hashmap[s[i]] + hashmap[s[i-1]]
+
+                i -= 2
+        
+        return new_string[::-1]
+
+
+
+
+        
