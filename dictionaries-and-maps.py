fc13e7fee7c321f4af54b410feb199f9a3c64c8c
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-08 23:35:56 +0300

    Create dictionaries-and-maps.py

---
+++ dictionaries-and-maps.py
@@ -0,0 +1,29 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from collections import defaultdict
+
+n = int(input())
+
+hashmap = defaultdict(int)
+
+for _ in range(n):
+    
+    name, phone_number = list(input().split())
+    hashmap[name] = phone_number
+    
+while True:
+    try:
+        
+        name = input().strip()
+        
+        if not name:
+            break
+        
+        if name in hashmap:
+            print("{}={}".format(name,hashmap[name]))
+        else:
+            print("Not found")
+        
+    except EOFError:
+        # Handle the end of input (EOF) gracefully
+        break
