93576bbdae825473d371a00915ae8b93bbcf7d8f
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-08 12:39:42 +0300

    Create find-k-th-smallest-pair-distance.py

---
+++ find-k-th-smallest-pair-distance.py
@@ -0,0 +1,24 @@
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        low = 0
+        high = nums[-1] - nums[0]
+        
+        while low < high:
+            mid = (low + high) // 2
+            count = 0
+            left = 0
+            
+            for right in range(len(nums)):
+                while nums[right] - nums[left] > mid:
+                    left += 1
+                count += right - left
+            
+            # Adjust the range based on the count of pairs
+            if count < k:
+                low = mid + 1
+            else:
+                high = mid
+        
+        return low
