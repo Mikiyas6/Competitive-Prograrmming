2724e7ea4af9f3e4e47d3d82ee52ceda8e726d60
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-10 02:27:02 +0300

    Update Shuffle_The_Array.py

--- Shuffle_The_Array.py
+++ Shuffle_The_Array.py
@@ -2,15 +2,16 @@ class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         
         i = 0
-        j = n 
+        j = n
+
         output = []
 
-        while j < 2*n:
+        while j < len(nums):
 
-            output.append(nums[i])
-            output.append(nums[j])
-
+            output.extend([nums[i],nums[j]])
+        
             i += 1
+
             j += 1
         
         return output
