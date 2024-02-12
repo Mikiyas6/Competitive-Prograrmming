5c72edda69e9719e0970713d5d54127ea430b537
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-13 01:32:15 +0300

    Time: 38 ms (97.71%) | Memory: 16.6 MB (61.47%) - LeetSync

--- 805-escape-the-ghosts/escape-the-ghosts.py
+++ 805-escape-the-ghosts/escape-the-ghosts.py
@@ -1,56 +1,27 @@
-# class Solution:
-#     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-
-#         max_row = 0
-#         max_column = 0
-
-#         for row, column in ghosts + [target]:
-
-#             max_row = max(max_row,row)
-#             max_column = max(max_column,column)
-
-#         size = max(max_row,max_column)
-
-#         def inbound(coordinate):
-
-#             return coordinate[0] > -1 and coordinate[0] < size and coordinate[1] > -1 and coordinate[1] < size
-        
-#         def dfs(coordinate,counter):
-
-#             if not inbound(coordinate) or coordinate == target:
-
-#                 return counter
-            
-#             if inbound(coordinate):
-
-#                 direction_1 = dfs([coordinate[0]-1,coordinate[1]], counter+1)
-#                 direction_2 = dfs([coordinate[0]+1,coordinate[1]], counter+1)
-#                 direction_3 = dfs([coordinate[0],coordinate[1]-1], counter+1)
-#                 direction_4 = dfs([coordinate[0],coordinate[1]+1], counter+1)
-            
-#             return min([direction_1,direction_2,direction_3,direction_4])
-        
-#         min_distance = float("inf")
-
-#         for coordinate in ghosts:
-
-#             print(coordinate)
-
-#             min_distance = min(min_distance,dfs(coordinate,0))
-        
-#         mine = dfs([0,0],counter,0)
-
-#         return True if mine < min_distance else False
-
 class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-        for ghost in ghosts:
-            distance_apart_x = abs(target[0] - ghost[0])
-            distance_apart_y = abs(target[1] - ghost[1])
-            distance_apart = distance_apart_x + distance_apart_y
-            target_distance = abs(target[0]) + abs(target[1])
-            
-            if (distance_apart <= target_distance):
+
+        my_coordinate = [0,0]
+
+        distance_in_x_direction = abs(my_coordinate[0] - target[0])
+
+        distance_in_y_direction = abs(my_coordinate[1] - target[1])
+
+        distance_from_me_to_target = distance_in_x_direction + distance_in_y_direction
+
+        for coordinate in ghosts:
+
+            distance_in_x_direction = abs(coordinate[0] - target[0])
+
+            distance_in_y_direction = abs(coordinate[1] - target[1])
+
+            distance_from_ghost_to_target = distance_in_x_direction + distance_in_y_direction
+
+            if distance_from_ghost_to_target <= distance_from_me_to_target:
+
                 return False
-            
+        
         return True
+
+            
+
