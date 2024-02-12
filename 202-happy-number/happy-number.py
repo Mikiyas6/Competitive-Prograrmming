dc2406982f0467f6592938d27695105ffd6e9fdb
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-12 20:44:57 +0300

    Time: 38 ms (60.48%) | Memory: 16.6 MB (65.34%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        def find_square(number:int) -> int:
+
+            answer = 0
+
+            while number > 0:
+
+                rem = number % 10
+
+                answer += rem ** 2
+
+                number = number // 10
+            
+            return answer
+        
+        if n == 1:
+
+            return True
+        
+        if n <= 1:
+
+            return False
+
+        slow = n
+        fast = find_square(find_square(n))
+
+        while slow != fast :
+
+            slow = find_square(slow)
+            fast = find_square(find_square(fast))
+
+            if slow == 1:
+
+                return True
