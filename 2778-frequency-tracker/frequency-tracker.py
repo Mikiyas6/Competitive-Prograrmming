f07e9ad37d9221c0d5352589d74a0f02b53bb6c2
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-16 14:20:32 +0300

    Time: 494 ms (50.67%) | Memory: 73.9 MB (18.22%) - LeetSync

---
+++ 2778-frequency-tracker/frequency-tracker.py
@@ -0,0 +1,81 @@
+class FrequencyTracker:
+
+    def __init__(self):
+        
+        self.hashmap = defaultdict(int)
+        self.frequency = defaultdict(list)
+
+    def add(self, number: int) -> None:
+
+        ######## For the frequency Hashmap ########
+
+            frequency = self.hashmap[number]
+
+            if frequency != 0:
+
+                list_of_numbers = self.frequency[frequency]
+
+                if number in list_of_numbers:
+
+                    list_of_numbers.remove(number)
+
+                self.frequency[frequency] = list_of_numbers
+
+                if not self.frequency[frequency]:
+
+                    del self.frequency[frequency]
+
+            self.frequency[frequency+1].append(number)
+
+            ####### For the main hashmap ###########
+            
+            self.hashmap[number] += 1
+
+    def deleteOne(self, number: int) -> None:
+
+        if str(number).isdigit() and number in self.hashmap:
+
+            ######## For the frequency Hashmap ########
+
+            frequency = self.hashmap[number]
+
+            if frequency != 0:
+
+                print(self.frequency,self.frequency[frequency],number)
+
+                list_of_numbers = self.frequency[frequency]
+
+                if number in list_of_numbers:
+
+                    list_of_numbers.remove(number)
+
+                self.frequency[frequency] = list_of_numbers
+
+            if not self.frequency[frequency]:
+
+                del self.frequency[frequency]
+
+            if frequency - 1 != 0:
+
+                self.frequency[frequency-1].append(number)
+
+            ####### For the main hashmap ###########
+            
+            self.hashmap[number] -= 1
+            
+            if not self.hashmap[number]:
+
+                del self.hashmap[number]
+
+    def hasFrequency(self, frequency: int) -> bool:
+
+        if frequency in self.frequency:
+        
+           return True
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
