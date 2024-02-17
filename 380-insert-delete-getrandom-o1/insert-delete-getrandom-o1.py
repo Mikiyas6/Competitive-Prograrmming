9c4d5de934aa0924ef0ebbe62894483f35dedef4
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-02-17 22:53:44 +0300

    Time: 467 ms (19.27%) | Memory: 63.9 MB (87.42%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,66 +1,83 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.dict = defaultdict(int)
-        self.list = []
+        self.set = set()
 
     def insert(self, val: int) -> bool:
-        if val in self.dict:
+        if val in self.set:
             return False
-        self.dict[val] = len(self.list)
-        self.list.append(val)
+        self.set.add(val)
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.dict:
+        if val not in self.set:
             return False
-        idx = self.dict[val]
-        last_val = self.list[-1]
-        self.list[idx] = last_val
-        self.dict[last_val] = idx
-        self.list.pop()
-        del self.dict[val]
+        self.set.remove(val)
         return True
 
     def getRandom(self) -> int:
-        return random.choice(self.list)
+        return random.choice(list(self.set))
+#     def __init__(self):
+#         self.dict = defaultdict(int)
+#         self.list = []
 
-    # def __init__(self):
-    #     self.set = []
-    #     self.index = 0
-    #     self.size = 0
+#     def insert(self, val: int) -> bool:
+#         if val in self.dict:
+#             return False
+#         self.dict[val] = len(self.list)
+#         self.list.append(val)
+#         return True
 
-    # def insert(self, val: int) -> bool:
+#     def remove(self, val: int) -> bool:
+#         if val not in self.dict:
+#             return False
+#         idx = self.dict[val]
+#         last_val = self.list[-1]
+#         self.list[idx] = last_val
+#         self.dict[last_val] = idx
+#         self.list.pop()
+#         del self.dict[val]
+#         return True
+
+#     def getRandom(self) -> int:
+#         return random.choice(self.list)
+
+#     # def __init__(self):
+#     #     self.set = []
+#     #     self.index = 0
+#     #     self.size = 0
+
+#     # def insert(self, val: int) -> bool:
         
-    #     if val not in self.set:
+#     #     if val not in self.set:
 
-    #         self.set.append(val)
+#     #         self.set.append(val)
 
-    #         self.size += 1
+#     #         self.size += 1
 
-    #         return True
+#     #         return True
         
-    # def remove(self, val: int) -> bool:
+#     # def remove(self, val: int) -> bool:
 
-    #     if val in self.set:
+#     #     if val in self.set:
 
-    #         self.set.remove(val)
+#     #         self.set.remove(val)
 
-    #         self.size -= 1
+#     #         self.size -= 1
 
-    #         return True
+#     #         return True
         
-    # def getRandom(self) -> int:
+#     # def getRandom(self) -> int:
 
-    #     result = self.set[self.index % self.size]
+#     #     result = self.set[self.index % self.size]
 
-    #     self.index += 1
+#     #     self.index += 1
 
-    #     return result
+#     #     return result
 
 
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+# # Your RandomizedSet object will be instantiated and called as such:
+# # obj = RandomizedSet()
+# # param_1 = obj.insert(val)
+# # param_2 = obj.remove(val)
+# # param_3 = obj.getRandom()
