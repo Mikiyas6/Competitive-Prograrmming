8cd9a39ed59dba4c615f1e941a4107eca287a76c
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-03-05 23:53:07 +0300

    Time: 62 ms (31.64%) | Memory: 16.8 MB (29.55%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,33 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        
+        s = ""
+        counter = 1
+        if len(chars) == 1:
+            return 1
+        else:
+            i = 0
+            pointer = 0
+            while i < len(chars):
+                if (i == len(chars) -1):
+                    if pointer == i:
+                        s += str(chars[i])
+                    else:
+                        s += str(chars[i]) + str(i - pointer + 1)
+                        chars [pointer:i+1] = list(s)
+                    break
+                elif chars[i] == chars [i+1]:
+                    pointer -= 1
+                else:
+                    if pointer == i:
+                        s += str(chars[i])
+                    else:
+                        s += str(chars[i]) + str(i - pointer + 1)
+                        chars [pointer:i+1] = list(s)
+                        pointer += 1
+                        i = pointer
+                    s = ""
+                i += 1
+                pointer += 1
+            return len(chars)  
+            
