a0a6a9f89edff2bac42c4a2e2de15436e70533a9
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-03-17 18:14:22 +0300

    Time: 216 ms (64.58%) | Memory: 20.4 MB (28.94%) - LeetSync

---
+++ 475-heaters/heaters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        
+        houses.sort()
+        heaters.sort()
+        
+        max_radius = 0
+        
+        for house in houses:
+            closest_heater_idx = bisect.bisect_left(heaters, house)
+
+            if closest_heater_idx == 0:
+                radius = heaters[0] - house
+            elif closest_heater_idx == len(heaters):
+                radius = house - heaters[-1]
+            else:
+                radius = min(heaters[closest_heater_idx] - house, house - heaters[closest_heater_idx - 1])
+            
+            max_radius = max(max_radius, radius)
+        
+        return max_radius
