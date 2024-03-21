b5b055a8c5a31e19ab7e57a8fe808abe9baf1220
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-03-21 17:09:45 +0300

    Time: 609 ms (16.32%) | Memory: 22.8 MB (14.74%) - LeetSync

---
+++ 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.py
@@ -0,0 +1,68 @@
+class Solution:
+    def longestBeautifulSubstring(self, word: str) -> int:
+
+        n = len(word)
+
+        if n < 5:
+
+            return 0
+
+        longest = 0
+
+        hashset = set()
+
+        hashmap = {"a":["a"],"e":["e","a"],"i":["e","i"],"o":["i","o"],"u":["o","u"]}
+
+        counter = 1
+
+        r = 1
+
+        while r < n:
+
+            while r < n and word[r-1] in hashmap[word[r]]:
+
+                counter += 1
+
+                hashset.add(word[r-1])
+                hashset.add(word[r])
+
+                r += 1
+            
+            if word[r-1] != "u":
+
+                counter = 1
+            
+            if word[r-1] == "u" and len(hashset) < 5:
+
+                print(hashset)
+
+                counter = 1
+            
+            longest = max(longest,counter)
+
+            counter = 1
+
+            r += 1
+
+            hashset = set()
+        
+        return longest if longest >= 5 else 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+        
