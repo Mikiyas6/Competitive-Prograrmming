85222bb1415f65fe59571a37a17775761ab49a7f
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-03-22 17:16:21 +0300

    Time: 333 ms (74.67%) | Memory: 20.6 MB (17.47%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,27 +1,87 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
 
-        def atMostK(nums, k):
-            count = defaultdict(int)
-            left = 0
-            result = 0
+        def at_most_k(nums: List[int], k: int) -> int:
 
-            for right in range(len(nums)):
-                if count[nums[right]] == 0:
+            hashmap = defaultdict(int)
+
+            counter = 0
+
+            l = 0
+
+            for r in range(len(nums)):
+
+                if hashmap[nums[r]] == 0:
+
                     k -= 1
-                count[nums[right]] += 1
+                
+                hashmap[nums[r]] += 1
 
                 while k < 0:
-                    count[nums[left]] -= 1
-                    if count[nums[left]] == 0:
+
+                    hashmap[nums[l]] -= 1
+
+                    if hashmap[nums[l]] == 0:
+
                         k += 1
-                    left += 1
+                    
+                    l += 1
+                
+                counter += r - l + 1
+            
+            return counter
+        
+        return at_most_k(nums,k) - at_most_k(nums,k-1)
 
-                result += right - left + 1
 
-            return result
 
-        return atMostK(nums, k) - atMostK(nums, k - 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def atMostK(nums, k):
+        #     count = defaultdict(int)
+        #     left = 0
+        #     result = 0
+
+        #     for right in range(len(nums)):
+        #         if count[nums[right]] == 0:
+        #             k -= 1
+        #         count[nums[right]] += 1
+
+        #         while k < 0:
+        #             count[nums[left]] -= 1
+        #             if count[nums[left]] == 0:
+        #                 k += 1
+        #             left += 1
+
+        #         result += right - left + 1
+
+        #     return result
+
+        # return atMostK(nums, k) - atMostK(nums, k - 1)
         
         # hashmap = defaultdict(int)
 
