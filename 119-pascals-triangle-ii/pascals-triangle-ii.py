4173ff58a3cb512c7b52cb083c510cb1c26da59d
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-03-30 16:15:21 +0300

    Time: 39 ms (27.85%) | Memory: 16.5 MB (37.44%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -5,16 +5,44 @@ class Solution:
         
         matrix = [[0]*n for _ in range(n)]
 
-        for row in range(n):
+        def fun(matrix,row,col):
 
-            for col in range(row+1):
+            if row == n:
 
-                if row == col or col == 0:
+                return matrix
+            
+            if row == col:
 
-                    matrix[row][col] = 1
-                
-                else:
+                matrix[row][col] = 1
 
-                    matrix[row][col] = matrix[row-1][col-1] + matrix[row-1][col]
+                return fun(matrix,row+1,0)
+            
+            if col == 0:
+
+                matrix[row][col] =  1
+
+            else:
+
+                matrix[row][col] = matrix[row-1][col-1] + matrix[row-1][col] 
+            
+            return fun(matrix,row,col + 1)
         
-        return matrix[-1]
+        return fun(matrix,0,0)[-1]
+
+        # n = rowIndex + 1
+        
+        # matrix = [[0]*n for _ in range(n)]
+
+        # for row in range(n):
+
+        #     for col in range(row+1):
+
+        #         if row == col or col == 0:
+
+        #             matrix[row][col] = 1
+                
+        #         else:
+
+        #             matrix[row][col] = matrix[row-1][col-1] + matrix[row-1][col]
+        
+        # return matrix[-1]
