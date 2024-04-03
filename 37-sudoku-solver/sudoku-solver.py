3cb93480f9bce7f9b70d88b102bc95114c32d33e
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-03 16:48:35 +0300

    Time: 300 ms (38.76%) | Memory: 16.5 MB (80.61%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.py
@@ -0,0 +1,43 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        n = 9
+
+        def is_safe(row, col, value):
+            for i in range(n):
+                if board[i][col] == value:
+                    return False
+                
+            for i in range(n):
+                if board[row][i] == value:
+                    return False
+            
+            start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+            if any(board[start_row + r][start_col + c] == value for r in range(3) for c in range(3)):
+                return False
+            
+            return True
+
+        def sudoku(row, col, board):
+            if row == n: 
+                return True
+            
+            if col == n:
+                return sudoku(row + 1, 0, board)
+                
+            if board[row][col] == ".": 
+                for value in range(1, 10):
+                    if is_safe(row, col, str(value)): 
+                        board[row][col] = str(value) 
+                        if sudoku(row, col + 1, board):
+                            return True
+                board[row][col] = "."
+                return False
+            
+            else:
+                return sudoku(row, col + 1, board)
+
+        sudoku(0, 0, board)
