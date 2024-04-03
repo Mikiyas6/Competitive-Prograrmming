d70371f87e4f89732469653f99b4a6c1853d87c0
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-03 19:14:13 +0300

    Time: 5325 ms (23.51%) | Memory: 16.6 MB (26.43%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,20 +1,40 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        def dfs(i, j, index):
-            if index == len(word):
+
+        m, n = len(board), len(board[0])
+
+        def is_safe(row, col):
+            return 0 <= row < m and 0 <= col < n and not journey[row][col]
+
+        def fun(row, col, word):
+
+            if not word:
                 return True
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[index]:
-                return False
 
-            temp = board[i][j]
-            board[i][j] = '#'  
-            found = dfs(i + 1, j, index + 1) or dfs(i - 1, j, index + 1) or dfs(i, j + 1, index + 1) or dfs(i, j - 1, index + 1)
-            board[i][j] = temp 
-            return found
+            if is_safe(row, col) and board[row][col] == word[0]:
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0] and dfs(i, j, 0):
+                journey[row][col] = True
+
+                up = fun(row-1, col, word[1:])
+
+                down = fun(row+1, col, word[1:])
+
+                left = fun(row, col-1, word[1:])
+
+                right = fun(row, col+1, word[1:])
+
+                journey[row][col] = False
+
+                if (up or down or left or right):
                     return True
+
+            return False
+
+        journey = [[False] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if fun(i, j, word):
+                    return True
+
         return False
