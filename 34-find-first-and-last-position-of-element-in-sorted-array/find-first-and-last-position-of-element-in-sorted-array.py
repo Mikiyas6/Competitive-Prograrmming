47ecc062f7a49423753b3d03feeb5b6a5317ec19
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-06 11:43:12 +0300

    Time: 74 ms (69.28%) | Memory: 17.8 MB (84.33%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,41 +1,45 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
 
-        def binary_search(s,e,target,side):
-    
-            ans = -1
+        def binary_search(s,e,ans,position):
+
+            if s > e:
+
+                return ans
             
-            while s <= e:
-                
-                mid = s + (e-s)//2
-                
-                if target > nums[mid]:
-                
-                    s = mid + 1
-            
-                elif target < nums[mid]:
-                    
+            mid = s + (e-s)//2
+
+            if target > nums[mid]:
+
+                s = mid + 1
+
+            elif target < nums[mid]:
+
+                e = mid - 1
+
+            else:
+
+                ans = mid
+
+                if position == "first":
+
                     e = mid - 1
                 
-                else: 
-                    
-                    ans = mid
-                    
-                    if side == "first":
-                        
-                        e = mid - 1
-                    
-                    else:
-                        
-                        s = mid + 1
-                
-            return ans
+                else:
 
-        first = binary_search(0,len(nums)-1,target,"first")
+                    s = mid + 1
+            
+            return binary_search(s,e,ans,position)
+        
+        ans = -1
 
-        last = binary_search(0,len(nums)-1,target,"last")
+        s, e = 0, len(nums) - 1
+        
+        first = binary_search(s,e,ans,"first")
+
+        last = binary_search(s,e,ans,"end")
 
         return [first,last]
+            
 
 
-        
