8cacaa06fdace7a30e29040ef62f04bd98b3fe58
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-09 16:29:34 +0300

    Time: 870 ms (5.00%) | Memory: 45 MB (5.66%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -1,86 +1,75 @@
-# # Definition for singly-linked list.
-# # class ListNode:
-# #     def __init__(self, val=0, next=None):
-# #         self.val = val
-# #         self.next = next
-# class Solution:
-#     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-#         dummy = ListNode(-1)
-
-#         while head:
-
-#             current = dummy
-#             prev = current
-
-#             while current:
-#                 if head.val < current.val:
-
-#                     node = ListNode(head.val)
-                    
-#                     if prev == current:
-#                         temp = prev
-#                         node.next = prev.next
-#                         prev.next = node
-#                     else:
-
-#                         prev.next = node
-#                         node.next = current
-
-#                     break
-                
-#                 prev = current 
-#                 current = current.next
-            
-#             if not current:
-
-#                 node = ListNode(head.val)
-#                 prev.next = node
-            
-#             head = head.next
-        
-#         return dummy.next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        
-        # Find the middle of the linked list
-        middle = self.find_middle(head)
-        second_half = middle.next
-        middle.next = None  # Break the list into two halves
-        
-        # Recursively sort the two halves
-        left = self.sortList(head)
-        right = self.sortList(second_half)
-        
-        # Merge the sorted halves
-        return self.merge(left, right)
-    
-    def find_middle(self, head):
-        slow = head
-        fast = head.next
-        
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        return slow
-    
-    def merge(self, l1, l2):
-        dummy = ListNode()
-        current = dummy
-        
-        while l1 and l2:
-            if l1.val < l2.val:
-                current.next = l1
-                l1 = l1.next
-            else:
-                current.next = l2
-                l2 = l2.next
+
+        def fun(head):
+
+            if not head or not head.next:
+
+                return head
             
-            current = current.next
+            left,right = get_middle(head) 
+            left = fun(left)
+            right = fun(right)
+
+            return merge(left, right)
         
-        current.next = l1 or l2
+        def get_middle(head):
+
+            if not head or not head.next:
+                return head, None
+
+            # Use fast-slow pointers to find the middle of the linked list
+            slow, fast = head, head.next
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+
+            # Split the linked list into two halves
+            second_half = slow.next
+            slow.next = None
+
+            return head, second_half
+
+        def merge(left,right):
+
+            dummy = ListNode(-1)
+            current = dummy
+
+            def function(current,left,right):
+
+                if not left:
+
+                    current.next = right
+
+                    return current
+                
+                if not right:
+
+                    current.next = left
+
+                    return current
+                
+                if left.val <= right.val:
+
+                    value = left.val
+                    left = left.next
+                
+                else:
+
+                    value = right.val
+                    right = right.next
+                
+                Node = ListNode(value)
+                
+                current.next = function(Node,left,right)
+
+                return current
+            
+            return function(current,left,right).next
         
-        return dummy.next
+        return fun(head)
