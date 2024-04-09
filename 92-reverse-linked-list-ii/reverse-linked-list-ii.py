d7beec82706fb83f8d8da7af2ae1aea63276cfd4
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-09 21:00:40 +0300

    Time: 44 ms (6.74%) | Memory: 16.6 MB (62.46%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -7,15 +7,12 @@ class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
 
         if not head or not head.next:
-
             return head
-        
+
         def fun(head):
-
             if not head or not head.next:
-
                 return head
-            
+
             second_part = head.next
             head.next = None
 
@@ -28,69 +25,144 @@ class Solution:
             current = head
 
             while current.next:
-
                 current = current.next
-            
+
             current.next = first_node
 
             return head
-        
+
         end_up = left - 2
 
         if right - left <= 0:
-
-            return head 
+            return head
 
         if left == 1:
-
             first_part = None
             second_half = head
         else:
             current = head
-
-            for i in range(end_up):
-
+            for _ in range(end_up):
                 current = current.next
-            
             second_half = current.next
             current.next = None
             first_part = head
-            
-        end_up = right-left
+
+        end_up = right - left
         current = second_half
-
-        for i in range(end_up):
-
+        for _ in range(end_up):
             current = current.next
 
         last_part = current.next
         current.next = None
         middle = second_half
-        
+
         middle_reversed = fun(middle)
 
         current = first_part
-
         if not first_part:
-
             current = middle_reversed
 
         while current.next:
-
             current = current.next
-        
+
         if not first_part:
-
             current.next = last_part
-
             return middle_reversed
-        
+
         current.next = middle_reversed
 
         while current.next:
-
             current = current.next
-        
+
         current.next = last_part
 
         return first_part
+
+        # if not head or not head.next:
+
+        #     return head
+        
+        # def fun(head):
+
+        #     if not head or not head.next:
+
+        #         return head
+            
+        #     second_part = head.next
+        #     head.next = None
+
+        #     first_node = head
+
+        #     second_part_reversed = fun(second_part)
+
+        #     head = second_part_reversed
+
+        #     current = head
+
+        #     while current.next:
+
+        #         current = current.next
+            
+        #     current.next = first_node
+
+        #     return head
+        
+        # end_up = left - 2
+
+        # if right - left <= 0:
+
+        #     return head 
+
+        # if left == 1:
+
+        #     first_part = None
+        #     second_half = head
+        # else:
+        #     current = head
+
+        #     for i in range(end_up):
+
+        #         current = current.next
+            
+        #     second_half = current.next
+        #     current.next = None
+        #     first_part = head
+            
+        # end_up = right-left
+        # current = second_half
+
+        # for i in range(end_up):
+
+        #     current = current.next
+
+        # last_part = current.next
+        # current.next = None
+        # middle = second_half
+        
+        # middle_reversed = fun(middle)
+
+        # current = first_part
+
+        # if not first_part:
+
+        #     current = middle_reversed
+
+        # while current.next:
+
+        #     current = current.next
+        
+        # if not first_part:
+
+        #     current.next = last_part
+
+        #     return middle_reversed
+        
+        # current.next = middle_reversed
+
+        # while current.next:
+
+        #     current = current.next
+        
+        # current.next = last_part
+
+        # return first_part
