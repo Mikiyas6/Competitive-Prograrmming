871fa33e0c69dfdba2de129cc7dddc1518f226d6
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-10 01:18:07 +0300

    Time: 290 ms (54.93%) | Memory: 32 MB (70.93%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,58 +5,81 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return False
-        elif not head.next:
-            return True
-        elif not head.next.next:
-            if head.val == head.next.val:
-                return True
-            else:
-                return False
-        else:
-            # Finding the middle node
-            slow = head
-            fast = head
-            while fast and fast.next:
-                previous = slow
-                slow = slow.next
-                fast = fast.next.next
-            # Finding the length of the Linked List
+
+        def fun(head):
+
+            prev = None
             current = head
-            counter = 0
+
             while current:
-                counter += 1
-                current = current.next
-            # Deciding our path based on the length of the Linked List
-            new_head = None
-            if counter %2 == 0:
-                new_head = slow
-                previous.next = None
-            else:
-                new_head = slow.next
-                previous.next = None
-            # Reversing the new Linked List
-            previous_node = None
-            current_node = new_head
-            while current_node:
-                if not previous_node and not current_node.next:
-                    previous_node = current_node
-                    break
-                next_node = current_node.next
-                current_node.next = previous_node
-                previous_node = current_node
-                current_node = next_node
-            new_head = previous_node
-            # Comparing corresponding values inside the two Linked Lists
-            current1 = head
-            current2 = new_head
-            while current1 and current2:
-                if current1.val != current2.val:
-                    return False
-                current1 = current1.next
-                current2 = current2.next
-            return True
+                next_node = current.next
+                current.next = prev
+                prev = current
+                current = next_node
+
+            return prev
+
+        # def fun(head):
+
+        #     if not head or not head.next:
+
+        #         return head
+            
+        #     second_part = head.next
+        #     head.next = None
+        #     first_node = head
+
+        #     reversed_second_part = fun(second_part)
+
+        #     current = reversed_second_part
+
+        #     while current.next:
+
+        #         current = current.next
+            
+        #     current.next = first_node
+
+        #     return reversed_second_part
+
+        if not head or not head.next:
+
+            return head
+        
+        # Break the linkedlist in half
+
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+
+            slow = slow.next
+            fast = fast.next.next
+        
+        second_half = slow.next
+        slow.next = None
+        first_half = head
+
+        # Reverse the second half
+        reversed_second_half = fun(second_half)
+
+        # Check the whether the two parts are equal or not. If they are then it's palindromic
+        ptr1 = first_half
+        ptr2 = reversed_second_half
+
+        while ptr1 and ptr2:
+
+            if ptr1.val != ptr2.val:
+
+                return False
+        
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+        
+        return True
+
+        
+
+
 
 
     
