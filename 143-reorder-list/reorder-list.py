3af1125b0a0a84fcfb81e7f9bcb9398e08b6553b
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-10 14:37:11 +0300

    Time: 2118 ms (5.07%) | Memory: 24.7 MB (14.10%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -9,7 +9,11 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
 
-        def fun(head): #To reverse a list
+        if not head or not head.next:
+
+            return head
+        
+        def fun(head):
 
             if not head or not head.next:
 
@@ -31,11 +35,7 @@ class Solution:
 
             return reversed_second_part
 
-        if not head or not head.next:
-
-            return head
-        
-        # Break the linkedlist in half
+        #Finding the middle node and break the linkedlist in half
 
         slow = head
         fast = head.next
@@ -50,14 +50,19 @@ class Solution:
         first_half = head
 
         # Reverse the second half
+
         reversed_second_half = fun(second_half)
 
-        ptr1 = head
+        # Re ordering everythin in place
+
+        ptr1 = first_half
         ptr2 = reversed_second_half
 
         while ptr1 and ptr2:
+
             next_ptr1 = ptr1.next
             next_ptr2 = ptr2.next
+
             ptr1.next = None
             ptr2.next = None
 
@@ -67,48 +72,6 @@ class Solution:
             ptr1 = next_ptr1
             ptr2 = next_ptr2
         
-        # combine one element from each half
-        # ptr1 = first_half
-        # ptr2 = reversed_second_half
-
-        # dummy = ListNode(-1)
-        # current = dummy
-
-        # flag = True
-
-        # while ptr1 and ptr2:
-
-        #     if flag:
-
-        #         value = ptr1.val
-        #         ptr1 = ptr1.next
-        #         flag = False
-            
-        #     else:
-
-        #         value = ptr2.val
-        #         ptr2 = ptr2.next
-        #         flag = True
-            
-        #     Node = ListNode(value)
-        #     current.next = Node
-        #     current = current.next
-        
-        # if not ptr1:
-
-        #     current.next = ptr2
-        
-        # else:
-
-        #     current.next = ptr1
-        
-        # linked_list = dummy.next
-        # print(linked_list)
-
-        
 
 
-
-
-        
         
