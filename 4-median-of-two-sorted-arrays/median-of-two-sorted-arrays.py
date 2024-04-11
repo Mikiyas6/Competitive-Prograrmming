bc7b87eaebdcab3f2633051c6840f2c73aaf7e81
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-11 21:11:27 +0300

    Time: 90 ms (13.91%) | Memory: 18.3 MB (5.18%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,29 +1,52 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
+        merged = []
+        m,n = len(nums1), len(nums2)
+        ptr1, ptr2 = 0, 0
 
-        x, y = len(nums1), len(nums2)
-        low, high = 0, x
+        def fun(merged,ptr1,ptr2):
 
-        while low <= high:
-            partitionX = (low + high) // 2
-            partitionY = (x + y + 1) // 2 - partitionX
+            if ptr1 >= m:
+                
+                merged.extend(nums2[ptr2:])
 
-            maxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
-            minX = float('inf') if partitionX == x else nums1[partitionX]
-            maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
-            minY = float('inf') if partitionY == y else nums2[partitionY]
+                return merged
+            
+            if ptr2 >= n:
 
-            if maxX <= minY and maxY <= minX:
-                if (x + y) % 2 == 0:
-                    return (max(maxX, maxY) + min(minX, minY)) / 2
-                else:
-                    return float(max(maxX, maxY))
-            elif maxX > minY:
-                high = partitionX - 1
+                merged.extend(nums1[ptr1:])
+
+                return merged
+            
+            if nums1[ptr1] <= nums2[ptr2]:
+
+                value = nums1[ptr1]
+                ptr1 += 1
+            
             else:
-                low = partitionX + 1
 
-        raise ValueError("Input arrays are not sorted.")
+                value = nums2[ptr2]
+                ptr2 += 1
+            
+            merged.append(value)
+
+            return fun(merged,ptr1,ptr2)
+
+        merged = fun(merged,ptr1,ptr2)
+
+        mid = (m+n-1)//2
+
+        if (m + n) %2 == 0:
+
+            return (merged[mid] + merged[mid+1])/2
+        
+        else:
+
+            return merged[mid]
+            
+
+
+
+
+                
