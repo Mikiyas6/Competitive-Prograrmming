1497e05a88d8fa961abd4e72634f245b1fa3ce7c
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-11 16:27:18 +0300

    Time: 50 ms (82.59%) | Memory: 20.4 MB (55.12%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -3,53 +3,32 @@ class MinStack:
     def __init__(self):
         
         self.stack = []
-        # self.min_stack = []
-        # self.ptr = 0
+        self.min_stack = []
 
     def push(self, val: int) -> None:
-        
+
         self.stack.append(val)
 
-        # if not self.min_stack:
-        #     self.min_stack.append(val)
-
-        # else:   
-
-        #     self.min_stack.append(0)
-        #     key = val
-        #     j = len(self.min_stack) - 2
-
-        #     while key > self.min_stack[j] and j >= 0:
-
-        #         self.min_stack[j+1] = self.min_stack[j]
-        #         j -= 1
+        if self.min_stack:
+            val = min(self.min_stack[-1],val)
+        
+        self.min_stack.append(val)
             
-        #     self.min_stack[j+1] = key
-        #     self.ptr += 1
-
     def pop(self) -> None:
-
-        if self.stack:
         
-            value = self.stack.pop()
+        self.stack.pop()
+        self.min_stack.pop()
 
-            # if value == self.min_stack[self.ptr]:
-
-            #     self.min_stack.pop()
-            #     self.ptr -= 1
-                
     def top(self) -> int:
-
-        if self.stack:
         
-            return self.stack[-1]
+        return self.stack[-1]
 
     def getMin(self) -> int:
         
-        if self.stack:
+        return self.min_stack[-1]
+        
 
-            return min(self.stack)
-            # return self.min_stack[self.ptr]
+
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
