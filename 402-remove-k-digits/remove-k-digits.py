ab5a2ebd2d4cccf307cd8b4145b229749afba355
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-13 14:53:59 +0300

    Time: 70 ms (22.58%) | Memory: 18 MB (31.17%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.py
+++ 402-remove-k-digits/remove-k-digits.py
@@ -1,22 +1,26 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        
+
         stack = []
-        removed = 0
-        
-        for digit in num:
-            while stack and stack[-1] > digit and removed < k:
+
+        for char in num:
+
+            while k > 0 and stack and char < stack[-1]:
+
+                k -= 1
                 stack.pop()
-                removed += 1
-            stack.append(digit)
+            
+            stack.append(char)
         
-        # If k is not exhausted, remove the remaining digits from the end
-        while removed < k:
-            stack.pop()
-            removed += 1
-        
-        # Remove leading zeroes
-        while stack and stack[0] == '0':
+        if k > 0:
+
+            stack = stack[:len(stack)-k]
+
+        while stack and stack[0] == "0":
+
             stack.pop(0)
         
-        return ''.join(stack) if stack else '0'
+        return "".join(stack) if stack else "0"
+
+
+        
