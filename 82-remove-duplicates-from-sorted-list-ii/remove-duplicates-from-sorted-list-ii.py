9f40769a553697de77539f4e43dcdc0da9e2d7ad
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-13 03:08:47 +0300

    Time: 39 ms (62.10%) | Memory: 16.7 MB (14.03%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -6,39 +6,41 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        visited = set()
-        
         if not head or not head.next:
 
             return head
-
+        
         dummy = ListNode(-1)
         current = dummy
-        
-        ptr1 = head
-        ptr2 = head.next
 
-        while ptr2:
+        def fun(current,ptr1,ptr2,visited):
 
-            if ptr1.val == ptr2.val:
-                visited.add(ptr1.val)
-                ptr1 = ptr1.next.next
-                if not ptr1:
-                    return dummy.next
-                ptr2 = ptr2.next.next
-            
-            else:
+            if not ptr2:
 
                 if ptr1.val not in visited:
-                    Node = ListNode(ptr1.val)
-                    current.next = Node
-                    current = current.next
-                visited.add(ptr1.val)
-                ptr1 = ptr1.next
-                ptr2 = ptr2.next
 
-        if ptr1.val not in visited:  
-            Node = ListNode(ptr1.val)
-            current.next = Node
-        return dummy.next
+                    Node = ListNode(ptr1.val)
+
+                    current.next = Node
+                
+                return current
             
+            if ptr1.val != ptr2.val:
+
+                if ptr1.val not in visited:
+
+                    Node = ListNode(ptr1.val)
+                    visited.add(ptr1.val)
+                    current.next = fun(Node,ptr1.next,ptr2.next,visited)
+
+                    return current
+
+            visited.add(ptr1.val)
+            return fun(current,ptr1.next,ptr2.next,visited)
+
+        return fun(current,head,head.next,set()).next
+
+
+
+
+        
