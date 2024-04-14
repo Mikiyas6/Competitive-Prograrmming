d63838381956909c4e03b45c01791df492f4c52e
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-14 18:55:53 +0300

    Time: 47 ms (6.17%) | Memory: 16.5 MB (67.28%) - LeetSync

--- 86-partition-list/partition-list.py
+++ 86-partition-list/partition-list.py
@@ -11,7 +11,11 @@ class Solution:
             return head
 
         def fun(left, right, head, x):
-            if not head:
+            if not head.next:
+                if head.val < x:
+                    left.next = ListNode(head.val)
+                else:
+                    right.next = ListNode(head.val)
                 return left.next, right.next
 
             if head.val < x:
@@ -31,91 +35,3 @@ class Solution:
         current.next = dummy_right.next
 
         return dummy_left.next
-
-
-
-
-        
-    # def get_middle(list2):
-
-        #     if not list2 or not list2.next:
-
-        #         return list2
-
-        #     slow = list2
-        #     fast = list2.next
-
-        #     while fast and fast.next:
-
-        #         slow = slow.next
-        #         fast = fast.next.next
-
-        #     return slow
-        
-        # def sort(list2):
-
-        #     if not list2 or not list2.next:
-
-        #         return list2
-            
-        #     middle = get_middle(list2)
-        #     right_half = middle.next
-        #     middle.next = None
-        #     left_half = list2
-
-        #     left = sort(left_half)
-        #     right = sort(right_half)
-
-        #     merged = merge(left,right)
-
-        #     return merged
-
-        # def merge(list1,list2):
-            
-        #     dummy = ListNode(-1)
-        #     current = dummy
-
-        #     def fun(current,list1,list2): 
-
-        #         if not list1:
-
-        #             current.next = list2
-                
-        #             return current
-                
-        #         if not list2:
-
-        #             current.next = list1
-
-        #             return current
-                
-        #         if list1.val <= list2.val:
-
-        #             value = list1.val
-        #             list1 = list1.next
-                
-        #         else:
-
-        #             value = list2.val
-        #             list2 = list2.next
-                
-        #         Node = ListNode(value)
-
-        #         current.next = fun(Node,list1,list2)
-
-        #         return current
-            
-        #     return fun(current,list1,list2).next
-        
-        # current = head
-
-        # while current.val != x:
-
-        #     current = current.next
-
-        # list2 = current.next
-        # list2 = sort(list2)
-        # current.next = None
-        # list1 = head
-
-        # return merge(list1,list2)
