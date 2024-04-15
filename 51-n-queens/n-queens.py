802e76dec9a9b0d9065a23aad15022dc6b706f78
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-16 02:16:59 +0300

    Time: 111 ms (11.15%) | Memory: 17.1 MB (17.53%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,18 +1,27 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+
         board = [["."]*n for _ in range(n)]
+
         configurations = []
 
-        def is_safe(board, row, col):
-            for i in range(row):
-                if board[i][col] == "Q":
+        def is_safe(board,row,col):
+            diagonal_right = min(row,n-col-1)
+            for i in range(1,diagonal_right+1):
+                if board[row-i][col+i] == "Q":
                     return False
-                if col-row+i >= 0 and board[i][col-row+i] == "Q":
+            
+            diagonal_left = min(row,col)
+            for i in range(1,diagonal_left+1):
+                if board[row-i][col-i] == "Q":
                     return False
-                if col+row-i < n and board[i][col+row-i] == "Q":
-                    return False
-            return True
 
+            for i in range(1, row + 1):
+                if board[row-i][col] == "Q":
+                    return False
+                
+            return True
+        
         def fun(board, row):
             if row == n:
                 # Append a copy of the board, not the board itself
@@ -22,9 +31,11 @@ class Solution:
             for col in range(n):
                 if is_safe(board, row, col):
                     board[row][col] = "Q"
-                    fun(board, row + 1)
+                    fun([row[:] for row in board], row + 1)  # Create a deep copy of the board
                     board[row][col] = "."
 
         fun(board, 0)
 
         return configurations
+        
+        
