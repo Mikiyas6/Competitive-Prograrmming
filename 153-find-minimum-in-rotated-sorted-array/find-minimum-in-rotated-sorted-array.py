6ce8fa7bf6d93523131d290b31231bf32624672a
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-16 00:10:01 +0300

    Time: 38 ms (84.53%) | Memory: 16.9 MB (73.26%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -2,31 +2,34 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         
         s, e = 0, len(nums) - 1
+        n = len(nums)
 
-        def find_pivot(s,e):
+        def fun(s,e):
 
             if s > e:
 
                 return -1
             
             mid = s + (e-s)//2
-            
-            if mid + 1 < len(nums) and nums[mid] > nums[mid+1]:
 
-                return mid 
+            if mid + 1 < n and nums[mid] > nums[mid+1]:
+
+                return mid
             
             if nums[mid] < nums[mid-1]:
 
-                return mid - 1
+                return mid-1
             
             if nums[s] >= nums[mid]:
 
-                e = mid - 1
+                e = mid-1
             
-            else:
+            elif nums[s] < nums[mid]:
 
-                s = mid + 1
+                s = mid
             
-            return find_pivot(s,e)
-        
-        return nums[(find_pivot(s,e)+1)%len(nums)]
+            return fun(s,e)
+
+        pivot = fun(s,e)
+
+        return nums[pivot+1]
