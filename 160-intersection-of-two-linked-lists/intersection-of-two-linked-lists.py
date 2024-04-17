d63bcd3ea6507dbeea41326e13d617b620cf4de0
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-17 23:34:41 +0300

    Time: 81 ms (57.44%) | Memory: 27 MB (74.31%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -7,33 +7,33 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
 
-        current = headA
-        size_A = 0 
+        def find_length(head):
 
-        while current:
-            size_A += 1
-            current = current.next
-        
-        size_B = 0
-        current = headB
+            current = head
+            size = 0
 
-        while current:
+            while current:
 
-            size_B += 1
-            current = current.next
+                size += 1
+                current = current.next
+            
+            return size
+
+        size_A = find_length(headA)
+        size_B = find_length(headB)
 
         if size_A > size_B:
 
-            end_up = size_A-size_B
             current1 = headA
             current2 = headB
         
         else:
-            
-            end_up = size_B-size_A
+        
             current1 = headB
             current2 = headA
-            
+
+        end_up = abs(size_A-size_B)
+
         for i in range(end_up):
 
             current1 = current1.next
@@ -44,37 +44,3 @@ class Solution:
             current2 = current2.next
         
         return current1
-        
-
-        # current = headA
-
-        # while current.next:
-
-        #     current = current.next
-        
-        # current.next = headB
-        
-        # slow = headA
-        # fast = headA
-
-        # while fast and fast.next:
-
-        #     slow = slow.next
-        #     fast = fast.next.next
-
-        #     if slow == fast:
-
-        #         break
-        
-        # if not fast or not fast.next:
-
-        #     return None
-        
-        # slow = headA
-
-        # while slow != headA:
-
-        #     slow = slow.next
-        #     fast = fast.next
-        
-        # return slow
