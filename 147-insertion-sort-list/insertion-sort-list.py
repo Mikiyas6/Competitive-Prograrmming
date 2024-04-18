68c50e6aa7fa7a900986ec4217d78be3e1d1fe5a
Mikiyas6 <110767281+Mikiyas6@users.noreply.github.com>
2024-04-18 20:31:25 +0300

    Time: 431 ms (31.67%) | Memory: 17.9 MB (100.00%) - LeetSync

--- 147-insertion-sort-list/insertion-sort-list.py
+++ 147-insertion-sort-list/insertion-sort-list.py
@@ -1,53 +1,3 @@
-# # Definition for singly-linked list.
-# # class ListNode:
-# #     def __init__(self, val=0, next=None):
-# #         self.val = val
-# #         self.next = next
-# class Solution:
-#     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-#         if not head or not head.next:
-            
-#             return head
-        
-#         unsorted_part = head.next
-#         head.next = None
-#         sorted_part = head
-
-#         def fun(sorted_part,unsorted_part):
-
-#             if not unsorted_part:
-
-#                 return sorted_part
-            
-#             unsorted_second_part = unsorted_part.next
-#             unsorted_part.next = None
-#             second_node = unsorted_part
-
-#             if second_node.val < sorted_part.val:
-
-#                 second_node.next = sorted_part
-#                 sorted_part = second_node
-
-#             else:
-
-#                 current = sorted_part
-#                 if not current.next:
-#                     sorted_part.next = second_node
-#                 else:
-#                     while current.next:
-
-#                         if second_node.val <= current.next.val:
-
-#                             broken_part = current.next
-#                             current.next = second_node.val
-#                             second_node.next = broken_part
-
-#                             break
-            
-#             return fun(sorted_part,unsorted_second_part)
-
-#         return fun(sorted_part,unsorted_part)
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -75,18 +25,16 @@ class Solution:
                 sorted_part = second_node
             else:
                 current = sorted_part
-                if not current.next:
-                    sorted_part.next = second_node
-                else:
-                    while current.next:
-                        if second_node.val <= current.next.val:
-                            broken_part = current.next
-                            current.next = second_node
-                            second_node.next = broken_part
-                            break
-                        current = current.next
-                    else:
+                while current.next:
+                    if second_node.val <= current.next.val:
+                        broken_part = current.next
                         current.next = second_node
+                        second_node.next = broken_part
+                        break
+                    current = current.next
+                
+                else:
+                    current.next = second_node
 
             return fun(sorted_part, unsorted_second_part)
 
