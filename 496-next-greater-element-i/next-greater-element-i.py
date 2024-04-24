47de15d034e3d06ac93035cb77587beeadb6b766
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-04-24 17:35:14 +0300

    Time: 49 ms (58.46%) | Memory: 16.8 MB (43.95%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -2,25 +2,21 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         hashmap = defaultdict(int)
+        stack = []
 
         for index, value in enumerate(nums1):
-
             hashmap[value] = index
         
-        stack = []
-        result = [-1]*len(nums1)
-
+        result = [-1] * len(nums1)
+        
         for value in nums2:
 
             while stack and value > stack[-1]:
-
                 popped = stack.pop()
                 index = hashmap[popped]
                 result[index] = value
             
             if value in hashmap:
-
                 stack.append(value)
         
         return result
-            
