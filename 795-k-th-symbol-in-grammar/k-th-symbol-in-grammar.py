41625d3eecf8813e5b3e422c6d9515a085f08746
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-05 13:59:17 +0300

    Time: 43 ms (8.87%) | Memory: 16.5 MB (91.77%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -5,11 +5,9 @@ class Solution:
             # Base case
             if n == 1:
                 return 0
-            
-            # Recursively calculate the value of the parent node
+                
             parent_node_value = fun(n - 1, (k + 1) // 2)
-            
-            # Determine the value of the k-th element based on the parent node value
+
             if parent_node_value == 0:
                 return 0 if k % 2 == 1 else 1
             else:
