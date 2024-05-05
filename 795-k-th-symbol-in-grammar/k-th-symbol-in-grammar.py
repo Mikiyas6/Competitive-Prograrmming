f2fc024e37d9a858f14defb7d24aaac241baf35c
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-05 13:59:30 +0300

    Time: 29 ms (90.73%) | Memory: 16.6 MB (50.81%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -2,10 +2,10 @@ class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
 
         def fun(n,k):
-            # Base case
+            
             if n == 1:
                 return 0
-                
+
             parent_node_value = fun(n - 1, (k + 1) // 2)
 
             if parent_node_value == 0:
