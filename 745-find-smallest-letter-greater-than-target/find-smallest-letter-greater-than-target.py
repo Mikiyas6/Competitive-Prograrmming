a06fb49fc08765bd77c1d0a73974344dff1be6b3
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-09 15:20:33 +0300

    Time: 110 ms (29.79%) | Memory: 16.7 MB (98.91%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -1,24 +1,23 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         
-        def ceil(s,e,target):
+        def find_ceil(s,e,target):
     
-            if s > e:
+            while s <= e:
                 
-                return s % len(letters)
-            
-            mid = (s+(e-s)//2)
-            
-            if target < letters[mid]:
+                mid = s + (e-s)//2
                 
-                e = mid-1
+                if target < letters[mid]:
+                    e = mid - 1
+                else:
+                    s = mid + 1
             
-            else:
-                
-                s = mid+1
-            
-            return ceil(s,e,target)
+            return s
         
-        return letters[ceil(0,len(letters)-1,target)]
+        n = len(letters)
+        index = find_ceil(0,n-1,target)
+        if index < 0 or index >= n:
+            index = 0
+        return letters[index]
         
 
