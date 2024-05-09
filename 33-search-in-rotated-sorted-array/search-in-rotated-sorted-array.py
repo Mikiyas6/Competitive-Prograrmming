41c7a3393bfa472880908d9aa551913114d1e54a
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-09 23:25:30 +0300

    Time: 35 ms (93.81%) | Memory: 16.9 MB (93.54%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,64 +1,59 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
 
-        s, e = 0, len(nums)-1
-        
-        def pivot(s,e):
+        def binary_search(s,e,target):
 
             if s > e:
-
-                return -1
-            
-            mid = s+(e-s)//2
-
-            if mid + 1 < len(nums) and nums[mid] > nums[mid+1]:
-
-                return mid
-            
-            if nums[mid] < nums[mid-1]:
-
-                return mid - 1
-            
-            if nums[s] >= nums[mid]:
-
-                e = mid - 1
-            
-            else:
-
-                s = mid
-            
-            return pivot(s,e)
-        
-        def binary_search(s,e):
-
-            if s > e:
-
                 return -1
             
             mid = s + (e-s)//2
 
-            if nums[mid] == target:
+            if target == nums[mid]:
+                return mid                
+            elif target < nums[mid]:
+                e = mid-1
+            else:
+                s = mid+1
+            
+            return binary_search(s,e,target)
+                  
+        def find_pivot(s,e):
 
+            if s > e:
+                return -1
+            
+            mid = s + (e-s)//2
+
+            if mid+1 < n and nums[mid] > nums[mid+1]:
                 return mid
             
-            if target > nums[mid]:
-
-                s = mid + 1
+            if nums[mid] < nums[mid-1]:
+                return mid-1
             
+            if nums[s] >= nums[mid]:
+                e = mid-1
             else:
-
-                e = mid - 1
+                s = mid+1
             
-            return binary_search(s,e)
-        
-        pivot = pivot(s,e)
+            return find_pivot(s,e)
+         
+        n = len(nums)
+        s, e = 0, n-1
 
-        if pivot < 0:
+        pivot = find_pivot(s,e)
 
-            pivot += len(nums)
+        if pivot == -1:
+            normal = binary_search(s,e,target)
+            return normal
         
         if nums[pivot] == target:
-
             return pivot
         
-        return max(binary_search(s,pivot),binary_search(pivot+1,e))
+        left = binary_search(s,pivot-1,target)
+        right = binary_search(pivot+1,e,target)
+
+        return max(left,right)
+
+
+
+        
