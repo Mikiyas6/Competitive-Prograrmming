42e5eef735dd2982100096d1ac1a838b3f0152fe
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-10 23:54:47 +0300

    Time: 35 ms (89.14%) | Memory: 17 MB (9.43%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,28 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+        n = len(citations)
+        citations.sort()
+        
+        def binary_search(s,e):
+
+            if s > e:
+                return n-s
+            
+            mid = s + (e-s)//2
+            length = n-mid
+
+            if citations[mid] == length:
+                return length
+
+            elif citations[mid] < length:
+                s = mid + 1
+
+            else:
+                e = mid - 1
+            
+            return binary_search(s, e)
+        
+        return binary_search(0,n-1)
+
+        
