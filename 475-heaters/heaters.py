8bcc6314706ef7df66a1b11066d869091b8aee74
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-10 20:45:06 +0300

    Time: 215 ms (70.09%) | Memory: 20.3 MB (24.88%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -1,6 +1,7 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
-        
+
+        n = len(heaters)
         houses.sort()
         heaters.sort()
         
@@ -11,7 +12,7 @@ class Solution:
 
             if closest_heater_idx == 0:
                 radius = heaters[0] - house
-            elif closest_heater_idx == len(heaters):
+            elif closest_heater_idx == n:
                 radius = house - heaters[-1]
             else:
                 radius = min(heaters[closest_heater_idx] - house, house - heaters[closest_heater_idx - 1])
