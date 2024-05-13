0892c7b974aa51e5fd74c34132b3db8ea870b60a
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-13 22:33:18 +0300

    Time: 41 ms (78.46%) | Memory: 18.4 MB (88.70%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
@@ -7,21 +7,47 @@
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
 
-        def BFS(root,level):
+        def BFS():
 
-            if not root:
-                return
+            queue = deque()
+            queue.append(root)
+            result = []
+
+            while queue:
+
+                level = len(queue)
+                level_values = []
+
+                for _ in range(level):
+
+                    node = queue.popleft()
+                    level_values.append(node.val)
+
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+                
+                result.append(level_values)
             
-            if len(result) == level:
-                result.append([])
+            return result
+                    
+        # def BFS(root,level):
+
+        #     if not root:
+        #         return
             
-            result[level].append(root.val)
-            BFS(root.left,level+1)
-            BFS(root.right,level+1)
-            return 
+        #     if len(result) == level:
+        #         result.append([])
+            
+        #     result[level].append(root.val)
+        #     BFS(root.left,level+1)
+        #     BFS(root.right,level+1)
+        #     return 
         
-        result = []
-        BFS(root,0)
+        # result = []
+        # BFS(root,0)
+        result = BFS()
         average = []
 
         for level in result:
