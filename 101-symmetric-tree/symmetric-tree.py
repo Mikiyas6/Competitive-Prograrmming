745f54f942d6c0244af8c1803febc45fcabfab55
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-15 00:12:38 +0300

    Time: 36 ms (62.31%) | Memory: 16.6 MB (64.27%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -7,47 +7,63 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         
-        def BFS(root):
+        # def BFS(root):
 
-            if not root:
+        #     if not root:
+        #         return False
+            
+        #     queue = deque([root])
+            
+        #     while queue:
+
+        #         level = len(queue)
+        #         nodes = []
+
+        #         for _ in range(level):
+
+        #             node = queue.popleft()
+
+        #             if not node:
+        #                 nodes.append(None)
+        #             else:
+        #                 nodes.append(node.val)
+
+        #                 if node.left:
+        #                     queue.append(node.left)
+        #                 else:
+        #                     queue.append(None)
+
+        #                 if node.right:
+        #                     queue.append(node.right)
+        #                 else:
+        #                     queue.append(None)
+                
+        #         i, j = 0, len(nodes)-1
+
+        #         while i <= j:
+
+        #             if nodes[i] != nodes[j]:
+        #                 return False
+                    
+        #             i += 1
+        #             j -= 1
+            
+        #     return True
+                        
+        # return BFS(root)
+
+        if not root:
+            return root
+
+        def fun(left_node,right_node):
+
+            if not left_node and not right_node:
+                return True
+            if not left_node and right_node:
+                return False
+            if not right_node and left_node:
                 return False
             
-            queue = deque([root])
-            
-            while queue:
-
-                level = len(queue)
-                nodes = []
-
-                for _ in range(level):
-
-                    node = queue.popleft()
-
-                    if not node:
-                        nodes.append(None)
-                    else:
-                        nodes.append(node.val)
-
-                        if node.left:
-                            queue.append(node.left)
-                        else:
-                            queue.append(None)
-
-                        if node.right:
-                            queue.append(node.right)
-                        else:
-                            queue.append(None)
-                
-                i, j = 0, len(nodes)-1
-
-                while i <= j:
-
-                    if nodes[i] != nodes[j]:
-                        return False
-                    
-                    i += 1
-                    j -= 1
-            
-            return True
-                        
-        return BFS(root)
+            return left_node.val == right_node.val and fun(left_node.left,right_node.right) and fun(left_node.right,right_node.left)
+        
+        return fun(root.left,root.right)
