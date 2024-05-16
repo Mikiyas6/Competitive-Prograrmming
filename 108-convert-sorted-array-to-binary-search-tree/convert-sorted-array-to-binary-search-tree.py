1cb3c44082d061394d83997b594a23cee167a331
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-16 17:10:11 +0300

    Time: 48 ms (88.96%) | Memory: 17.9 MB (12.10%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -7,43 +7,9 @@
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         
-        # def rotate_left(root):
-
-        #     p = root
-        #     c = p.right
-        #     g = c.right
-        #     t2 = c.left
-
-        #     c.left = p
-        #     p.right = t2
-
-        #     return c
-        
-        # def rotate_right(root):
-
-        #     p = root
-        #     c = p.left
-        #     g = c.left
-        #     t2 = c.right
-
-        #     c.right = p
-        #     p.left = t2
-
-        #     return c
-
-        # def find_height(root):
-
-        #     if not root:
-        #         return -1
-            
-        #     left_height = find_height(root.left)
-        #     right_height = find_height(root.right)
-
-        #     return max(left_height,right_height) + 1
-
         def BST(nums,s,e):
 
-            if not nums or s > e:
+            if s > e:
                 return None
             
             mid = s + (e-s)//2
@@ -51,13 +17,15 @@ class Solution:
             root = TreeNode(nums[mid])
 
             root.left = BST(nums,s,mid-1)
+
             root.right = BST(nums,mid+1,e)
 
             return root
-
-        return BST(nums,0,len(nums)-1)
-            
         
+        return BST(nums,0,len(nums)-1)
+
+
+
                 
 
             
