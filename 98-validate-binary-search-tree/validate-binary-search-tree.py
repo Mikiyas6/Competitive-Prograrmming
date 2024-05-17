96feca937648343a5508041042fa8a9eb9f264e5
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-17 13:04:00 +0300

    Time: 49 ms (13.11%) | Memory: 18.3 MB (48.66%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -7,54 +7,36 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
-        min_val=float('-inf')
-        max_val=float('inf')
-        
-        def validate(root,min_val,max_val):
+        result = []
+
+        def store(value):
+            
+            nonlocal result
+            result.append(value)
+
+        def DFS(root):
 
             if not root:
-                return True
+                return
+            
+            DFS(root.left)
+            store(root.val)
+            DFS(root.right)
+
+            return
         
-            # Check if the current root's value is within the valid range
-            if not (min_val < root.val < max_val):
+        DFS(root)
+        
+        i, j = 0, 1
+
+        while j < len(result):
+
+            if result[j] <= result[i]:
                 return False
-            
-            # Recursively validate the left and right subtrees
-            return (validate(root.left, min_val, root.val) and
-                    validate(root.right, root.val, max_val))
-        
-        return validate(root,min_val,max_val)
 
-        # result = []
+            i += 1
+            j += 1
 
-        # def store(value):
-            
-        #     nonlocal result
-        #     result.append(value)
-
-        # def DFS(root):
-
-        #     if not root:
-        #         return
-            
-        #     DFS(root.left)
-        #     store(root.val)
-        #     DFS(root.right)
-
-        #     return
-        
-        # DFS(root)
-        
-        # i, j = 0, 1
-
-        # while j < len(result):
-
-        #     if result[j] <= result[i]:
-        #         return False
-
-        #     i += 1
-        #     j += 1
-
-        # return True
+        return True
     
         
