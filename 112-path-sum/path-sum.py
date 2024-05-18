e182f8d92ab0da8fbee1052eb00d9292e7efa9a7
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-05-18 13:27:12 +0300

    Time: 45 ms (27.19%) | Memory: 17.4 MB (40.19%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -7,29 +7,41 @@
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         
-        def accumulate(root,cumulative):
+        # def accumulate(root,cumulative):
 
-            if not root:
-                return root
+        #     if not root:
+        #         return root
             
-            root.val = root.val + cumulative
+        #     root.val = root.val + cumulative
 
-            root.left = accumulate(root.left,root.val)
+        #     root.left = accumulate(root.left,root.val)
 
-            root.right = accumulate(root.right,root.val)
+        #     root.right = accumulate(root.right,root.val)
 
-            return root
+        #     return root
         
-        def find_node(root,target):
+        # def find_node(root,target):
+
+        #     if not root:
+        #         return False
+            
+        #     if not root.left and not root.right and root.val == target:
+        #         return True
+            
+        #     return find_node(root.left,target) or find_node(root.right,target)
+
+        # root = accumulate(root,0)
+
+        # return find_node(root,targetSum)
+
+        def path_sum(root,cumulative,target):
 
             if not root:
                 return False
             
-            if not root.left and not root.right and root.val == target:
+            if not root.left and not root.right and root.val + cumulative == target:
                 return True
             
-            return find_node(root.left,target) or find_node(root.right,target)
-
-        root = accumulate(root,0)
-
-        return find_node(root,targetSum)
+            return path_sum(root.left,root.val+cumulative,target) or path_sum(root.right,root.val+cumulative,target)
+        
+        return path_sum(root,0,targetSum)
