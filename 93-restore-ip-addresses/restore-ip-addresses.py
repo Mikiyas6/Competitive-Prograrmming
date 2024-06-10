782bb12405ea89363fb7bf302e5978c7f6b99902
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-10 20:51:23 +0300

    Time: 36 ms (74.73%) | Memory: 16.4 MB (85.39%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -1,19 +1,39 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        def is_valid_segment(segment):
-            # Check if segment is valid (between 0 and 255 and without leading zeros)
-            return 0 <= int(segment) <= 255 and (len(segment) == 1 or segment[0] != '0')
-        def backtrack(start, dots, path, result):
-            if dots == 0:
-                # Check if the remaining string is a valid segment
-                if is_valid_segment(s[start:]):
-                    result.append(path + s[start:])
+        
+        ipAdresses = []
+
+        def isValidInteger(string):
+
+            if len(string) > 1 and string[0] == '0':
+                return False
+            
+            val = int(string)
+
+            return 0 <= val and val <= 255
+
+        def dfs(i,address):
+
+            if i == len(s):
+                if len(address) == 4:
+                    add = address[:]
+                    add = '.'.join(add)
+                    ipAdresses.append(add)
+                return
+
+            if len(address) == 4:
                 return
             
-            for i in range(1, 4):
-                if start + i < len(s) and is_valid_segment(s[start:start+i]):
-                    backtrack(start+i, dots-1, path + s[start:start+i] + ".", result)
+            for j in range(i,len(s)):
+
+                string = s[i:j+1]
+
+                if isValidInteger(string):
+
+                    address.append(string)
+                    dfs(j+1,address)
+                    address.pop()
         
-        result = []
-        backtrack(0, 3, "", result)
-        return result
+        dfs(0,[])
+
+        return ipAdresses
