35a17bea671e6544767d96e690ba68a771859834
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-10 21:28:03 +0300

    Time: 32 ms (75.52%) | Memory: 16.4 MB (80.47%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,57 +2,30 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
 
         if not digits:
-
             return []
 
         hashmap = {
             '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
             '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
         }
+        
+        combinations = []
 
-        def fun(processed,unprocessed):
-    
-            if not unprocessed:
-                
-                return [processed]
+        def dfs(digits,combo):
 
-            digit = unprocessed[0]
+            if len(digits) == 0:
+                com = combo[:]
+                com = ''.join(combo)
+                combinations.append(com)
+                return
             
-            array = []
-            
-            for i in range(len(hashmap[digit])):
-                
-                array.extend(fun(processed+hashmap[digit][i],unprocessed[1:]))
-            
-            return array
+            for letter in hashmap[digits[0]]:
 
-        return fun("",digits)
+                combo.append(letter)
+                dfs(digits[1:],combo)
+                combo.pop()
+        
+        dfs(digits,[])
 
-        
-        # if not digits:
-        #     return []
-        
-        # # Create a mapping of digits to letters
-        # mapping = {
-        #     '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
-        #     '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
-        # }
-        
-        # # Initialize the result list
-        # result = []
-        
-        # # Helper function to backtrack and generate combinations
-        # def backtrack(combination, next_digits):
-        #     # If there are no more digits to check, add the combination to the result
-        #     if not next_digits:
-        #         result.append(combination)
-        #     else:
-        #         # Iterate through the letters corresponding to the next digit
-        #         for letter in mapping[next_digits[0]]:
-        #             # Backtrack with the current combination plus the current letter
-        #             backtrack(combination + letter, next_digits[1:])
-        
-        # # Start the backtracking process with an empty combination and the input digits
-        # backtrack('', digits)
-        
-        # return result
+        return combinations
+
