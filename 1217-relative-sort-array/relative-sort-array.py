5cf51b6832a12ad8ec659109ab1171369db8da69
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-11 11:40:03 +0300

    Time: 39 ms (68.91%) | Memory: 16.7 MB (30.54%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,57 +1,20 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
 
-        dictionary = {}
-
-        for index,value in enumerate(arr2):
-
-            dictionary[value] = index
-
-        lists1 = []
-        lists2 = []
+        hashmap = Counter(arr1)
+        hashmap2 = Counter(arr2)
+        hashset = []
         
         for value in arr1:
+            if value not in hashmap2:
+                hashset.append(value)
+        hashset.sort()
 
-            if value not in arr2:
-                lists1.append(value)
-            else:
-                lists2.append(value)
+        result = []
 
-        def sorter(element):
-
-            return dictionary[element]
-
-        lists2.sort(key = sorter)
-        lists1.sort()
-
-        return lists2 + lists1
+        for value in arr2:
+            result.extend([value]*hashmap[value])
         
-        # dictionary = {}
-
-        # for i in range(len(arr2)):
-        #     dictionary[arr2[i]] = i
-
-        # lists = []
+        result.extend(hashset)
         
-        # def custom_comparator(items):
-
-        #     nonlocal lists
-        #     nonlocal dictionary
-            
-        #     if items in dictionary.keys():
-        #         return dictionary[items]
-            
-        
-        # new_list1 = []
-        # new_list2 = []
-
-        # for value in arr1:
-        #     if value in arr2:
-        #         new_list1.append(value)
-        #     else:
-        #         new_list2.append(value)
-
-        # new_list1.sort(key = custom_comparator)
-        # new_list2.sort()
-
-        # return new_list1 + new_list2
+        return result
