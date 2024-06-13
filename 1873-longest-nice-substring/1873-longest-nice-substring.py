b232cbd0bd5f8922ce72b48dd31cc5cec8387653
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-13 10:45:11 +0300

    Time: 36 ms (85.02%), Space: 16.5 MB (81.43%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.py
+++ 1873-longest-nice-substring/1873-longest-nice-substring.py
@@ -1,17 +1,17 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
         
-        def longestNiceSubstringRecursive(s):
+        def divAndCon(s):
             if len(s) < 2:
                 return ""
             for i in range(len(s)):
                 if s[i].swapcase() not in s:
-                    left = longestNiceSubstringRecursive(s[:i])
-                    right = longestNiceSubstringRecursive(s[i+1:])
+                    left = divAndCon(s[:i])
+                    right = divAndCon(s[i+1:])
                     if len(left) >= len(right):
                         return left
                     else:
                         return right
             return s
         
-        return longestNiceSubstringRecursive(s)
+        return divAndCon(s)
