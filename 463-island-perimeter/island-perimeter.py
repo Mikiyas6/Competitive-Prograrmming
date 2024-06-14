b87ccb3053877c544cac894d67b38a3932b31716
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-14 12:13:48 +0300

    Time: 467 ms (12.68%) | Memory: 24.4 MB (5.85%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,32 +1,30 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
 
-        visited = [[False]*len(grid[0]) for _ in range(len(grid))]
+        visited = set()
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
 
         def inbound(row,col):
-            return (0 <= row < len(grid)) and (0 <= col < len(grid[0]))
+            return 0 <= row and row < len(grid) and 0 <= col and col < len(grid[0])
 
         def dfs(row,col):
+
             if not inbound(row,col) or grid[row][col] == 0:
                 return 1
-            if visited[row][col]:
-                return 0
-            visited[row][col] = True
-            perimeter =  dfs(row,col+1)
-            perimeter += dfs(row,col-1)
-            perimeter += dfs(row-1,col)
-            perimeter += dfs(row+1,col)
+            
+            perimeter = 0
+            visited.add((row,col))
+
+            for dx, dy in directions:
+                newRow = row+dx
+                newCol = col+dy
+                if (newRow,newCol) not in visited:
+                    perimeter += dfs(newRow,newCol)
+                    
             return perimeter
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    return dfs(i,j)
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    return dfs(row,col)
             
-
-
-
-                    
-
-
-
