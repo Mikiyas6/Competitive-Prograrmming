82aedabd3792b4ae03df10b8e6f539ec53b7ee59
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-14 22:38:22 +0300

    Time: 37 ms (46.11%), Space: 16.4 MB (70.47%) - LeetHub

--- 0306-additive-number/0306-additive-number.py
+++ 0306-additive-number/0306-additive-number.py
@@ -5,37 +5,35 @@ class Solution:
             return False
         
         def fun(i,part):
-
             if i == len(num):
-
                 if len(part) < 3:
                     return False
+                # if len(part) < 3:
+                #     return False
 
-                j, k = 0, 1
+                # j, k = 0, 1
+                # while k+1 < len(part):
+                #     if part[j] + part[k] != part[k+1]:
+                #         return False
 
-                while k+1 < len(part):
-
-                    if part[j] + part[k] != part[k+1]:
-                        return False
-
-                    j += 1
-                    k += 1
+                #     j += 1
+                #     k += 1
 
                 return True
 
             for j in range(i,len(num)):
-
                 string = num[i:j+1]
-
                 if len(string) > 1 and string[0] == '0':
                     continue
-
                 val = int(string)
-
+                if len(part) >= 2 and part[-1] + part[-2] != val:
+                    continue
                 part.append(val)
                 if fun(j+1,part):
                     return True
                 part.pop()
+            
+            return False
 
         return fun(0,[])
             
