6c07d230dcdc88d777bee7ce98887cece184f6e3
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-15 11:14:55 +0300

    Time: 583 ms (93.67%) | Memory: 21.5 MB (67.68%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -5,17 +5,33 @@ class Solution:
 
         for u, v in trust:
             hashmap[u].append(v)
-
-        flag = False
+        
+        possible = []
         for i in range(1,n+1):
-            for j in range(1,n+1):
-                if i != j and i not in hashmap[j]:
+            if not hashmap[i]:
+                possible.append(i)
+        
+        flag = False
+        for value in possible:
+            for keys in hashmap:
+                if value != keys and value not in hashmap[keys]:
                     flag = True
                     break
-            if not flag and not hashmap[i]:
-                return i
-            flag = False
+            if not flag:
+                return value
         
         return -1
+
+        # flag = False
+        # for i in range(1,n+1):
+        #     for j in range(1,n+1):
+        #         if i != j and i not in hashmap[j]:
+        #             flag = True
+        #             break
+        #     if not flag and not hashmap[i]:
+        #         return i
+        #     flag = False
+        
+        # return -1
 
     
