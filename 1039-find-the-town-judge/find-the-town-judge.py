d335e5940dfeafd7e9366388b684078d73e11170
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-15 11:20:28 +0300

    Time: 612 ms (26.34%) | Memory: 21.4 MB (89.28%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,26 +1,38 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        
-        hashmap = defaultdict(list)
+
+        incoming = defaultdict(int)
+        outgoing = defaultdict(int)
 
         for u, v in trust:
-            hashmap[u].append(v)
+            outgoing[u] += 1
+            incoming[v] += 1
         
-        possible = []
         for i in range(1,n+1):
-            if not hashmap[i]:
-                possible.append(i)
-        
-        flag = False
-        for value in possible:
-            for keys in hashmap:
-                if value != keys and value not in hashmap[keys]:
-                    flag = True
-                    break
-            if not flag:
-                return value
+            if outgoing[i] == 0 and incoming[i] == n-1:
+                return i
         
         return -1
+        # hashmap = defaultdict(list)
+
+        # for u, v in trust:
+        #     hashmap[u].append(v)
+        
+        # possible = []
+        # for i in range(1,n+1):
+        #     if not hashmap[i]:
+        #         possible.append(i)
+        
+        # flag = False
+        # for value in possible:
+        #     for keys in hashmap:
+        #         if value != keys and value not in hashmap[keys]:
+        #             flag = True
+        #             break
+        #     if not flag:
+        #         return value
+        
+        # return -1
 
         # flag = False
         # for i in range(1,n+1):
