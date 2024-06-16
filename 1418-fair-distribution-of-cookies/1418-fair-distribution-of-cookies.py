bf4803e4499d16522293425c42f05b9cb5eb1673
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-16 17:20:56 +0300

    Time: 6386 ms (13.74%), Space: 16.4 MB (91%) - LeetHub

--- 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
@@ -1,23 +1,23 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-        positions = [0] * k
-        min_unfairness = float('inf')
+        
+        positions = [0]*k
+        min_value = float('inf')
 
         def dfs(i):
-            nonlocal min_unfairness
+
+            nonlocal min_value
             if i == len(cookies):
-                min_unfairness = min(min_unfairness, max(positions))
+                min_value = min(min_value,max(positions))
+                return 
+
+            if min_value <= max(positions):
                 return
 
             for j in range(k):
                 positions[j] += cookies[i]
-                # Prune the search space if the current distribution is already worse than the best found
-                if positions[j] < min_unfairness:
-                    dfs(i + 1)
+                dfs(i+1)
                 positions[j] -= cookies[i]
-                # If one bucket is empty, no need to distribute to further empty buckets
-                if positions[j] == 0:
-                    break
-
+        
         dfs(0)
-        return min_unfairness
+        return min_value
