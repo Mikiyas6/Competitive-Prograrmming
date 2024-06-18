bb7b7bcfb833eb8839c0cf0025e77895eed033fa
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-18 17:27:02 +0300

    Time: 298 ms (41.95%) | Memory: 19.6 MB (56.95%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
         
-        jobs = sorted(zip(difficulty, profit))  # Create a list of tuples and sort based on difficulty
-        worker.sort()  # Sort the worker abilities
+        jobs = sorted(zip(difficulty, profit))  
+        worker.sort()  
         total_profit = 0
         i = 0
         best = 0
 
         for ability in worker:
             while i < len(jobs) and jobs[i][0] <= ability:
-                best = max(best, jobs[i][1])  # Find the best profit for current ability
+                best = max(best, jobs[i][1])  
                 i += 1
             total_profit += best
 
