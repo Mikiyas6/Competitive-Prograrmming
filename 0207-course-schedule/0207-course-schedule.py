f2182d185122c507de81480c954e7d2f4df97548
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-06-21 21:43:01 +0300

    Time: 93 ms (37.92%), Space: 18.3 MB (71.03%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -5,32 +5,28 @@ class Solution:
         for u, v in prerequisites:
             graph[u].append(v)
         
-        # Create two sets for visited and currently in path nodes
         visited = set()
         path = set()
 
-        # Helper function for DFS
         def dfs(node):
-            if node in path:  # cycle detected
+
+            if node in path:
                 return False
-            if node in visited:  # already processed node
+            if node in visited:
                 return True
             
-            # Mark the current node in the path
             path.add(node)
-            # Visit all the neighbors
-            for neighbor in graph[node]:
+            neighbors = graph[node]
+            for neighbor in neighbors:
                 if not dfs(neighbor):
                     return False
             
-            # Remove from current path and add to visited
             path.remove(node)
             visited.add(node)
             return True
         
-        # Check all nodes
-        for course in range(numCourses):
-            if not dfs(course):
+        for i in range(numCourses):
+            if not dfs(i):
                 return False
         
         return True
