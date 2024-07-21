2eee981b313170e452fba756e6b61dd3702438ca
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-07-21 12:54:14 +0300

    Time: 566 ms (93.13%), Space: 25.5 MB (45.8%) - LeetHub

---
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.py
@@ -0,0 +1,41 @@
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        def topological_sort(conditions,k):
+            graph = defaultdict(list)
+            in_degree = [0] * (k + 1)
+            
+            for u, v in conditions:
+                graph[u].append(v)
+                in_degree[v] += 1
+            
+            queue = deque([i for i in range(1, k + 1) if in_degree[i] == 0])
+            topo_order = []
+            
+            while queue:
+                node = queue.popleft()
+                topo_order.append(node)
+                for neighbor in graph[node]:
+                    in_degree[neighbor] -= 1
+                    if in_degree[neighbor] == 0:
+                        queue.append(neighbor)
+            
+            if len(topo_order) != k:
+                return []
+            
+            return topo_order
+
+        row_order = topological_sort(rowConditions, k)
+        col_order = topological_sort(colConditions, k)
+        
+        if not row_order or not col_order:
+            return []
+
+        row_pos = {num: i for i, num in enumerate(row_order)}
+        col_pos = {num: i for i, num in enumerate(col_order)}
+        
+        matrix = [[0] * k for _ in range(k)]
+        
+        for num in range(1, k + 1):
+            matrix[row_pos[num]][col_pos[num]] = num
+        
+        return matrix
