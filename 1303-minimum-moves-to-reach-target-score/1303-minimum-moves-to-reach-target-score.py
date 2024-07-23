bb4fa098677582f630d120099300e31069cd2106
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-07-23 19:49:13 +0300

    Time: 34 ms (65.69%), Space: 16.4 MB (99.08%) - LeetHub

---
+++ 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        moves = 0
+        while target > 1:
+            if target % 2 == 0 and maxDoubles > 0:
+                target //= 2
+                maxDoubles -= 1
+            elif target % 2 == 0 and maxDoubles == 0:
+                moves += (target - 1)
+                break
+            else:
+                target -= 1
+            moves += 1
+        
+        return moves
