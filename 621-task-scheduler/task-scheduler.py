d7a0c7306f9c393cfbff327bda985b59b80c7096
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-07-26 23:11:16 +0300

    Time: 354 ms (88.41%) | Memory: 17.1 MB (51.27%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,15 +1,11 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         
-        task_counts = [0] * 26  # Assuming only uppercase English letters
+        task_counts = [0] * 26  
         for task in tasks:
             task_counts[ord(task) - ord('A')] += 1
 
         max_freq = max(task_counts)
         max_freq_count = task_counts.count(max_freq)
-
-        # Calculate the minimum number of units of time required
         min_time = (max_freq - 1) * (n + 1) + max_freq_count
-
-        # If the tasks are more than the minimum time, return the tasks length
         return max(len(tasks), min_time)
