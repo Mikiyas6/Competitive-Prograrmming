d597450d38e039eb90e009d93cb71a12ddcc7a34
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-07-30 21:35:28 +0300

    Time: 94 ms (33.21%), Space: 18.5 MB (25.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,6 +1,6 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+
         graph = defaultdict(list)
         for u, v in prerequisites:
             graph[u].append(v)
@@ -9,11 +9,11 @@ class Solution:
         path = set()
 
         def dfs(node):
-
-            if node in path:
-                return False
+            
             if node in visited:
                 return True
+            if node in path:
+                return False
             
             path.add(node)
             neighbors = graph[node]
@@ -21,12 +21,42 @@ class Solution:
                 if not dfs(neighbor):
                     return False
             
-            path.remove(node)
             visited.add(node)
+            path.remove(node)
             return True
         
         for i in range(numCourses):
-            if not dfs(i):
-                return False
-        
+            if i not in visited:
+                if not dfs(i):
+                    return False
         return True
+        
+        # graph = defaultdict(list)
+        # for u, v in prerequisites:
+        #     graph[u].append(v)
+        
+        # visited = set()
+        # path = set() 
+
+        # def dfs(node):
+
+        #     if node in path:
+        #         return False
+        #     if node in visited:
+        #         return True
+            
+        #     path.add(node)
+        #     neighbors = graph[node]
+        #     for neighbor in neighbors:
+        #         if not dfs(neighbor):
+        #             return False
+            
+        #     visited.add(node)
+        #     path.remove(node)
+        #     return True
+        
+        # for i in range(numCourses): #The loop is for if there are disconnected graphs
+        #     if not dfs(i):
+        #         return False
+        
+        # return True
