18080d084f4de335529411d329246abba8669417
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-07-30 21:04:15 +0300

    Time: 86 ms (80.57%) | Memory: 18.4 MB (56.19%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,38 +1,79 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
+
+        graph = defaultdict(list)
+        indegree = defaultdict(int)
         hashmap = defaultdict(list)
+        queue = deque([])
+        sources = []
+
         for u, v in prerequisites:
-            hashmap[u].append(v)
+            indegree[u] += 1
+            graph[u].append(v)
+            hashmap[v].append(u)
         
-        output = []
-        visited = set()
-        path = set()
+        for i in range(numCourses):
+            if i not in graph:
+                queue.append(i)
+        
+        def bfs(queue):
 
-        def dfs(node):
-            
-            if node in path:
-                return False
-            if node in visited:
-                return True
-            
-            neighbours = hashmap[node]
-            path.add(node)
-            for neighbour in neighbours:
-                if not dfs(neighbour):
-                    return False
+            topo_sort = []
+            while queue:
+                level = len(queue)
+                for _ in range(level):
+                    node = queue.popleft()
+                    topo_sort.append(node)
+                    neighbors = hashmap[node]
+                    for neighbor in neighbors:
+                        if indegree[neighbor] > 0:
+                            indegree[neighbor] -= 1
+                            if indegree[neighbor] == 0:
+                                queue.append(neighbor)
+            return topo_sort
+        
+        ans = bfs(queue)
 
-            output.append(node)
-            visited.add(node)
-            path.remove(node)
-            return True
+        if len(ans) != numCourses:
+            return []
+        return ans
+                    
+
+
+
         
-        for node in range(numCourses):
-            if node in visited and node not in output:
-                output.append(node)
-            else:
-                if not dfs(node):
-                    return []
+        # hashmap = defaultdict(list)
+        # for u, v in prerequisites:
+        #     hashmap[u].append(v)
         
-        return output
+        # output = []
+        # visited = set()
+        # path = set()
+
+        # def dfs(node):
+            
+        #     if node in path:
+        #         return False
+        #     if node in visited:
+        #         return True
+            
+        #     neighbours = hashmap[node]
+        #     path.add(node)
+        #     for neighbour in neighbours:
+        #         if not dfs(neighbour):
+        #             return False
+
+        #     output.append(node)
+        #     visited.add(node)
+        #     path.remove(node)
+        #     return True
+        
+        # for node in range(numCourses):
+        #     if node in visited and node not in output:
+        #         output.append(node)
+        #     else:
+        #         if not dfs(node):
+        #             return []
+        
+        # return output
 
