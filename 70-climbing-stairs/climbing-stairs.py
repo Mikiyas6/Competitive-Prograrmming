46e4c461b0dba71a1b3e19384117675bd57c6a06
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-08-08 18:33:52 +0300

    Time: 43 ms (6.13%) | Memory: 16.5 MB (14.09%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,17 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        hashmap = defaultdict(int)
-        hashmap[0] = 1
-        def fun(n):
-            if n < 0:
-                return 0
-            if n in hashmap:
-                return hashmap[n]
-            
-            value1 = fun(n-1)
-            value2 = fun(n-2)
-            hashmap[n] = value1 + value2
-            return hashmap[n]
-        
-        return fun(n)
+        if n <= 1:
+            return n
 
+        dp = [0]*n
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2,n):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n-1]
