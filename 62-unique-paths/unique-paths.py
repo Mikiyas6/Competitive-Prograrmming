a32022a7850ec19b581a98388cbbd532ed22ce1f
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-08-13 14:06:45 +0300

    Time: 42 ms (15.02%) | Memory: 16.5 MB (34.59%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,23 +1,32 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        memo = defaultdict(int)
+        dp = [[1]*(n+1) for _ in range(m+1)]
 
-        def inbound(row,col):
-            return 0 <= row and row < m and 0 <= col and col < n
-        
-        def dp(row,col):
+        for row in range(1,len(dp)):
+            for col in range(1,len(dp[0])):
+                dp[row][col] = dp[row-1][col] + dp[row][col-1] 
 
-            if row == m-1 and col == n-1:
-                return 1
-            
-            if not inbound(row,col):
-                return 0
-            
-            if (row,col) not in memo:
-                memo[(row,col)] = dp(row+1,col) + dp(row,col+1)
-            
-            return memo[(row,col)]
+        return dp[m-1][n-1]
+
+
+        # memo = defaultdict(int)
+
+        # def inbound(row,col):
+        #     return 0 <= row and row < m and 0 <= col and col < n
         
-        return dp(0,0)
+        # def dp(row,col):
+
+        #     if row == m-1 and col == n-1:
+        #         return 1
+            
+        #     if not inbound(row,col):
+        #         return 0
+            
+        #     if (row,col) not in memo:
+        #         memo[(row,col)] = dp(row+1,col) + dp(row,col+1)
+            
+        #     return memo[(row,col)]
+        
+        # return dp(0,0)
             
