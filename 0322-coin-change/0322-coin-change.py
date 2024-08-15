ec71867f85da3ed36b8d11cdaf8d810a82dcd096
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-08-15 21:34:55 +0300

    Time: 1154 ms (15.61%), Space: 44.9 MB (8.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,12 +1,33 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = defaultdict(int)
 
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
+        def fun(total):
+            if total in memo:
+                return memo[total]
 
-        for coin in coins:
-            for x in range(coin, amount + 1):
-                dp[x] = min(dp[x], dp[x - coin] + 1)
+            if total == amount:
+                return 0
+
+            if total > amount:
+                return float('inf')
+
+            minCoins = float('inf')
+            for value in coins:
+                minCoins = min(minCoins, fun(total + value) + 1)
+
+            memo[total] = minCoins
+            return minCoins
+
+        result = fun(0)
+        return result if result != float('inf') else -1
+
+        # dp = [float('inf')] * (amount + 1)
+        # dp[0] = 0
+
+        # for coin in coins:
+        #     for x in range(coin, amount + 1):
+        #         dp[x] = min(dp[x], dp[x - coin] + 1)
                 
-        return dp[amount] if dp[amount] != float('inf') else -1
+        # return dp[amount] if dp[amount] != float('inf') else -1
             
