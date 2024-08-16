c1b6e9c4ec6b05661194e00391973094c18f03aa
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-08-16 20:49:02 +0300

    Time: 780 ms (57.92%), Space: 16.9 MB (50.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,26 +1,38 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = defaultdict(int)
 
-        def fun(total):
-            if total in memo:
-                return memo[total]
-
-            if total == 0:
-                return 0
-
-            if total < 0:
-                return float('inf')
-
+        dp = [0]*(amount+1)
+        dp[0] = 0
+        for i in range(1,amount+1):
             minCoins = float('inf')
-            for value in coins:
-                minCoins = min(minCoins, fun(total - value))
+            for coin in coins:
+                remainder = i - coin
+                if remainder >= 0:
+                    minCoins = min(minCoins,dp[remainder])
+            dp[i] = minCoins + 1
+        return dp[-1] if dp[-1] != float('inf') else -1
 
-            memo[total] = minCoins + 1
-            return memo[total]
+        # memo = defaultdict(int)
 
-        result = fun(amount)
-        return result if result != float('inf') else -1
+        # def fun(total):
+        #     if total in memo:
+        #         return memo[total]
+
+        #     if total == 0:
+        #         return 0
+
+        #     if total < 0:
+        #         return float('inf')
+
+        #     minCoins = float('inf')
+        #     for value in coins:
+        #         minCoins = min(minCoins, fun(total - value))
+
+        #     memo[total] = minCoins + 1
+        #     return memo[total]
+
+        # result = fun(amount)
+        # return result if result != float('inf') else -1
 
         # dp = [float('inf')] * (amount + 1)
         # dp[0] = 0
