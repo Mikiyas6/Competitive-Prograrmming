2b9b3cd7f9b140c46ef82606dfc14f220a781196
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-08-21 20:59:00 +0300

    Time: 1300 ms (34.63%), Space: 72.3 MB (18.75%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -2,36 +2,35 @@ class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
 
         ######### Top-Down
-        # n = len(questions)
-        # memo = defaultdict(int)
-
-        # def fun(i,total):
-
-        #     if i >= n:
-        #         return total
-            
-        #     if (i,total) not in memo:
-        #         memo[(i,total)] = max(fun(i+1,total),fun(i+questions[i][1]+1,total+questions[i][0]))
-            
-        #     return memo[(i,total)]
-        
-        # return fun(0,0)
-
-        # Bottom-up
         n = len(questions)
-        dp = [0]*(n)
+        memo = defaultdict(int)
 
-        dp[-1] = questions[-1][0]
+        def fun(i):
 
-        for i in range(n-2,-1,-1):
-            index = i+questions[i][1]+1
-            value = 0
-            if index < n:
-                value = dp[index]
-            dp[i] = max(value+questions[i][0],dp[i+1])
+            if i >= n:
+                return 0
+            
+            if i not in memo:
+                memo[i] = max(fun(i+1),questions[i][0]+fun(i+questions[i][1]+1))
+            
+            return memo[i]
         
-        print(dp)
-        return max(dp)
+        return fun(0)
+
+        ############# Bottom-up
+        # n = len(questions)
+        # dp = [0]*(n)
+
+        # dp[-1] = questions[-1][0]
+
+        # for i in range(n-2,-1,-1):
+        #     index = i+questions[i][1]+1
+        #     value = 0
+        #     if index < n:
+        #         value = dp[index]
+        #     dp[i] = max(value+questions[i][0],dp[i+1])
+        
+        # return max(dp)
 
         
         # n = len(questions)
