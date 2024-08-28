119a5a31cf5d968676d48a9f20a9c65695a1bcd6
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-08-28 21:10:18 +0300

    Time: 111 ms (58.80%) | Memory: 17.7 MB (51.90%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,22 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        n = len(nums)
-
-        nums.sort()
-
-        s, e = 0, n-1
-
-        while s <= e:
-
-            mid = s + (e-s)//2
-
-            if mid == nums[mid]:
-
-                s = mid + 1
-
-            elif mid < nums[mid]:
-
-                e = mid - 1
-            
-        return s
+       return sum(list(range(0,len(nums)+1))) - sum(nums)
