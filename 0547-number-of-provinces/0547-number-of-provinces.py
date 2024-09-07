11ac1ddab0dfc389f686358a167f4dcbebc61dbb
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-09-07 18:36:20 +0300

    Time: 193 ms (21.82%), Space: 17.4 MB (49.78%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+        n, m = len(isConnected), len(isConnected[0])
+        parent = [i for i in range(n+1)]
+        rank = [1]*(n+1)
+
+        def find(node):
+            if node == parent[node]:
+                return node
+            
+            parent[node] = find(parent[node])
+
+            return parent[node]
+
+        def union(node1,node2):
+
+            parent1, parent2 = find(node1), find(node2)
+
+            if rank[parent1] >= rank[parent2]:
+                parent[parent2] = parent1
+                rank[parent1] += 1
+            else:
+                parent[parent1] = parent2
+                rank[parent2] += 1
+        
+        for i in range(n):
+            for j in range(i + 1, m):
+                if isConnected[i][j] == 1:
+                    union(i, j)
+        
+        province_count = len(set(find(i) for i in range(n)))
+        
+        return province_count
