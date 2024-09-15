6435c794cfdae1008fab281e76afc2f9967127dc
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-09-15 13:55:48 +0300

    Time: 1206 ms (91.42%) | Memory: 61.2 MB (85.24%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        p = list(range(n + 1))
+        hash = {}
+
+        def find(x):
+            if x != p[x]:
+                p[x] = find(p[x])
+            return p[x]
+
+        def union(x, y, val):
+            px = find(x)
+            py = find(y)
+
+            a = hash[px] if px in hash else val
+            b = hash[py] if py in hash else val
+            hash[px] = min(val, a, b)
+
+            if px != py:
+                p[py] = px
+
+        for x, y, val in roads:
+            union(x, y, val)
+
+        return hash[find(1)]
