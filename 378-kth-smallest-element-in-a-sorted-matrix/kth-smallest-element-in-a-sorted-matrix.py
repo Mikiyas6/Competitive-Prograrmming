d86fc78be7829252629ad3754b762148b2eda321
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-10-27 10:25:31 +0300

    Time: 31 ms (22.70%) | Memory: 20.3 MB (95.43%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -4,7 +4,7 @@ class Solution:
         rowCount = len(matrix)
         min_heap = []
 
-        # Push the first element of each row into the minHeap
+       
         for i in range(min(rowCount, k)):
             heapq.heappush(min_heap, (matrix[i][0], i, 0))
 
@@ -12,15 +12,15 @@ class Solution:
         smallest_element = 0
 
         while min_heap:
-            # Get the smallest element from the heap
+           
             smallest_element, row_index, col_index = heapq.heappop(min_heap)
             numbers_checked += 1
 
-            # If we've checked k elements, return the k-th smallest element
+           
             if numbers_checked == k:
                 break
 
-            # If there is a next element in the current row, add it to the heap
+           
             if col_index + 1 < len(matrix[row_index]):
                 heapq.heappush(min_heap, (matrix[row_index][col_index + 1], row_index, col_index + 1))
 
