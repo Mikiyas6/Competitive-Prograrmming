036bbecb42105e7ec5e0193006fc2cce61516caa
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-02 16:10:11 +0300

    Time: 3 ms (71.13%) | Memory: 16.6 MB (28.20%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -2,28 +2,22 @@ class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
 
         n = len(arr)
-
-        output = []
-
+        maxValue = n
+        result = []
         if sorted(arr) != arr:
+            for i in range(n-1):
+                # Find the index of the maximum value with in the specified range
+                index = arr[:n-i].index(maxValue)
+                # Store that index along with the 1 index in result
+                result.extend([index+1,n-i])
+                # Flip the array twice
+                arr[:index+1] = arr[:index+1][::-1]
+                arr[:n-i] = arr[:n-i][::-1]
+                # Look for the next max Value
+                maxValue -= 1
+            result.append(1)
+        return result
 
-            for i in range(n):
-
-                if arr[:n-i-1]:
-
-                    max_value = max(arr[:n-i])
-
-                    index_of_max_value = arr[:n-i].index(max_value)
-
-                    output.append(index_of_max_value + 1)
-
-                    output.append(n-i)
-
-                    arr[:index_of_max_value+1] = arr[:index_of_max_value+1][::-1]
-
-                    arr[:n-i] = arr[:n-i][::-1]
-        
-        return output
 
 
         
