a95bfbe2046875170647b91d2ee7d450682f4ed9
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-06 00:11:34 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (24.53%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,48 +5,34 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
 
-        dummy = ListNode(-1) # A chekal pointer who is going to point at that node forever
-        current = dummy # A pointer that points to the end of the node
+        dummy = ListNode(-1)
+        current = dummy
+        ptr1 = list1
+        ptr2 = list2
 
-        # dummy
-        #  ↓
-        #  O → O → O
-        #          ↑
-        #          current
+        def fun(current,ptr1,ptr2):
 
-        def fun(current,list1,list2):
-
-            if not list1:
-
-                current.next = list2
-
+            if not ptr1:
+                current.next = ptr2
                 return current
-
-            if not list2:
-
-                current.next = list1
-
+            if not ptr2:
+                current.next = ptr1
                 return current
             
-            if list1.val <= list2.val:
-
-                value = list1.val
-                list1 = list1.next
-            
+            if ptr1.val <= ptr2.val:
+                value = ptr1.val
+                ptr1 = ptr1.next
             else:
-
-                value = list2.val
-                list2 = list2.next
+                value = ptr2.val
+                ptr2 = ptr2.next
             
-            Node = ListNode(value)
-
-            current.next = fun(Node,list1,list2)
-
+            node = ListNode(value)
+            current.next = fun(node,ptr1,ptr2)
             return current
-    
-        return fun(current,list1,list2).next
-
-
-
-        
+                
+        return fun(current,ptr1,ptr2).next
