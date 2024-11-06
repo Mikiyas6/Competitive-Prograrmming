985f77ac02154decc97ca35aa825196bb2ad9a65
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-06 22:55:53 +0300

    Time: 1 ms (23.64%) | Memory: 16.8 MB (13.08%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -6,38 +6,39 @@
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
 
+        def reverse(head):
+            prev = None
+            current = head
+
+            while current:
+                next_node = current.next  
+                current.next = prev       
+                prev = current            
+                current = next_node       
+
+            return prev  
+       
         if not head or not head.next:
-
             return head
-
+        n = 0
         current = head
-        size = 0
-
         while current:
-
             current = current.next
-            size += 1
-
-        k = k%size
-        if k == 0:
-
-            return head
-        end_up = size - k - 1
+            n += 1
+        k %= n
+        end_up = n - k - 1
         current = head
-
         for i in range(end_up):
             current = current.next
-        
-        rotated_part = current.next
+        second_part = current.next
         current.next = None
-
-        current = rotated_part
-
+        first_part = head
+        reversed_first_part = reverse(first_part)
+        reversed_second_part = reverse(second_part)
+        current = reversed_first_part
         while current.next:
-
             current = current.next
+        current.next = reversed_second_part
+        final_result = reverse(reversed_first_part)
+        return final_result
         
-        current.next = head
-
-        return rotated_part
-
