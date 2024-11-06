02b8784e90cefe0324d5454d4acbebdc1d3adccd
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-06 13:50:47 +0300

    Time: 1288 ms (70.49%), Space: 29.3 MB (39.68%) - LeetHub

---
+++ 2526-longest-increasing-subsequence-ii/2526-longest-increasing-subsequence-ii.py
@@ -0,0 +1,38 @@
+class SegTree: 
+
+    def __init__(self, arr: List[int]): 
+        self.n = len(arr)
+        self.tree = [0] * (2*self.n)
+
+    def query(self, lo: int, hi: int) -> int: 
+        ans = 0 
+        lo += self.n 
+        hi += self.n
+        while lo < hi: 
+            if lo & 1: 
+                ans = max(ans, self.tree[lo])
+                lo += 1
+            if hi & 1: 
+                hi -= 1
+                ans = max(ans, self.tree[hi])
+            lo >>= 1
+            hi >>= 1
+        return ans 
+
+    def update(self, i: int, val: int) -> None: 
+        i += self.n 
+        self.tree[i] = val
+        while i > 1: 
+            self.tree[i>>1] = max(self.tree[i], self.tree[i^1])
+            i >>= 1
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int], k: int) -> int:
+        m = max(nums)
+        ans = 0 
+        tree = SegTree([0] * (m+1))
+        for x in nums: 
+            val = tree.query(max(0, x-k), x) + 1
+            ans = max(ans, val)
+            tree.update(x, val)
+        return ans 
