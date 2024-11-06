343383731a7e6cf17d6a429722a0ba51238ea078
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-06 16:56:07 +0300

    Time: 3 ms (5.49%) | Memory: 16.8 MB (5.02%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -6,91 +6,70 @@
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
 
-        if not head or not head.next:
+        if not head:
+            return head
 
+        if left == right:
             return head
         
-        def fun(head):
+        def reverse(head):
 
-            if not head or not head.next:
-
+            if not head.next:
                 return head
             
+            right_part = head.next
+            head.next = None
+            node = head
+            reversedList = reverse(right_part)
+            current = reversedList
+
+            while current.next:
+                current = current.next
+            
+            current.next = node
+            return reversedList
+
+        if left == 1:
+            first_part = None
+            second_part = head
+
+        elif left == 2:
             second_part = head.next
             head.next = None
-            first_node = head
+            first_part = head
 
-            reversed_second_part = fun(second_part)
-
-            current = reversed_second_part
-
-            while current.next:
-
+        else:
+            left_end_up = left-2
+            current = head
+            for i in range(left_end_up):
                 current = current.next
-            
-            current.next = first_node
-
-            return reversed_second_part
-
-        current =  head
-        end_up = left - 2
-
-        if end_up >= 0:
-
-            for i in range(end_up):
-
-                current = current.next
-            
             second_part = current.next
             current.next = None
-            
-            first_part = head # The first part
-
-        else:
-
-            second_part = head
-            first_part = None
-
+            first_part = head
+        
+        
+        if not second_part:
+            return first_part
+        right_end_up = right-left
         current = second_part
-        calibrated = right - left
-
-        for i in range(calibrated):
-
+        for _ in range(right_end_up):
             current = current.next
+        last_part = current.next
+        current.next = None
+        to_be_reversed = second_part
 
+        if not to_be_reversed:
+            return head
+        reversedList = reverse(to_be_reversed)
+        current = first_part
         if current:
-            last_part = current.next  # The last part
-            current.next = None
-        
-        else:
-
-            last_part = None
-
-        middle_part = second_part # The part we want to reverse
-
-        reversed_middle_part = fun(middle_part)
-
-        if first_part:
-
-            current = first_part
-
             while current.next:
-
                 current = current.next
-
-            current.next = reversed_middle_part
-        
-        else:
-
-            first_part = reversed_middle_part
-
-        current = reversed_middle_part
+            current.next = reversedList
+        current = reversedList
         while current.next:
-
             current = current.next
-
         current.next = last_part
 
-        return first_part
-
+        return first_part if first_part else reversedList
 
