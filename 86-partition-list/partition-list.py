c561ec6bb87e9da978ab96a964f2dc87fafc233e
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-07 14:20:42 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (28.89%) - LeetSync

--- 86-partition-list/partition-list.py
+++ 86-partition-list/partition-list.py
@@ -3,71 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        
-#         if not head or not head.next:
-#             return head
-
-#         def fun(left, right, head):
-#             if not head.next:
-#                 if head.val < x:
-#                     left.next = ListNode(head.val)
-#                 else:
-#                     right.next = ListNode(head.val)
-#                 return left.next, right.next
-
-#             Node = ListNode(head.val)
-#             if head.val < x:
-#                 left.next = Node
-#                 return fun(left.next, right, head.next)
-#             else:
-#                 right.next = Node
-#                 return fun(left, right.next, head.next)
-
-#         dummy_left = ListNode(-1)
-#         dummy_right = ListNode(-1)
-        
-#         left, right = fun(dummy_left,dummy_right, head)
-
-#         current = dummy_left
-#         while current.next:
-#             current = current.next
-#         current.next = dummy_right.next
-
-#         return dummy_left.next
-
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        
-        if not head or not head.next:
-            return head
-
-        def fun(current, head,side):
-            if not head.next:
-                if (head.val < x and side == "less") or( head.val >= x and side == "greater"):
-                    Node = ListNode(head.val)
-                    current.next = Node
-                return current
-
-            if (head.val < x and side == "less") or( head.val >= x and side == "greater"):
-                Node = ListNode(head.val)
-                current.next =  fun(Node, head.next, side)
-                return current
-            
+        dummy_left = ListNode(-1)
+        dummy_right = ListNode(-1)
+        ptr1 = dummy_left
+        ptr2 = dummy_right
+        current = head
+        while current:
+            if current.val < x:
+                ptr1.next = ListNode(current.val)
+                ptr1 = ptr1.next
             else:
-
-                return fun(current,head.next,side)
-
-        less = fun(ListNode(-1),head,"less") 
-        greater = fun(ListNode(-1),head,"greater")
-
-        if not less.next:
-
-            return greater.next
-        current = less.next
+                ptr2.next = ListNode(current.val)
+                ptr2 = ptr2.next
+            current = current.next
+        current = dummy_left
         while current.next:
             current = current.next
-        current.next = greater.next
-
-        return less.next
+        current.next = dummy_right.next
+        return dummy_left.next
