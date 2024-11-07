4024d0b5867103cb1b16c4dedf809907609d1f3d
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-07 16:12:48 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (18.72%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,54 +5,19 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-
         if not head:
-
             return head
-
-        rear = head
-
-        for i in range(n):
-
-            rear = rear.next
-        
-        front = head
-
-        if not rear:
-
-            return front.next
-
-        while rear.next:
-
-            front = front.next
-            rear = rear.next
-        
-        front.next = front.next.next
-
+        size = 0
+        current = head
+        while current:
+            size += 1
+            current = current.next
+        end_up = size - n - 1
+        if end_up < 0:
+            return head.next
+        current = head
+        for i in range(end_up):
+            current = current.next
+        current.next = current.next.next
         return head
 
-        # # Calculate the size
-
-        # current = head
-        # size = 0
-
-        # while current:
-
-        #     current = current.next
-        #     size += 1
-        
-        # # Removing the nth node
-        # end_up = size - n - 1
-        # if end_up < 0:
-        #     head = head.next
-        #     return head
-            
-        # current = head
-
-        # for i in range(end_up):
-
-        #     current = current.next
-        
-        # current.next = current.next.next
-
-        # return head
