7ec165e61cad15efd388e34728a064e6afe376ea
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-08 10:57:18 +0300

    Time: 0 ms (100%), Space: 16.7 MB (31.35%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = (10**9)+7
+        even = (n//2) + (n%2)
+        odd = n//2
+
+        def fun(num,pow):
+            if pow == 0:
+                return 1
+            if pow == 1:
+                return num
+            
+            result = fun(num,pow//2)
+            final_result = result*result*num if pow%2 != 0 else result*result
+            return final_result%MOD
+
+        for_even_numbers = fun(5,even)
+        for_prime_numbers = fun(4,odd)
+        return (for_even_numbers*for_prime_numbers)%MOD
+
+
