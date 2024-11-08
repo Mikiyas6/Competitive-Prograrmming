f68d3302d0ffe3e2d1d74b1e685ba3d959f1da72
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-08 11:23:11 +0300

    Time: 0 ms (100%), Space: 16.6 MB (19.14%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        numRows = rowIndex+1
+        grid = [[0]*(numRows) for _ in range(numRows)]
+        for i in range(numRows):
+            grid[i][0] = 1
+        for row in range(1,numRows):
+            for col in range(1,numRows):
+                grid[row][col] = grid[row-1][col-1] + grid[row-1][col]
+        for i in range(numRows):
+            grid[i] = grid[i][:i+1]
+        return grid[rowIndex]
