fd574fa1970120c0b4ee439f9032bb724a241da5
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-08 10:57:18 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (31.35%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,49 +1,21 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-       
         MOD = (10**9)+7
-
         even = (n//2) + (n%2)
         odd = n//2
 
         def fun(num,pow):
-
             if pow == 0:
                 return 1
             if pow == 1:
                 return num
             
-            ans = fun(num,pow//2)
-            ans = ans * ans
+            result = fun(num,pow//2)
+            final_result = result*result*num if pow%2 != 0 else result*result
+            return final_result%MOD
 
-            if pow %2 == 0:
-                return ans%MOD
-            else:
-                return (ans*num)%MOD
-        
-        for_even_positions = fun(5,even)
-        for_odd_positions = fun(4,odd)
+        for_even_numbers = fun(5,even)
+        for_prime_numbers = fun(4,odd)
+        return (for_even_numbers*for_prime_numbers)%MOD
 
-        return (for_even_positions * for_odd_positions)%MOD
-        
-        # prime = {2,3,5,7}
-        # MOD = (10**9)+7
- 
-        # def fun(string,position):  
 
-        #     if position == n:
-        #         return 1
-            
-        #     counter = 0  
-        #     for i in range(10):
-        #         if (position %2 == 0 and i%2 == 0) or (position %2 != 0 and i in prime):
-        #             string.append(i)
-        #             position += 1
-        #             counter += fun(string,position)
-        #             string.pop()
-        #             position -= 1
-                
-        #     return counter%MOD
-
-        # final_answer = fun([],0)
-        # return final_answer
