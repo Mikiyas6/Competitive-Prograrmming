3517877df8cb190d5af928909a2a99c487b86b6d
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-08 11:23:14 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (19.14%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,38 +1,12 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-
-        n = rowIndex
-
-        board = [[0]*(n+1) for _ in range(n+1)]
-
-        def store(board,row,col,value):
-            board[row][col] = value
-            return
-
-        def fun(row,col):
-
-            if row == n+1:
-                return
-
-            if row == col:
-                value = 1
-                store(board,row,col,value)
-                fun(row+1,0)
-                return
-            
-            if col == 0:
-                value = 1
-                store(board,row,col,value)
-                fun(row,col+1)
-                return
-            
-            value = board[row-1][col-1] + board[row-1][col]
-            store(board,row,col,value)
-            fun(row,col+1)
-            return
-        
-        fun(0,0)
-        return board[-1]
-        
-
-            
+        numRows = rowIndex+1
+        grid = [[0]*(numRows) for _ in range(numRows)]
+        for i in range(numRows):
+            grid[i][0] = 1
+        for row in range(1,numRows):
+            for col in range(1,numRows):
+                grid[row][col] = grid[row-1][col-1] + grid[row-1][col]
+        for i in range(numRows):
+            grid[i] = grid[i][:i+1]
+        return grid[rowIndex]
