5bd371ac0f9390f9cef2fd040e46a7e87f85e1b6
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-09 12:44:27 +0300

    Time: 0 ms (100.00%) | Memory: 17.1 MB (15.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,59 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-
-        def binary_search(s,e,target):
-
-            if s > e:
+        def fun(start,end,nums,target):
+            if start > end:
                 return -1
-            
-            mid = s + (e-s)//2
-
+            mid = start + (end-start)//2
             if target == nums[mid]:
-                return mid                
-            elif target < nums[mid]:
-                e = mid-1
-            else:
-                s = mid+1
-            
-            return binary_search(s,e,target)
-                  
-        def find_pivot(s,e):
-
-            if s > e:
-                return -1
-            
-            mid = s + (e-s)//2
-
-            if mid+1 < n and nums[mid] > nums[mid+1]:
                 return mid
-            
-            if nums[mid] < nums[mid-1]:
-                return mid-1
-            
-            if nums[s] >= nums[mid]:
-                e = mid-1
+            if nums[start] <= nums[mid]:
+                if nums[start] <= target and target < nums[mid]:
+                    end = mid-1
+                else:
+                    start = mid+1
             else:
-                s = mid+1
-            
-            return find_pivot(s,e)
-         
-        n = len(nums)
-        s, e = 0, n-1
-
-        pivot = find_pivot(s,e)
-
-        if pivot == -1:
-            normal = binary_search(s,e,target)
-            return normal
-        
-        if nums[pivot] == target:
-            return pivot
-        
-        left = binary_search(s,pivot-1,target)
-        right = binary_search(pivot+1,e,target)
-
-        return max(left,right)
-
-
-
-        
+                if nums[mid] < target and target <= nums[end]:
+                    start = mid+1
+                else:
+                    end = mid-1
+            return fun(start,end,nums,target)
+        start = 0
+        end = len(nums)-1
+        return fun(start,end,nums,target)
