5c51643ace96008c7268d47fffdfacc57b76c133
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-12 16:13:04 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (32.08%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -4,55 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-
-        def DFS(root,cumulative):
-
+        
+        def DFS(root,result):
             if not root:
-                return 0
-
+                return "0"
             if not root.left and not root.right:
-                result = (cumulative)*10 + root.val
-                return result
-            
-            result = (cumulative)*10 + root.val
-            
-            return DFS(root.left,result) + DFS(root.right,result)
+                final_result = result+[str(root.val)]
+                return ''.join(final_result)
+            left = DFS(root.left,result+[str(root.val)])
+            right = DFS(root.right,result+[str(root.val)])
+            return int(left)+int(right)
 
-        return DFS(root,0)
-
-##################################################################
-        # total = 0
-
-        # def store(string):
-
-        #     nonlocal total
-            
-        #     string = "".join(string)
-        #     total += int(string)
-        
-        # def DFS(root,string):
-
-        #     if not root:
-        #         return
-
-        #     if not root.left and not root.right:
-        #         store(string+[str(root.val)])
-        #         return
-            
-        #     DFS(root.left,string+[str(root.val)])
-
-        #     DFS(root.right,string+[str(root.val)])
-
-        #     return
-
-        # DFS(root,[])
-
-        # return total
-
-        
-
-
+        return int(DFS(root,[]))
 
