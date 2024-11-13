5784053675b19775d8bb7ca0a746346de4d8a900
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-13 15:30:17 +0300

    Time: 3 ms (76.88%) | Memory: 16.6 MB (34.86%) - LeetSync

--- 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
+++ 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
@@ -1,58 +1,20 @@
 class Solution:
     def splitString(self, s: str) -> bool:
-
-        def dfs(i,prev):
-            if i == len(s):
-                return True
-            
-            for j in range(i,len(s)):
-                string = int(s[i:j+1])
-                if prev[-1] - string == 1:
-                    prev.append(string)
-                    if dfs(j+1,prev):
-                        return True
-                    prev.pop()
-            
-            return False
-
-        prev = []
-        for i in range(len(s)-1):
-
-            string = int(s[:i+1])
-            prev.append(string)
-            if dfs(i+1,prev):
-                return True
-            prev.pop()
+        n = len(s)
         
-        return False
-
-
-        # def dfs(i,splits):
-
-        #     if i == len(s):
-        #         if len(splits) == 1:
-        #             return False
-
-        #         inValue = splits[0]
-        #         for value in splits[1:]:
-        #             if inValue - value != 1:
-        #                 return False
-        #             inValue = value
+        def backtrack(s,path):
+            if not s:
+                if len(path) == 1:
+                    return False
+                return True
                 
-        #         return True
-            
-        #     for j in range(i,len(s)):  # j is the ending of a split
+            for i in range(1,len(s)+1):
+                if not path or int(path[-1]) - int(s[:i]) == 1:
+                    path.append(s[:i])
+                    if backtrack(s[i:],path):
+                        return True
+                    path.pop()
 
-        #         string = int(s[i:j+1])
-        #         splits.append(string)
-        #         if dfs(j+1,splits):
-        #             return True
-        #         splits.pop()
-            
-        #     return False
-    
+            return False
         
-        # return dfs(0,[])
-
-
-            
+        return backtrack(s,[])
