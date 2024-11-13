1ab726ebf3b07012d6c1edce4c4772affb2d608b
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-13 16:27:20 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (51.50%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,27 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-
-        N = len(nums)
-        result = []
-        k = int(math.pow(2,N))
-        for i in range(k):
-            subset = []
-            for j in range(N):
-                if (i & 1 << j):
-                    subset.append(nums[j])
-            result.append(subset)
-        return result
-
-        # def fun(i,subset):
-
-        #     if i == len(nums):
-
-        #         return [subset]
-            
-        #     return fun(i+1,subset+[nums[i]]) + fun(i+1,subset)
         
-        # return fun(0,[])
-            
-            
+        n = len(nums)
+        result = []
 
+        def backtrack(start,path):
+            result.append(path[:])
+            if start == n:
+                return
+            for i in range(start,n):
+                path.append(nums[i])
+                backtrack(i+1,path)
+                path.pop()
 
+        backtrack(0,[])
+        return result
