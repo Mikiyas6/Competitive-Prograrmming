20652b147a2bfbc737d1d7216072b1bed0fbed08
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-13 13:36:07 +0300

    Time: 131 ms (65.3%), Space: 58.7 MB (77.05%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,18 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-
-        combinations = []
         
-        def dfs(start,combo):
+        result = []
 
-            if len(combo) == k:
-                combinations.append(combo[:])
+        def backtrack(start,path):
+            if len(path) == k:
+                result.append(path[:])
                 return
             
             for i in range(start,n+1):
-                combo.append(i)
-                dfs(i+1,combo)
-                combo.pop()
-
-        dfs(1,[])
-        return combinations          
+                path.append(i)
+                backtrack(i+1,path)
+                path.pop()
+        
+        backtrack(1,[])
+            
+        return result
