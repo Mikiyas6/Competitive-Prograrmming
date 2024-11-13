09b297811faeb07caac64d661d0cbaa529e645d6
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-13 19:58:16 +0300

    Time: 6203 ms (13.39%), Space: 16.8 MB (24.07%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,42 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        m, n = len(board), len(board[0])
 
-        rowLength, colLength = len(board), len(board[0])
-        directions = [[0,1],[1,0],[-1,0],[0,-1]]
-        path = [[False]*colLength for _ in range(rowLength)]
+        def inbound(row, col):
+            return 0 <= row < m and 0 <= col < n
 
-        def inbound(row,col):
-            return 0 <= row and row < rowLength and 0 <= col and col < colLength
-        
-        def isVisited(row,col):
-            return path[row][col]
-
-        def dfs(i,row,col):
-
-            if i == len(word):
+        def backtrack(row, col, index, visited):
+            if index == len(word):  
                 return True
-
-            if not inbound(row,col) or board[row][col] != word[i] or isVisited(row,col):
+            if not inbound(row, col) or (row, col) in visited or board[row][col] != word[index]:
                 return False
             
-            path[row][col] = True
-
-            for dx,dy in directions:
-                newRow = row + dx
-                newCol = col + dy
-
-                if dfs(i+1,newRow,newCol):
+            visited.add((row, col))  
+            for dx, dy in directions:
+                newRow, newCol = row + dx, col + dy
+                if backtrack(newRow, newCol, index + 1, visited):
                     return True
-            
-            path[row][col] = False
-            
+            visited.remove((row, col))  
             return False
         
-        for row in range(rowLength):
-            for col in range(colLength):
-                if board[row][col] == word[0]:
-                    if dfs(0,row,col):
+        for row in range(m):
+            for col in range(n):
+                if board[row][col] == word[0]:  
+                    if backtrack(row, col, 0, set()):
                         return True
-        
         return False
-            
