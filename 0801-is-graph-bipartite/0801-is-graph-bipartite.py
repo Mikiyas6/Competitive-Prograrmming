d28dcff22a15566b13241b9d4d95559b32a9fc0d
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-15 01:14:20 +0300

    Time: 95 ms (5.07%), Space: 17.2 MB (39.92%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,37 +1,28 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
 
-        color = [0]*len(graph)
+        def BFS():
 
-        def bfs(i):
-
-            if color[i]:
-                return True
-            
-            queue = deque([i])
-            color[i] = -1
-
-            while queue:
-
-                level = len(queue)
-                node = queue.popleft()
-
-                for _ in range(level):
-                    neighbors = graph[node]
-                    for neighbor in neighbors:
-                        if color[neighbor] and color[neighbor] == color[node]:
-                            return False
-
-                        elif not color[neighbor]:
-                            color[neighbor] = color[node] * -1
-                            queue.append(neighbor)
-            
+            for i in range(len(graph)):
+                queue = deque([i])
+                hashmap = defaultdict(int)
+                hashmap[i] = 0
+                visited = set()
+                while queue:
+                    level = len(queue)
+                    for _ in range(level):
+                        node = queue.popleft()
+                        neighbors = graph[node]
+                        color = hashmap[node]
+                        for neighbor in neighbors:
+                            if neighbor in visited:
+                                if hashmap[neighbor] == color:
+                                    return False
+                            else:
+                                visited.add(neighbor)
+                                hashmap[neighbor] = 1 if color == 0 else 0
+                                queue.append(neighbor)
             return True
-        
-        for i in range(len(graph)):
-            if not bfs(i):
-                return False
-        
-        return True
 
-                        
+        return BFS()    
+                    
