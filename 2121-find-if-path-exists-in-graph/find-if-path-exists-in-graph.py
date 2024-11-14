ca0b7d3e7f6d42efaa7139c7b3fd0563c264e7f8
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-14 19:43:03 +0300

    Time: 604 ms (40.95%) | Memory: 140.4 MB (13.64%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,22 +1,40 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        
-        # Build the adjacency list representation of the graph
-        adj_list = defaultdict(list)
-        for u, v in edges:
-            adj_list[u].append(v)
-            adj_list[v].append(u)
-        
-        # Perform DFS from the source vertex
-        visited = set()
-        stack = [source]
-        while stack:
-            current = stack.pop()
-            if current == destination:
+
+        def Create_Adjacency_List(edges):
+
+            from collections import defaultdict
+
+            Graph = defaultdict(list)
+
+            for edge in edges:
+                Graph[edge[0]].append(edge[1])
+                Graph[edge[1]].append(edge[0])
+            return Graph
+
+        def dfs(node,visited):
+
+            if node == destination:
                 return True
-            visited.add(current)
-            for neighbor in adj_list[current]:
+            
+            
+            for neighbor in Graph[node]:
                 if neighbor not in visited:
-                    stack.append(neighbor)
+                    visited.add(node)
+                    result = dfs(neighbor,visited)
+                    if result:
+                        return True
+                else:
+                    continue
+            return False
+
+        Graph = Create_Adjacency_List(edges)
+
+        visited = set()
+
+        boolean = dfs(source,visited)
+
+        return boolean
         
-        return False
+            
+            
