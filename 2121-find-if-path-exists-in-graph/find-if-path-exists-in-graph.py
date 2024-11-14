d305f5cd1c3da24fc4994d387cd80f9923cfb190
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-14 19:46:20 +0300

    Time: 658 ms (27.59%) | Memory: 139.3 MB (22.52%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,40 +1,25 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-
-        def Create_Adjacency_List(edges):
-
-            from collections import defaultdict
-
-            Graph = defaultdict(list)
-
-            for edge in edges:
-                Graph[edge[0]].append(edge[1])
-                Graph[edge[1]].append(edge[0])
-            return Graph
-
-        def dfs(node,visited):
-
-            if node == destination:
-                return True
-            
-            
-            for neighbor in Graph[node]:
-                if neighbor not in visited:
-                    visited.add(node)
-                    result = dfs(neighbor,visited)
-                    if result:
-                        return True
-                else:
-                    continue
-            return False
-
-        Graph = Create_Adjacency_List(edges)
-
-        visited = set()
-
-        boolean = dfs(source,visited)
-
-        return boolean
         
+        graph = defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        def backtrack(source,visited):
+
+            if source == destination:
+                return True
+
+            neighbors = graph[source]
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    if backtrack(neighbor,visited):
+                        return True
             
-            
+            return False
+        
+        visited = set()
+        visited.add(source)
+        return backtrack(source,visited)
