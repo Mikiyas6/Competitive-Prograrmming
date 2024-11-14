1364bcfcf42307b11f1406ba99f5adb8b0adfe2a
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-15 01:33:13 +0300

    Time: 269 ms (26.85%) | Memory: 24.5 MB (17.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,31 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        visited = set()
-        directions = [[0,1],[1,0],[0,-1],[-1,0]]
-
         def inbound(row,col):
-            return 0 <= row and row < len(grid) and 0 <= col and col < len(grid[0])
-
-        def dfs(row,col): 
+            return 0 <= row and row < m and 0 <= col and col < n
+        
+        def backtrack(row,col,visited):
 
             for dx, dy in directions:
-                newRow = row+dx
-                newCol = col+dy
-
-                if (newRow,newCol) not in visited and inbound(newRow,newCol) and grid[newRow][newCol] == '1':
+                newRow = row + dx
+                newCol = col + dy
+                if inbound(newRow,newCol) and (newRow,newCol) not in visited and grid[newRow][newCol] == '1':
                     visited.add((newRow,newCol))
-                    dfs(newRow,newCol)
-            
-            return
+                    backtrack(newRow,newCol,visited)
         
-        counter = 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if (row,col) not in visited and grid[row][col] == '1':
-                    counter += 1
-                    dfs(row,col)
-        
-        return counter
-            
-
+        m = len(grid)
+        n = len(grid[0])
+        count = 0
+        visited = set()
+        directions = [[0,-1],[-1,0],[0,1],[1,0]]
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] == '1' and (row,col) not in visited:
+                    visited.add((row,col))
+                    backtrack(row,col,visited)
+                    count += 1
+        return count
