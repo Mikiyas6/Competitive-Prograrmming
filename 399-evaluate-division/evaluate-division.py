d78996dd8b1d25422ea292ac5a81b720bb5c9750
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-15 21:26:47 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (46.79%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,35 +1,37 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = {}
-        
-        def add_edge(f: str, t: str, value: float):
-            if f not in graph:
-                graph[f] = {}
-            graph[f][t] = value
-        
-        for (dividend, divisor), value in zip(equations, values):
-            add_edge(dividend, divisor, value)
-            add_edge(divisor, dividend, 1 / value)
-        
-        def dfs(start: str, end: str, visited: Set[str]) -> float:
-            if start not in graph or end not in graph:
-                return -1.0
-            if start == end:
-                return 1.0
-            visited.add(start)
-            for neighbor, value in graph[start].items():
-                if neighbor in visited:
-                    continue
-                result = dfs(neighbor, end, visited)
-                if result != -1.0:
-                    return result * value
-            return -1.0
-        
-        results = []
-        for dividend, divisor in queries:
-            if dividend == divisor and dividend in graph:
-                results.append(1.0)
+        graph = defaultdict(list)
+        n = len(equations)
+        # Building Adjacency List
+        for i in range(n):
+            A,B = equations[i]
+            graph[A].append([B,values[i]])
+            graph[B].append([A,1/values[i]])
+
+        def dfs(graph, start, end, path, visited):
+            if start == end:  
+                return path
+
+            for neighbor in graph[start]:
+                node, weight = neighbor
+                if node not in visited:
+                    visited.add(node)
+                    result = dfs(graph, node, end, path * weight, visited)
+                    if result != -1:  
+                        return result
+            return -1  
+
+        output = []
+        for query in queries:
+            node1,node2 = query
+            if node1 not in graph or node2 not in graph:
+                output.append(-1)
+            elif node1 == node2:
+                output.append(1)
             else:
-                results.append(dfs(dividend, divisor, set()))
+                visited = set()
+                visited.add(node1)
+                result = dfs(graph,node1,node2,1,visited)
+                output.append(result)
         
-        return results
+        return output
