fc48efe2cd944ed54930372f23cf9f47cfd80ec1
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-16 10:24:17 +0300

    Time: 270 ms (26.05%), Space: 24.3 MB (28.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,25 +3,25 @@ class Solution:
 
         def inbound(row,col):
             return 0 <= row and row < m and 0 <= col and col < n
-        
-        def backtrack(row,col,visited):
+
+        def DFS(row,col,visited):
 
             for dx, dy in directions:
                 newRow = row + dx
                 newCol = col + dy
-                if inbound(newRow,newCol) and (newRow,newCol) not in visited and grid[newRow][newCol] == '1':
+                if inbound(newRow,newCol) and (newRow,newCol) not in visited and grid[newRow][newCol] == "1":
                     visited.add((newRow,newCol))
-                    backtrack(newRow,newCol,visited)
-        
+                    DFS(newRow,newCol,visited)
+
         m = len(grid)
         n = len(grid[0])
-        count = 0
         visited = set()
-        directions = [[0,-1],[-1,0],[0,1],[1,0]]
+        counter = 0
+        directions = [[-1,0],[1,0],[0,-1],[0,1]]
         for row in range(m):
             for col in range(n):
-                if grid[row][col] == '1' and (row,col) not in visited:
+                if (row,col) not in visited and grid[row][col] == '1':
                     visited.add((row,col))
-                    backtrack(row,col,visited)
-                    count += 1
-        return count
+                    counter += 1
+                    DFS(row,col,visited)
+        return counter
