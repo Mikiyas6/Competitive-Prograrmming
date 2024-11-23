97e1ddee2f2bd736a4890d9949f8c5eaad014981
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-24 01:20:58 +0300

    Time: 2142 ms (5.12%), Space: 24.3 MB (39.5%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,50 +8,44 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-
-        #Functin to find the middle of LinkedList
         def findMiddle(head):
             if not head or not head.next:
                 return head
             slow = head
             fast = head.next
-
             while fast and fast.next:
                 slow = slow.next
                 fast = fast.next.next
             return slow
-
-        #Function to reverse Linkedlist
-        def reverse(head):
-            prev = None
-            current = head
-
-            while current:
-                next_node = current.next  
-                current.next = prev       
-                prev = current            
-                current = next_node       
-
-            return prev  
-
-        def fun(list1,list2):
-            if not list1:
-                return list2
-            if not list2:
-                return list1
-            
-            segment1 = list1.next
-            segment2 = list2.next
-            list1.next = list2
-            list2.next = fun(segment1,segment2)
-            return list1
-            
         
+        def reverse(head):
+            if not head or not head.next:
+                return head
+            
+            second_part = head.next
+            head.next = None
+            reversed_second_part = reverse(second_part)
+            current = reversed_second_part
+            while current.next:
+                current = current.next
+            current.next = head
+            return reversed_second_part
         middle = findMiddle(head)
-        list2 = middle.next
+        second_part = middle.next
         middle.next = None
-        list1 = head
-
-        reversed_list2 = reverse(list2)
-
-        fun(list1,reversed_list2)
+        first_part = head
+        reversed_second_part = reverse(second_part)
+        def fun(part1,part2):
+            if not part2:
+                return part1
+            if not part1.next and not part2.next:
+                part1.next = part2
+                return part1
+            second_1 = part1.next
+            part1.next = None
+            second_2 = part2.next
+            part2.next = None
+            part1.next = part2
+            part2.next = fun(second_1,second_2)
+            return part1
+        fun(first_part,reversed_second_part)
