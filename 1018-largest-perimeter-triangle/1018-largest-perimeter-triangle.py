d5980bf10500525db1f775f2431d8db36f523d06
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-23 13:58:21 +0300

    Time: 42 ms (5.61%), Space: 17.8 MB (50.29%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.py
@@ -4,18 +4,9 @@ class Solution:
         if n < 3:
             return 0
         nums.sort()
-        first_side,second_side,third_side = nums[0],nums[1],nums[2]
-        total = first_side + second_side
-        if total <= third_side:
-            maxTotal = 0
-        else:
-            maxTotal = total + third_side
-        left = 0
-        for right in range(3,n):
-            total -= nums[left]
-            total += nums[right-1]
-            left += 1
-            if total <= nums[right]:
-                continue
-            maxTotal = max(maxTotal,total+nums[right])
-        return maxTotal
+        maxArea = 0
+        for index in range(2,n):
+            if nums[index-2] + nums[index-1] > nums[index]:
+                area = nums[index-2] + nums[index-1] + nums[index]
+                maxArea = max(maxArea,area)
+        return maxArea
