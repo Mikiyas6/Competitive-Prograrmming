fb92a550341162701c90d93b17c4f116d24725e8
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-23 19:58:46 +0300

    Time: 1 ms (80.99%) | Memory: 16.9 MB (7.37%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -8,50 +8,30 @@ class Solution:
         
         dummy = ListNode(-1)
         current = dummy
-        ptr1,ptr2,carry = l1,l2,0
 
         def fun(current,ptr1,ptr2,carry):
-
+            
             if not ptr1 and not ptr2:
-
-                if carry > 0:
-
-                    Node = ListNode(carry)
-
-                    current.next = Node
-
+                if carry>0:
+                    node = ListNode(carry)
+                    current.next = node
                 return current
-            
-            if not ptr2:
-
-                total = ptr1.val + carry
-                ptr1 = ptr1.next
-            
-            elif not ptr1:
-
+            if not ptr1:
                 total = ptr2.val + carry
-                ptr2 = ptr2.next
-            
-            else:
-            
-                total = ptr1.val + ptr2.val + carry
-                ptr1 = ptr1.next
-                ptr2 = ptr2.next
-
-            result = total % 10
-            carry = total // 10
-
-            Node = ListNode(result)
-
-            current.next = fun(Node,ptr1,ptr2,carry)
-
+                node = ListNode(total%10)
+                carry = total//10
+                current.next = fun(node,None,ptr2.next,carry)
+                return current
+            if not ptr2:
+                total = ptr1.val + carry
+                node = ListNode(total%10)
+                carry = total//10
+                current.next = fun(node,ptr1.next,None,carry)
+                return current
+            total = ptr1.val + ptr2.val + carry
+            node = ListNode(total%10)
+            carry = total//10
+            current.next = fun(node,ptr1.next,ptr2.next,carry)
             return current
         
-        return fun(current,ptr1,ptr2,0).next
-
-
-
-
-
-
-
+        return fun(current,l1,l2,0).next
