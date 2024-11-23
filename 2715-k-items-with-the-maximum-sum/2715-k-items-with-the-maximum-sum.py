4f27d7740316e62e2f1e8aa09490ec155acb4d77
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-23 12:22:14 +0300

    Time: 7 ms (7.98%), Space: 16.7 MB (28.34%) - LeetHub

--- 2715-k-items-with-the-maximum-sum/2715-k-items-with-the-maximum-sum.py
+++ 2715-k-items-with-the-maximum-sum/2715-k-items-with-the-maximum-sum.py
@@ -1,12 +1,10 @@
 class Solution:
     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
-        ones_to_pick = min(numOnes, k)
-        k -= ones_to_pick
-        
-        zeros_to_pick = min(numZeros, k)
-        k -= zeros_to_pick
-    
-        neg_ones_to_pick = min(numNegOnes, k)
-        k -= neg_ones_to_pick
-        
-        return ones_to_pick - neg_ones_to_pick
+        array = []
+        for i in range(numOnes):
+            array.append(1)
+        for i in range(numZeros):
+            array.append(0)
+        for i in range(numNegOnes):
+            array.append(-1)
+        return sum(array[:k])
