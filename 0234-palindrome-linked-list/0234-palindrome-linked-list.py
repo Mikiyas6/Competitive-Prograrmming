b8e9886dd1dd502d32d2103ce6743a006045cc31
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-24 13:55:35 +0300

    Time: 27 ms (75.68%), Space: 33.9 MB (54.66%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,51 +5,40 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return head
-        #Functin to find the middle of LinkedList
+
+        if not head or not head.next:
+            return True
+        
         def findMiddle(head):
             if not head or not head.next:
                 return head
             slow = head
             fast = head.next
-
             while fast and fast.next:
                 slow = slow.next
                 fast = fast.next.next
             return slow
-
-        #Function to reverse Linkedlist
+        
         def reverse(head):
-            prev = None
-            current = head
-
+            prev = None  
+            current = head 
             while current:
-                next_node = current.next  
-                current.next = prev       
-                prev = current            
-                current = next_node       
+                next_node = current.next 
+                current.next = prev 
+                prev = current 
+                current = next_node 
+            return prev 
 
-            return prev  
-
-        #Find the middle and break it in half
         middle = findMiddle(head)
         second_part = middle.next
         middle.next = None
         first_part = head
-
-        # Reverse the second part
-        reversedList = reverse(second_part)
-        
-        # Use two pointers to check the value in each node
+        reversed_second_part = reverse(second_part)
         ptr1 = first_part
-        ptr2 = reversedList
-
+        ptr2 = reversed_second_part
         while ptr1 and ptr2:
             if ptr1.val != ptr2.val:
                 return False
             ptr1 = ptr1.next
             ptr2 = ptr2.next
-        
-        return True
-
+        return True 
