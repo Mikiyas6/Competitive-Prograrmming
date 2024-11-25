8aed9510bcd408f462402cc6981be5db92f4620d
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2024-11-25 17:52:23 +0300

    Time: 0 ms (100%), Space: 17 MB (57.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def fun(start,end,nums,target):
+        def binary(start,end,nums,target):
             if start > end:
                 return -1
             mid = start + (end-start)//2
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
             if nums[start] <= nums[mid]:
                 if nums[start] <= target and target < nums[mid]:
@@ -16,7 +16,7 @@ class Solution:
                     start = mid+1
                 else:
                     end = mid-1
-            return fun(start,end,nums,target)
+            return binary(start,end,nums,target)
         start = 0
         end = len(nums)-1
-        return fun(start,end,nums,target)
+        return binary(start,end,nums,target)
