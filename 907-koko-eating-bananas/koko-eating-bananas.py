a9dd7b78d370284f029b32b3d4cd3bc0b62f6bb3
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-01-13 23:35:19 +0300

    Time: 177 ms (27.08%) | Memory: 18.9 MB (35.71%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,26 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def findHours(piles,k):
-            count = 0
+
+        def findK(k):
+            time = 0
             for pile in piles:
                 if pile <= k:
-                    count += 1
+                    time += 1
                 else:
-                    count += math.ceil(pile/k)
-            return count
-
-        def findK(start,end,piles,h):
-            if start > end:
-                return start
-            k = start + (end-start)//2
-            hours = findHours(piles,k)
-            if hours <= h:
-                end = k-1
-            else:
-                start = k+1
-            return findK(start,end,piles,h)
-
+                    time += math.ceil(pile/k)
+            return time
+        
         start = 1
         end = max(piles)
-        return findK(start,end,piles,h)
+        while start <= end:
+            mid = start + (end-start)//2
+            result = findK(mid)
+            if result > h:
+                start = mid+1
+            else:
+                end = mid-1
+        return start
         
+            
