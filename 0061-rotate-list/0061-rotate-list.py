12e4ac21ee5b0f307c120ae62ffed93e644cd57e
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-01-13 15:49:05 +0300

    Time: 0 ms (100%), Space: 17.9 MB (24.46%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,40 +5,34 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-
+        if not head:
+            return head
         def reverse(head):
             prev = None
             current = head
-
             while current:
-                next_node = current.next  
-                current.next = prev       
-                prev = current            
-                current = next_node       
-
-            return prev  
-       
-        if not head or not head.next:
-            return head
-        n = 0
+                next = current.next
+                current.next = prev
+                prev = current
+                current = next
+            return prev
+        size = 0
         current = head
         while current:
+            size += 1
             current = current.next
-            n += 1
-        k %= n
-        end_up = n - k - 1
+        end_up = size-(k%size)- 1
         current = head
-        for i in range(end_up):
+        for _ in range(end_up):
             current = current.next
-        second_part = current.next
+        second_half = current.next
         current.next = None
-        first_part = head
-        reversed_first_part = reverse(first_part)
-        reversed_second_part = reverse(second_part)
-        current = reversed_first_part
+        first_half = head
+        reversed_first_half = reverse(first_half)
+        reversed_second_half = reverse(second_half)
+        current = reversed_first_half
         while current.next:
             current = current.next
-        current.next = reversed_second_part
-        final_result = reverse(reversed_first_part)
-        return final_result
-        
+        current.next = reversed_second_half
+        result = reverse(reversed_first_half)
+        return result
