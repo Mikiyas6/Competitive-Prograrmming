8dfa3ae48a48b9a466198c85e363cb5ce4fda4ac
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-01-13 15:30:36 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (40.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,30 +5,24 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        def merge(list1,list2):
-            dummy = ListNode(-1)
-            current = dummy
-            node1 = list1
-            node2 = list2
-            def merger(current,list1,list2):
-                if not list1:
-                    current.next = list2
-                    return current
-                if not list2:
-                    current.next = list1
-                    return current
-                if list1.val <= list2.val:
-                    value = list1.val
-                    list1 = list1.next
-                else:
-                    value = list2.val
-                    list2 = list2.next
-                node = ListNode(value)
-                current.next = merger(node,list1,list2)
+        dummy = ListNode(-1)
+        current = dummy
+        node1 = list1
+        node2 = list2
+        def merge(current,list1,list2):
+            if not list1:
+                current.next = list2
                 return current
-            return merger(current,node1,node2).next
-        return merge(list1,list2)
+            if not list2:
+                current.next = list1
+                return current
+            if list1.val <= list2.val:
+                value = list1.val
+                list1 = list1.next
+            else:
+                value = list2.val
+                list2 = list2.next
+            node = ListNode(value)
+            current.next = merge(node,list1,list2)
+            return current
+        return merge(current,list1,list2).next
