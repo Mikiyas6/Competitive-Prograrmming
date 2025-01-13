8a105378d9bd364ed807b46c81106815a8e68c89
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-01-13 16:14:00 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (19.73%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -5,42 +5,26 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
         if not head or not head.next:
-
             return head
-        
         dummy = ListNode(-1)
         current = dummy
-
-        def fun(current,ptr1,ptr2,visited):
-
+        ptr1 = head
+        ptr2 = head.next
+        def remover(current,ptr1,ptr2,flag):
             if not ptr2:
-
-                if ptr1.val not in visited:
-
-                    Node = ListNode(ptr1.val)
-
-                    current.next = Node
-                
+                if ptr1.val != flag:
+                    node = ListNode(ptr1.val)
+                    current.next = node
                 return current
-            
             if ptr1.val != ptr2.val:
-
-                if ptr1.val not in visited:
-
-                    Node = ListNode(ptr1.val)
-                    visited.add(ptr1.val)
-                    current.next = fun(Node,ptr1.next,ptr2.next,visited)
-
-                    return current
-
-            visited.add(ptr1.val)
-            return fun(current,ptr1.next,ptr2.next,visited)
-
-        return fun(current,head,head.next,set()).next
-
-
-
-
-        
+                if ptr1.val != flag:
+                    node = ListNode(ptr1.val)
+                else:
+                    return remover(current,ptr1.next,ptr2.next,flag)
+            else:
+                flag = ptr1.val
+                return remover(current,ptr1.next,ptr2.next,flag)
+            current.next = remover(node,ptr1.next,ptr2.next,flag)
+            return current
+        return remover(current,ptr1,ptr2,-101).next
