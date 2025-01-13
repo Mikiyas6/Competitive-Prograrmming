a89b1dc77cf65571026fdc1e2eacf0e522fa3aca
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-01-13 17:44:33 +0300

    Time: 7 ms (23.51%), Space: 25.2 MB (5.17%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,7 +8,7 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        def findMiddle(head):
+        def find_middle(head):
             if not head or not head.next:
                 return head
             slow = head
@@ -17,35 +17,29 @@ class Solution:
                 slow = slow.next
                 fast = fast.next.next
             return slow
-        
         def reverse(head):
-            if not head or not head.next:
-                return head
-            
-            second_part = head.next
-            head.next = None
-            reversed_second_part = reverse(second_part)
-            current = reversed_second_part
-            while current.next:
-                current = current.next
-            current.next = head
-            return reversed_second_part
-        middle = findMiddle(head)
-        second_part = middle.next
+            prev = None
+            current = head
+            while current:
+                next = current.next
+                current.next = prev
+                prev = current
+                current = next
+            return prev
+        middle = find_middle(head)
+        second_half = middle.next
         middle.next = None
-        first_part = head
-        reversed_second_part = reverse(second_part)
-        def fun(part1,part2):
-            if not part2:
-                return part1
-            if not part1.next and not part2.next:
-                part1.next = part2
-                return part1
-            second_1 = part1.next
-            part1.next = None
-            second_2 = part2.next
-            part2.next = None
-            part1.next = part2
-            part2.next = fun(second_1,second_2)
-            return part1
-        fun(first_part,reversed_second_part)
+        ptr1 = head
+        ptr2 = reverse(second_half)
+        def fun(ptr1,ptr2):
+            if not ptr1 and not ptr2:
+                return None
+            if not ptr2 and ptr1:
+                return ptr1
+            next1 = ptr1.next
+            next2 = ptr2.next
+            ptr1.next = ptr2
+            ptr2.next = fun(next1,next2)
+            return ptr1
+        fun(ptr1,ptr2)
+
