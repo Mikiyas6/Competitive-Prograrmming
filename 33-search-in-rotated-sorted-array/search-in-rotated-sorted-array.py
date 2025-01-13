0eb994eb2838fdb4ce09a10a3d5a2c8c9801a516
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-01-13 20:57:20 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (30.30%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,22 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def fun(start,end,nums,target):
+        
+        def find(nums,start,end,target):
             if start > end:
                 return -1
             mid = start + (end-start)//2
             if target == nums[mid]:
                 return mid
             if nums[start] <= nums[mid]:
-                if nums[start] <= target and target < nums[mid]:
+                if target >= nums[start] and target < nums[mid]:
                     end = mid-1
                 else:
                     start = mid+1
             else:
-                if nums[mid] < target and target <= nums[end]:
+                if target > nums[mid] and target <= nums[end]:
                     start = mid+1
                 else:
                     end = mid-1
-            return fun(start,end,nums,target)
+            return find(nums,start,end,target)
+        
         start = 0
         end = len(nums)-1
-        return fun(start,end,nums,target)
+        return find(nums,start,end,target)
