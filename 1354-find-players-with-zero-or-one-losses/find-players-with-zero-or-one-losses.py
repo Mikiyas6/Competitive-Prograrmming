7c9e648a95112ea7b9c5f53ff050caac0ac654f4
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-02-03 12:52:17 +0300

    Time: 2222 ms (5.05%) | Memory: 69.2 MB (5.00%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -1,31 +1,20 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        hashmap = defaultdict(list)
+        visited = set()
+        length = 0
+        for winner,loser in matches:
+            hashmap[loser].append(winner)
+            length = max(length,max(winner,loser))
+            visited.add(loser)
+            visited.add(winner)
+        ans1 = []
+        ans2 = []
+        for i in range(1,length+1):
+            if i in visited and i not in hashmap.keys():
+                ans1.append(i)
+            if hashmap[i] and len(hashmap[i]) == 1:
+                ans2.append(i)
+        return [ans1,ans2]
         
-        hashmap_count = defaultdict(int)
-        hashmap_streaks = defaultdict(int)
-
-        for winner, loser in matches:
-
-            hashmap_count[winner] += 1
-            hashmap_count[loser] += 1
-
-            hashmap_streaks[winner] += 1
-            hashmap_streaks[loser] -= 1
         
-        havent_lost = []
-        lost_one = []
-        
-        for value in hashmap_count:
-
-            if hashmap_count[value] == hashmap_streaks[value]:
-
-                havent_lost.append(value)
-            
-            elif hashmap_count[value] - 2 == hashmap_streaks[value]:
-
-                lost_one.append(value)
-        
-        havent_lost.sort()
-        lost_one.sort()
-        
-        return [havent_lost,lost_one]
