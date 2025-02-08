501417acf6dc577b460cbdd9cc16accb6819a747
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-02-08 14:38:23 +0300

    Time: 44 ms (14.15%) | Memory: 17.9 MB (29.23%) - LeetSync

--- 1449-print-words-vertically/print-words-vertically.py
+++ 1449-print-words-vertically/print-words-vertically.py
@@ -1,28 +1,11 @@
 class Solution:
     def printVertically(self, s: str) -> List[str]:
-        my_list=s.split(" ")
-        my_final_result=[]
-        max_length_item = max(my_list, key=len)
-        for index in range(len(max_length_item)):
-            for item in my_list:
-                if(index<len(item)):
-                    my_final_result.append(item[index])
-                else:
-                    my_final_result.append(" ")
-            my_final_result.append(",")
-        my_final_result.pop()   
-        ###################################
+        words = s.split(" ")
+        max_length = len(max(words,key=len))
         result = []
-        string = []
-        for value in "".join(my_final_result):
-            if value == ",":
-                result.append("".join(string).rstrip(" "))
-                string = []
-            else:
-                string.append(value)
-        result.append("".join(string).rstrip(" "))
+        for j in range(max_length):
+            string = []
+            for i in range(len(words)):
+                string.append(words[i][j] if j < len(words[i]) else " ")
+            result.append(("".join(string)).rstrip(" "))
         return result
-        #####################################
-        new_list=[item.rstrip(" ") for item in my_final_result]  
-        temp="".join(new_list)    
-        return temp.split(",")
