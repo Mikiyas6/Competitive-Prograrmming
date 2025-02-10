e6e62a93dc54c73f8dc71af88c3c37c529d4a4aa
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-02-10 16:56:10 +0300

    Time: 5 ms (60.81%) | Memory: 17.7 MB (75.40%) - LeetSync

--- 829-subdomain-visit-count/subdomain-visit-count.py
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -1,28 +1,14 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        
         hashmap = defaultdict(int)
-
         for cpdomain in cpdomains:
-
-            number_of_visit, domains = cpdomain.split()
-
-            number_of_visit = int(number_of_visit)
-
-            domains = domains.split(".")
-
-            for i in range(len(domains)-1,-1,-1):
-
-                domain = ".".join(domains[i:])
-
-                hashmap[domain] += number_of_visit
-        
-        output = []
-
-        for value in hashmap:
-
-            answer = " ".join([str(hashmap[value]), value])
-
-            output.append(answer)
-        
-        return output
+            visit_times,domain = cpdomain.split()
+            visit_times = int(visit_times)
+            splitted_domain = domain.split(".")
+            n = len(splitted_domain)
+            for i in range(n):
+                hashmap[".".join(splitted_domain[i:])] += visit_times
+        result = []
+        for key,value in hashmap.items():
+            result.append(f"{value} {key}")
+        return result
