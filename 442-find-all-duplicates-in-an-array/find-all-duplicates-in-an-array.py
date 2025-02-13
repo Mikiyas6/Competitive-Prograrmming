ca734fde13fb84caf7d82dbede6d697676b78936
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-02-13 09:49:31 +0300

    Time: 31 ms (48.70%) | Memory: 36.4 MB (7.21%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,6 +1,4 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        
         hashmap = Counter(nums)
-
         return [value for value in hashmap if hashmap[value] == 2]
