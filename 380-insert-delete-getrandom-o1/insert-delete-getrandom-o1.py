dd0def3327030442651912dc068675eeb6840a80
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-02-13 11:00:45 +0300

    Time: 303 ms (22.48%) | Memory: 57.9 MB (62.91%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,26 +1,29 @@
+
 class RandomizedSet:
 
     def __init__(self):
-        self.set = set()
+        self.hashmap = defaultdict(int) 
 
     def insert(self, val: int) -> bool:
-        if val in self.set:
+        if val in self.hashmap:
             return False
-        self.set.add(val)
+        self.hashmap[val] += 1
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.set:
-            return False
-        self.set.remove(val)
-        return True
+        if val in self.hashmap:
+            del self.hashmap[val]
+            return True
+        return False
 
     def getRandom(self) -> int:
-        return random.choice(list(self.set))
+        import random
+        random = random.choice(list(self.hashmap.keys()))
+        return random
 
 
-# # Your RandomizedSet object will be instantiated and called as such:
-# # obj = RandomizedSet()
-# # param_1 = obj.insert(val)
-# # param_2 = obj.remove(val)
-# # param_3 = obj.getRandom()
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
