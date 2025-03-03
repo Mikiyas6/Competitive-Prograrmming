45c4027896b03c9eaf83f4fb85792709353dc0e7
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-03 15:13:24 +0300

    Time: 118 ms (9.28%) | Memory: 20.2 MB (58.14%) - LeetSync

--- 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
@@ -1,27 +1,18 @@
 class Solution:
     def maxSum(self, grid: List[List[int]]) -> int:
-        
-        row_length = len(grid)
-        col_length = len(grid[0])
-
+        def inbound(row,col):
+            return 0 <= row and row < len(grid) and 0 <= col and col < len(grid[0])
         max_sum = 0
-
-        for row in range(1,row_length-1):
-
-            for col in range(1,col_length-1):
-
-                sum = grid[row][col]
-
-                left_diagonals = grid[row+1][col-1] + grid[row-1][col-1]
-
-                right_diagonals = grid[row+1][col+1] + grid[row-1][col+1]
-
-                verticals = grid[row+1][col] + grid[row-1][col]
-
-                sum += left_diagonals + right_diagonals + verticals
-
-                max_sum = max(max_sum,sum)
-        
+        for i in range(1,len(grid)):
+            for j in range(1,len(grid[0])):
+                if inbound(i-1,j) and inbound(i-1,j-1) and inbound(i-1,j+1)and inbound(i+1,j) and inbound(i+1,j-1) and inbound(i+1,j+1):
+                    current = grid[i][j]
+                    top = grid[i-1][j] 
+                    top_left = grid[i-1][j-1] 
+                    top_right = grid[i-1][j+1] 
+                    bottom = grid[i+1][j] 
+                    bottom_left = grid[i+1][j-1] 
+                    bottom_right = grid[i+1][j+1] 
+                    total =  current + top + top_left + top_right + bottom + bottom_left + bottom_right
+                    max_sum = max(max_sum,total)
         return max_sum
-
-
