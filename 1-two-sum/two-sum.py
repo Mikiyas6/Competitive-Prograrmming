652ea6371a2eb94b4de7ccd5fabdf9573bb33f3c
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-07 19:55:04 +0300

    Time: 0 ms (100.00%) | Memory: 18.8 MB (50.96%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,15 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
         hashmap = defaultdict(int)
-
-        for index, value in enumerate(nums):
-
-            result = target - value
-
+        for index,value in enumerate(nums):
+            result = target-value
             if result in hashmap:
-
                 return [hashmap[result],index]
-
             hashmap[value] = index
-
+        
