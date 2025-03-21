40b18a95357702b0155c43d085e203c5ce9fb593
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-21 11:20:52 +0300

    Time: 3 ms (87.29%) | Memory: 18.1 MB (5.22%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.py
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -3,12 +3,8 @@ class Solution:
         hashmap = defaultdict(int)
         for email in emails:
             local, domain = email.split("@")
-            print("local->",local)
-            print("domain->",domain)
             local = "".join(local.split("."))
             local = local.split("+")[0]
-            print("local->",local)
             final_email = "@".join([local,domain])
-            print("final_email->",final_email)
             hashmap[final_email] += 1
         return len(hashmap)
