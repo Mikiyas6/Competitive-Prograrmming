c14d05e9c8e9942b0d1f8a82ba70c0cd63ece9f6
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-21 12:04:13 +0300

    Time: 7 ms (57.09%) | Memory: 18 MB (55.03%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,16 +1,32 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+        # hashset = set()
+        # hashmap = defaultdict(str)
+        # for i in range(len(s)):
+        #     if hashmap[s[i]]:
+        #         if t[i] != hashmap[s[i]]:
+        #             return False
+        #     else:
+        #         if t[i] in hashset:
+        #             return False
+        #         hashmap[s[i]] = t[i]
+        #         hashset.add(t[i])
+        # return True
+
         if len(s) != len(t):
             return False
-        hashset = set()
-        hashmap = defaultdict(str)
+        hashmap_s_t = defaultdict(str)
+        hashmap_t_s = defaultdict(str)
         for i in range(len(s)):
-            if hashmap[s[i]]:
-                if t[i] != hashmap[s[i]]:
+            if hashmap_s_t[s[i]] or hashmap_t_s[t[i]]:
+                if hashmap_s_t[s[i]] != t[i] or hashmap_t_s[t[i]] != s[i]:
                     return False
             else:
-                if t[i] in hashset:
-                    return False
-                hashmap[s[i]] = t[i]
-                hashset.add(t[i])
+                hashmap_s_t[s[i]] = t[i]
+                hashmap_t_s[t[i]] = s[i]
         return True
+            
+
+
