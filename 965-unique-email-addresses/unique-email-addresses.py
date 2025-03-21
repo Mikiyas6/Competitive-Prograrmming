dadf6bbdcd1ed8cc20bb8450118f57f8b1a82de5
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-21 11:20:37 +0300

    Time: 15 ms (20.17%) | Memory: 18.2 MB (5.22%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.py
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -1,8 +1,14 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        unique_emails = set()
+        hashmap = defaultdict(int)
         for email in emails:
-            local_name, domain_name = email.split('@')
-            local_name = local_name.split('+')[0].replace('.', '')
-            unique_emails.add(local_name + '@' + domain_name)
-        return len(unique_emails)
+            local, domain = email.split("@")
+            print("local->",local)
+            print("domain->",domain)
+            local = "".join(local.split("."))
+            local = local.split("+")[0]
+            print("local->",local)
+            final_email = "@".join([local,domain])
+            print("final_email->",final_email)
+            hashmap[final_email] += 1
+        return len(hashmap)
