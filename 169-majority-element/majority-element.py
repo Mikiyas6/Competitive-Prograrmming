75b4ef9d0e0cfd23fadedec2e8900af580f7f9df
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-26 09:47:39 +0300

    Time: 7 ms (56.25%) | Memory: 19.4 MB (19.99%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
         hashmap = Counter(nums)
-        majority_element = 0
-        max_occurance = 0
-        print(hashmap)
-        for value in nums:
-            if hashmap[value] > max_occurance:
-                majority_element = value
-                max_occurance = hashmap[value]
-        return majority_element
+        hashset = set(nums)
+        max_length = 1
+        answer = nums[0]
+        for value in hashset:
+            if hashmap[value] > n//2 and hashmap[value] > max_length:
+                max_length = hashmap[value]
+                answer = value
+        return answer
