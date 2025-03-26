145a9efb88dc7a16ccbe0c3d9603162c0bd6b69c
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-26 10:04:57 +0300

    Time: 6 ms (61.35%) | Memory: 18.7 MB (38.66%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,58 +1,11 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-
-        n = len(nums)
-
-        left_prefix_sum = [0]*n
-
-        right_prefix_sum = [0]*n
-
         cumulative = 0
-
-        for index, value in enumerate(nums):
-
-            left_prefix_sum[index] = cumulative
-
-            cumulative += value
-        
-        cumulative = 0
-        
-        for index in range(n-1,-1,-1):
-
-            right_prefix_sum[index] = cumulative
-
-            cumulative += nums[index]
-        
-        for index in range(n):
-
-            if left_prefix_sum[index] == right_prefix_sum[index]:
-
+        prefix_sum = []
+        for num in nums:
+            cumulative += num
+            prefix_sum.append(cumulative)
+        for index in range(len(nums)):
+            if prefix_sum[index] - nums[index] == prefix_sum[-1] - prefix_sum[index]:
                 return index
-        
         return -1
-
-        
-        # n = len(nums)
-
-        # prefix_sum = [0] * (n+2)
-
-        # cumulative = 0
-
-        # for index,value in enumerate(nums):
-
-        #     cumulative += value
-
-        #     prefix_sum[index+1] = cumulative
-        
-        # for index,value in enumerate(prefix_sum[1:n+1]):
-
-        #     left = prefix_sum[index]
-
-        #     right = prefix_sum[n] - value
-
-        #     if left == right:
-
-        #         return index
-        
-        # return -1
-            
