57321f81a0bcb16528d33032fb514faee889b190
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-30 17:42:41 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.51%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,16 +1,11 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         hashmap = Counter(blocks[:k])
-        if hashmap["B"] == k:
-            return 0
-        min_operation = hashmap["W"]
+        min_operations = hashmap['W']
         left = 0
         for right in range(k,len(blocks)):
-            hashmap[blocks[left]] -= 1
             hashmap[blocks[right]] += 1
-            if hashmap["B"] == k:
-                return 0
-            min_operation = min(min_operation,hashmap["W"])
+            hashmap[blocks[left]] -= 1
+            min_operations = min(min_operations,hashmap["W"])
             left += 1
-        return min_operation
-
+        return min_operations if min_operations > 0 else 0
