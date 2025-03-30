8cf097d464ff354084508aac12c53c4161c41cbb
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-03-30 16:24:05 +0300

    Time: 43 ms (15.31%) | Memory: 36.8 MB (7.33%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,11 +1,9 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        hashmap = defaultdict(list)
-        for index,value in enumerate(nums):
-            hashmap[value].append(index)
-        for value in hashmap:
-            if len(hashmap[value]) > 1:
-                for index in range(1,len(hashmap[value])):
-                    if hashmap[value][index] - hashmap[value][index-1] <= k:
-                        return True
+        hashmap = defaultdict(int)
+        for right in range(len(nums)):
+            if hashmap[nums[right]] and right+1 - hashmap[nums[right]] <= k:
+                return True
+            hashmap[nums[right]] = right+1
         return False
+
