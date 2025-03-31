0adce8a07dd75db75e1ae153d207bd34e2ff7096
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-01 01:53:47 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (12.10%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        numRows = rowIndex+1
-        grid = [[0]*(numRows) for _ in range(numRows)]
-        for i in range(numRows):
-            grid[i][0] = 1
-        for row in range(1,numRows):
-            for col in range(1,numRows):
-                grid[row][col] = grid[row-1][col-1] + grid[row-1][col]
-        for i in range(numRows):
-            grid[i] = grid[i][:i+1]
-        return grid[rowIndex]
+        matrix = [[0]*(rowIndex+2) for _ in range(rowIndex+1)]
+        matrix[0][1] = 1
+        print(matrix)
+        def fun(row):
+            if row >= len(matrix):
+                return
+            for col in range(1,len(matrix[0])):
+                matrix[row][col] = matrix[row-1][col-1] + matrix[row-1][col]
+            fun(row+1)
+        fun(1)
+        return matrix[-1][1:]
