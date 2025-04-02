8ca3752cb9ba1a9a7e7b560b053e163f3fbeac77
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-03 00:52:36 +0300

    Time: 743 ms (5.01%) | Memory: 45.1 MB (6.59%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -6,64 +6,46 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        if not head:
-            return head
+        def merge_sort(left,right):
+            dummy = ListNode(-1)
+            current = dummy
+            def fun(current,left,right):
+                if not left:
+                    current.next = right
+                    return current
+                if not right:
+                    current.next = left
+                    return current
+                if left.val <= right.val:
+                    node = ListNode(left.val)
+                    left = left.next
+                else:
+                    node = ListNode(right.val)
+                    right = right.next
+                current.next = fun(node,left,right)
+                return current
+            return fun(current,left,right).next
 
-        def findMiddle(current):
-            
-            slow = current
-            fast = current.next
-
+        def findMiddle(head):
+            if not head or not head.next:
+                return head
+            slow = head
+            fast = head.next
             while fast and fast.next:
                 slow = slow.next
                 fast = fast.next.next
-            
             return slow
-        
-        def mergeSort(list1,list2):
-            ptr1 = list1
-            ptr2 = list2
-            dummy = ListNode(-1)
-            current = dummy
 
-            def merge(current,ptr1,ptr2):
-
-                if not ptr1:
-                    current.next = ptr2
-                    return current
-                if not ptr2:
-                    current.next = ptr1
-                    return current
-                
-                if ptr1.val <= ptr2.val:
-                    value = ptr1.val
-                    ptr1 = ptr1.next
-                else:
-                    value = ptr2.val
-                    ptr2 = ptr2.next
-                
-                node = ListNode(value)
-                current.next = merge(node,ptr1,ptr2)
-                return current
-            
-            return merge(current,ptr1,ptr2).next
-
-        def fun(current):
-
-            if not current or not current.next:
-                return current
-            
-            middle = findMiddle(current)
+        def split(head):
+            if not head or not head.next:
+                return head
+            middle = findMiddle(head)
             right_side = middle.next
             middle.next = None
-
-            left = fun(current)
-            right = fun(right_side)
-
-            mergedList = mergeSort(left,right)
-
-            return mergedList
+            left_side = head
+            left = split(left_side)
+            right = split(right_side)
+            merged = merge_sort(left,right)
+            return merged
         
-        current = head
-        return fun(current)
-    
+        return split(head)
