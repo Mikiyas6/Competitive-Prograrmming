42a2d68cf11b967cba951d63fe00f0014c5f34f0
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-03 00:20:51 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.79%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,14 +1,32 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        hashset = set([0])
-        while n != 1:
+        # hashset = set([0])
+        # while n != 1:
+        #     temp = n
+        #     total = 0
+        #     while temp != 0:
+        #         total +=  (temp%10)**2
+        #         temp //= 10
+        #     if total in hashset:
+        #         return False
+        #     n = total
+        #     hashset.add(total)
+        # return True
+        if n == 1:
+            return True
+        def sum_of_squares(n):
             temp = n
             total = 0
             while temp != 0:
-                total +=  (temp%10)**2
+                total += (temp%10)**2
                 temp //= 10
-            if total in hashset:
-                return False
-            n = total
-            hashset.add(total)
-        return True
+            return total
+        slow = n
+        fast = sum_of_squares(n)
+        while slow != fast:
+            slow = sum_of_squares(slow)
+            fast = sum_of_squares(sum_of_squares(fast))
+            if slow == 1:
+                return True
+        return False
+            
