f8f9e46f65f07e425c0b8a3e5cea81ea8fd4a073
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-03 19:57:50 +0300

    Time: 1 ms (40.92%) | Memory: 19 MB (43.13%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -6,42 +6,18 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
-        def path_sum(root,cumulative,target):
-
+        if not root:
+            return False
+        self.ans = False
+        def helper(root,total):
             if not root:
-                return False
-            
-            if not root.left and not root.right and root.val + cumulative == target:
-                return True
-            
-            return path_sum(root.left,root.val+cumulative,target) or path_sum(root.right,root.val+cumulative,target)
-        
-        return path_sum(root,0,targetSum)
-
-        # def accumulate(root,cumulative):
-
-        #     if not root:
-        #         return root
-            
-        #     root.val = root.val + cumulative
-
-        #     root.left = accumulate(root.left,root.val)
-
-        #     root.right = accumulate(root.right,root.val)
-
-        #     return root
-        
-        # def find_node(root,target):
-
-        #     if not root:
-        #         return False
-            
-        #     if not root.left and not root.right and root.val == target:
-        #         return True
-            
-        #     return find_node(root.left,target) or find_node(root.right,target)
-
-        # root = accumulate(root,0)
-
-        # return find_node(root,targetSum)
+                return
+            if not root.left and not root.right:
+                total += root.val
+                self.ans = self.ans or total == targetSum
+                return 
+            total += root.val
+            helper(root.left,total)
+            helper(root.right,total)
+        helper(root,0)
+        return self.ans
