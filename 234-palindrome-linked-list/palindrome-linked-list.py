8f0c1c11d3914f392bb350f64d3b7f4f38d38221
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-03 20:38:58 +0300

    Time: 273 ms (5.04%) | Memory: 39.5 MB (28.78%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,41 +5,31 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-
-        if not head or not head.next:
-            return True
-        
-        def findMiddle(head):
-            if not head or not head.next:
-                return head
-            slow = head
-            fast = head.next
-            while fast and fast.next:
-                slow = slow.next
-                fast = fast.next.next
-            return slow
-        
         def reverse(head):
             prev = None
             current = head
             while current:
-                second_part = current.next
-                current.next = None
+                next = current.next
                 current.next = prev
                 prev = current
-                current = second_part
-            return prev 
+                current = next
+            return prev
 
-        middle = findMiddle(head)
-        second_part = middle.next
-        middle.next = None
-        first_part = head
-        reversed_second_part = reverse(second_part)
-        ptr1 = first_part
-        ptr2 = reversed_second_part
+        def makeCopy(head):
+            dummy = ListNode(-1)
+            current = dummy
+            ptr = head
+            while ptr:
+                current.next = ListNode(ptr.val)
+                ptr = ptr.next
+                current = current.next
+            return dummy.next
+        
+        ptr1 = head
+        ptr2 = reverse(makeCopy(head))
         while ptr1 and ptr2:
             if ptr1.val != ptr2.val:
                 return False
             ptr1 = ptr1.next
             ptr2 = ptr2.next
-        return True 
+        return True if (not ptr1 and not ptr2) else False
