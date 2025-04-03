f76449bd08321abb208f55ad8e89ec36fe9b0823
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-03 16:09:16 +0300

    Time: 44 ms (84.12%) | Memory: 19.5 MB (21.65%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -1,25 +1,21 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, val=None, children=None):
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
         self.val = val
         self.children = children
 """
 
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        
-        def fun(root):
-
+        result = []
+        def postOrder(root):
+            nonlocal result
             if not root:
                 return
-            
             children = root.children
-            result = []
             for child in children:
-                result.extend(fun(child))
+                postOrder(child)
             result.append(root.val)
-            return result
-        
-        return fun(root)
-                
+        postOrder(root)
+        return result
