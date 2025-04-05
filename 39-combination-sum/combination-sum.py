5ae7c48cc2003bb47f132395061d874fcce5b5f0
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-05 16:35:00 +0300

    Time: 14 ms (32.72%) | Memory: 17.8 MB (87.50%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,13 +1,26 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.result = []
+        self.size = len(candidates)
         def dfs(i,comb,total):
-            if total >= target:
-                if total == target:
-                    self.result.append(comb[:])
+            if total == target:
+                self.result.append(comb[:])
                 return
-            for j in range(i,len(candidates)):
-                dfs(j,comb+[candidates[j]],total+candidates[j])
+            if i >= self.size or total > target:
+                return
+            dfs(i,comb+[candidates[i]],total+candidates[i])
+            dfs(i+1,comb,total)
         dfs(0,[],0)
         return self.result
+
+        # self.result = []
+        # def dfs(i,comb,total):
+        #     if total >= target:
+        #         if total == target:
+        #             self.result.append(comb[:])
+        #         return
+        #     for j in range(i,len(candidates)):
+        #         dfs(j,comb+[candidates[j]],total+candidates[j])
+        # dfs(0,[],0)
+        # return self.result
 
