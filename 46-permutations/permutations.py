7c3e9b34eac02af641ceac1c197f608c27b9896e
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-05 22:46:09 +0300

    Time: 3 ms (32.48%) | Memory: 18 MB (32.48%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,18 +1,13 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
-        result = []
-        def backtrack(nums,path):
-
-            if len(path) == len(nums):
-                result.append(path[:])
-                return 
-            
-            for num in nums:
-                if num not in path:
-                    path.append(num)
-                    backtrack(nums,path)
-                    path.pop()
-            
-        backtrack(nums,[])
-        return result
+        self.result = []
+        self.size = len(nums)
+        def dfs(perm,length):
+            if length == self.size:
+                self.result.append(perm[:])
+                return
+            for value in nums:
+                if value not in perm:
+                    dfs(perm+[value],length+1)
+        dfs([],0)
+        return self.result
