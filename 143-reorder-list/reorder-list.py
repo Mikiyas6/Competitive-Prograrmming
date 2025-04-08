a24b3e96de60fd689756f1de78885d6c0ff9b145
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-09 01:32:40 +0300

    Time: 8 ms (9.95%) | Memory: 23.3 MB (38.94%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,38 +8,28 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        def find_middle(head):
+        def findMiddle(head):
             if not head or not head.next:
                 return head
             slow = head
-            fast = head.next
+            fast = head
             while fast and fast.next:
                 slow = slow.next
                 fast = fast.next.next
             return slow
-        def reverse(head):
-            prev = None
-            current = head
-            while current:
-                next = current.next
-                current.next = prev
-                prev = current
-                current = next
-            return prev
-        middle = find_middle(head)
-        second_half = middle.next
+       
+        middle = findMiddle(head) 
+        last_part = middle.next
         middle.next = None
-        ptr1 = head
-        ptr2 = reverse(second_half)
-        def fun(ptr1,ptr2):
-            if not ptr1 and not ptr2:
-                return None
-            if not ptr2 and ptr1:
-                return ptr1
-            next1 = ptr1.next
-            next2 = ptr2.next
-            ptr1.next = ptr2
-            ptr2.next = fun(next1,next2)
-            return ptr1
-        fun(ptr1,ptr2)
+        stack = []
+        while last_part:
+            stack.append(last_part)
+            last_part = last_part.next
+        current = head
+        while current and stack:
+            next = current.next
+            node = stack.pop()
+            current.next = node
+            node.next = next
+            current = next
 
