1f3b68c0be2ecf1ee59e58bb3aa6e5e20ec2ed83
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-10 18:53:00 +0300

    Time: 7 ms (58.18%) | Memory: 18.3 MB (9.67%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,42 +1,16 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        
-        def fun(processed,unprocessed):
-
-            if not unprocessed:
-
-                return [processed]
-            
-            first_char = unprocessed[0]
-
-            combinations = []
-
-            for i in range(len(processed)+1):
-
-                combinations.extend(fun(processed[:i]+[first_char]+processed[i:],unprocessed[1:]))
-            
-            return combinations
-        
-        nums = fun([],nums)
-
-        nums.sort()
-
-        i = 0
-        j = i + 1
-
-        result = []
-
-        while j < len(nums):
-
-            if nums[j] != nums[i]:
-
-                result.append(nums[i])
-            
-            i += 1
-            j += 1
-        
-        if nums[i] not in result:
-
-            result.append(nums[i])
-
-        return result
+        hashmap = Counter(nums)
+        self.result = []
+        self.size = len(nums)
+        def dfs(hashmap,permute,length):
+            if length == self.size:
+                self.result.append(permute[:])
+                return
+            for value in hashmap:
+                if hashmap[value] > 0:
+                    hashmap[value] -= 1
+                    dfs(hashmap,permute+[value],length+1)
+                    hashmap[value] += 1
+        dfs(hashmap,[],0)
+        return self.result
