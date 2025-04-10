5715af927b41015b8abad9ec2ca5f1ab658b4f3e
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-11 00:53:56 +0300

    Time: 51 ms (34.35%) | Memory: 17.9 MB (68.38%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -2,16 +2,15 @@ class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         self.result = 0
         hashmap = Counter(tiles)
-        def helper(hashmap,permute):
+        def helper(hashmap):
             for value in hashmap:
                 result = 0
                 if hashmap[value] > 0:
                     self.result += 1
-                    permute.append(hashmap[value])
                     hashmap[value] -= 1
-                    result += helper(hashmap,permute)
+                    result += helper(hashmap)
                     hashmap[value] += 1
                 self.result += result
             return result
-        helper(hashmap,[])
+        helper(hashmap)
         return self.result
