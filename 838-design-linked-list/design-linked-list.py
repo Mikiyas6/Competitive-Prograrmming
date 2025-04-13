5910d30726be303135b35fd694aa05b87641985a
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-13 19:51:51 +0300

    Time: 56 ms (45.26%) | Memory: 18.8 MB (37.48%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -1,131 +1,72 @@
 class ListNode:
-
-    def __init__(self, val=0, next=None):
+    def __init__(self,val=0,next=None):
         self.val = val
         self.next = next
-
 class MyLinkedList:
 
     def __init__(self):
-        
         self.head = None
         self.size = 0
 
     def get(self, index: int) -> int:
-        
-        if index >= self.size or index < 0:
-
+        if index >= self.size:
             return -1
-        
         current = self.head
-        
-        for i in range(index):
-
+        for _ in range(index):
             current = current.next
-        
         return current.val
 
     def addAtHead(self, val: int) -> None:
-
-        Node = ListNode(val)
-        Node.next = self.head
-        self.head = Node
+        node = ListNode(val)
+        node.next = self.head
+        self.head = node
         self.size += 1
-    
+
     def addAtTail(self, val: int) -> None:
-
         if not self.head:
-
             self.addAtHead(val)
             return
-        
+        end_up = self.size - 1
         current = self.head
-
-        Node = ListNode(val)
-
-        while current.next:
-
+        for _ in range(end_up):
             current = current.next
-        
-        current.next = Node
-
+        current.next = ListNode(val)
         self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
-
         if index > self.size:
-
             return
-        
         if index == 0:
-
             self.addAtHead(val)
-        
-        elif index == self.size:
-
-            self.addAtTail(val)
-        
-        else:
-                
-            end_up = index-1
-
-            current = self.head
-
-            for i in range(end_up):
-
-                current = current.next
-            
-            temp = current.next
-    
-            Node = ListNode(val)
-            current.next= Node
-            Node.next = temp
-            self.size += 1
-
-    def deleteAtIndex(self, index: int) -> None:
-
-        if index < 0 or index >= self.size:
-
             return
-
-        if index == 0:
-
-            self.deleteAtFirst()
-        
-        elif index == self.size-1:
-
-            self.deleteAtEnd()
-        
-        else:
-            
-            end_up = index - 1
-
-            current = self.head
-
-            for i in range(end_up):
-
-                current = current.next
-            
-            current.next = current.next.next
-            self.size -= 1
-        
-    def deleteAtFirst(self) -> None:
-        
-        self.head = self.head.next
-        self.size -= 1
-    
-    def deleteAtEnd(self) -> None:
-
+        if index == self.size:
+            self.addAtTail(val)
+            return
+        end_up = index-1
         current = self.head
-
-        while current.next.next:
-
+        for _ in range(end_up):
             current = current.next
+        next = current.next
+        node = ListNode(val)
+        current.next = node
+        node.next = next
+        self.size += 1
         
-        current.next = None
-
+    def deleteAtIndex(self, index: int) -> None:
+        if not self.head:
+            return
+        if index == 0:
+            self.head = self.head.next
+            self.size -= 1
+            return
+        if index >= self.size:
+            return
+        end_up = index-1
+        current = self.head
+        for _ in range(end_up):
+            current = current.next
+        current.next = current.next.next
         self.size -= 1
-
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
