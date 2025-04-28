9aec5fcae8ba1f89d588f723f37e0027b9c8d6c1
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-04-28 06:41:41 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.68%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,30 +1,74 @@
 class MyQueue:
-
     def __init__(self):
+        # Stack to handle incoming elements
         self.in_stack = []
+        # Stack that is used to return elements in the correct queue order
         self.out_stack = []
-
     def push(self, x: int) -> None:
+        """
+        Push element x to the back of the queue.
+
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+        """
         self.in_stack.append(x)
 
     def pop(self) -> int:
-        if not self.out_stack:
+        """
+        - If there are elements inside the out_stack, remove the element at the top of the stack and return it.
+        - If there are no elements inside the out_stack, we will proceed to transfer elements from in_stack to out_stack. And then, we will do the same thing as above, (i.e remove and return the value at the top of the out_stack)
+
+        Amortized Time Complexity: O(1)
+        Worst-case Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        if self.out_stack:
+            return self.out_stack.pop()
+        else:
             self._transfer()
-        return self.out_stack.pop()
-        
+            return self.out_stack.pop()
+
+
     def peek(self) -> int:
-        if not self.out_stack:
+        """
+        - If there are elements inside the out_stack, return the element at the top of the stack without removing it.
+        - If there are no elements inside the out_stack, We will proceed to transfer elements from in_stack to out_stack. And then, we will return the value at the top of the out_stack (also without removing it)
+
+        Amortized Time Complexity: O(1)
+        Worst-case Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        if self.out_stack:
+            return self.out_stack[-1]
+        else:
             self._transfer()
-        return self.out_stack[-1]
+            return self.out_stack[-1]
 
     def empty(self) -> bool:
-        return len(self.in_stack) == 0 and len(self.out_stack) == 0
-    
-    def _transfer(self) -> None:
+        """
+        Returns True if it's empty and False if it's not.
+
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+        """
+        # The queue is empty if and only if both the stacks are empty. Else, if we have at least one element in either of the two stacks, it's not empty.
+        if not self.in_stack and not self.out_stack:
+            return True
+        else:
+            return False
+
+    def _transfer(self):
+        """
+        Transfer all elements from in_stack to out_stack.
+        This is done only when out_stack is empty and we need
+        to perform a pop or peek operation.
+
+        Time Complexity: O(n) (only when transferring)
+        Space Complexity: O(1)
+        """
         while self.in_stack:
             popped_value = self.in_stack.pop()
             self.out_stack.append(popped_value)
-
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
