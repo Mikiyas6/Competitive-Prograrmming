2827d11ce2d78dabfaed1116716058d69f8bb77b
Mikiyas Teowodroes <110767281+Mikiyas6@users.noreply.github.com>
2025-07-05 21:52:49 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (50.63%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -2,7 +2,7 @@ class Solution:
     def findLucky(self, arr: List[int]) -> int:
         largest = -1
         hashmap = Counter(arr)
-        for value in hashmap:
+        for value in set(arr):
             if value == hashmap[value]:
                 largest = max(largest,value)
         return largest
